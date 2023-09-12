94199e51b2117309596383ef549095a24a048787
JivkoSp <atjspasov@gmail.com>
2023-09-12 18:46:03 +0300

    Added new custom domain layer exception type: WireChat.Domain/Exceptions/EmptyUserEmailException.cs

---
+++ WireChat/WireChat.Domain/Exceptions/EmptyUserEmailException.cs
@@ -0,0 +1,10 @@
+﻿
+namespace WireChat.Domain.Exceptions
+{
+    public sealed class EmptyUserEmailException : DomainException
+    {
+        internal EmptyUserEmailException() : base(message: "User email cannot be empty!")
+        {
+        }
+    }
+}
