f0ff8b735956954a93a50de3ef8d6c8c0a8be21d
JivkoSp <atjspasov@gmail.com>
2023-09-12 18:52:19 +0300

    Added new custom domain layer exception type: WireChat.Domain/Exceptions/EmptyUserFirstNameException.cs

---
+++ WireChat/WireChat.Domain/Exceptions/EmptyUserFirstNameException.cs
@@ -0,0 +1,10 @@
+﻿
+namespace WireChat.Domain.Exceptions
+{
+    public sealed class EmptyUserFirstNameException : DomainException
+    {
+        internal EmptyUserFirstNameException() : base(message: "User first name cannot be empty!")
+        {
+        }
+    }
+}
