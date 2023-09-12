187c2d80ae48a3d981c680703e018bdac68fdaa8
JivkoSp <atjspasov@gmail.com>
2023-09-12 14:17:10 +0300

    Updated WireChat/bin/Debug/net8.0/

--- WireChat/WireChat/obj/Debug/net8.0/WireChat.AssemblyInfo.cs
+++ WireChat/WireChat/obj/Debug/net8.0/WireChat.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WireChat")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+035d4fc3ebc93ca82d7b673ccff50090e57ec94b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9ec97c401697d75d292555c7ff0d0e0c9b46baad")]
 [assembly: System.Reflection.AssemblyProductAttribute("WireChat")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WireChat")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
