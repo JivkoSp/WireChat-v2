c7428f50ac923a07a1c356e2dbd389c733125b98
JivkoSp <atjspasov@gmail.com>
2023-09-13 09:17:10 +0300

    Added new custom domain layer exception type: WireChat.Domain/Exceptions/EmptyContactRequestMessageException.cs

---
+++ WireChat/WireChat.Domain/Exceptions/EmptyContactRequestMessageException.cs
@@ -0,0 +1,10 @@
+﻿
+namespace WireChat.Domain.Exceptions
+{
+    public sealed class EmptyContactRequestMessageException : DomainException
+    {
+        internal EmptyContactRequestMessageException() : base(message: "The ContactRequest Message cannot be empty!")
+        {
+        }
+    }
+}
