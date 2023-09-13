b3d0309585aee3de9b3db4947db036edafa5a5a1
JivkoSp <atjspasov@gmail.com>
2023-09-13 13:16:44 +0300

    Added new custom domain layer exception type: WireChat.Domain/Exceptions/NullChatIdException.cs

---
+++ WireChat/WireChat.Domain/Exceptions/NullChatIdException.cs
@@ -0,0 +1,10 @@
+﻿
+namespace WireChat.Domain.Exceptions
+{
+    public sealed class NullChatIdException : DomainException
+    {
+        internal NullChatIdException() : base(message: "ChatId cannot be null!")
+        {
+        }
+    }
+}
