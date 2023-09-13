6fcc2e3c37e76ed661b32786593efc6501255100
JivkoSp <atjspasov@gmail.com>
2023-09-13 17:24:38 +0300

    Added new domain value object: WireChat.Domain/ValueObjects/Message.cs

---
+++ WireChat/WireChat.Domain/ValueObjects/Message.cs
@@ -0,0 +1,19 @@
+﻿using WireChat.Domain.Exceptions;
+
+namespace WireChat.Domain.ValueObjects
+{
+    public record Message
+    {
+        internal string Value { get; }
+
+        public Message(string value)
+        {
+            if (string.IsNullOrWhiteSpace(value))
+            {
+                throw new EmptyChatMessageException();
+            }
+
+            Value = value;
+        }
+    }
+}
