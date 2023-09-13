4c1c401afad62b1e9af70d8e75c2b135f553eb5c
JivkoSp <atjspasov@gmail.com>
2023-09-13 15:56:58 +0300

    Added new custom domain layer exception type: WireChat.Domain/Exceptions/NullChatMessageParametersException.cs

---
+++ WireChat/WireChat.Domain/Exceptions/NullChatMessageParametersException.cs
@@ -0,0 +1,11 @@
+﻿
+namespace WireChat.Domain.Exceptions
+{
+    public sealed class NullChatMessageParametersException : DomainException
+    {
+        internal NullChatMessageParametersException() 
+            : base(message: "ChatMessage cannot be initialized with one or more null parameters!")
+        {
+        }
+    }
+}
