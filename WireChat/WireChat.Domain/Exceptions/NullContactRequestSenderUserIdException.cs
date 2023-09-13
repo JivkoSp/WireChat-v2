4ff6104a156c025d6f9cea7b71f11bf32dae971d
JivkoSp <atjspasov@gmail.com>
2023-09-13 09:37:04 +0300

    Added new custom domain layer exception type: WireChat.Domain/Exceptions/NullContactRequestSenderUserIdException.cs

---
+++ WireChat/WireChat.Domain/Exceptions/NullContactRequestSenderUserIdException.cs
@@ -0,0 +1,10 @@
+﻿
+namespace WireChat.Domain.Exceptions
+{
+    public sealed class NullContactRequestSenderUserIdException : DomainException
+    {
+        internal NullContactRequestSenderUserIdException() : base(message: "The UserID for ContactRequest Sender cannot be null!")
+        {
+        }
+    }
+}
