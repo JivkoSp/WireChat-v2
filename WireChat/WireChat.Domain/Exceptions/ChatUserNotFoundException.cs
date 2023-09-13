b33bea78b1166581f91100c6dfbd50a2549aaf6e
JivkoSp <atjspasov@gmail.com>
2023-09-13 11:53:29 +0300

    Added new custom domain layer exception type: WireChat.Domain/Exceptions/ChatUserNotFoundException.cs

---
+++ WireChat/WireChat.Domain/Exceptions/ChatUserNotFoundException.cs
@@ -0,0 +1,11 @@
+﻿
+namespace WireChat.Domain.Exceptions
+{
+    public sealed class ChatUserNotFoundException : DomainException
+    {
+        internal ChatUserNotFoundException(Guid userId, Guid chatId) 
+            : base(message: $"There is no chat with ID #{chatId} that has user #{userId}!")
+        {
+        }
+    }
+}
