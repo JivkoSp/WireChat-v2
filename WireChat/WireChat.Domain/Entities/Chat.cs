8f6ff86c8e735a110de86edc45703197e7cce813
JivkoSp <atjspasov@gmail.com>
2023-09-13 14:45:04 +0300

    Removed the ChatType enum from WireChat.Domain/Entities/Chat.cs. The enum is replaced with the newly added ChatType value object.

--- WireChat/WireChat.Domain/Entities/Chat.cs
+++ WireChat/WireChat.Domain/Entities/Chat.cs
@@ -4,12 +4,6 @@ using WireChat.Domain.ValueObjects;
 
 namespace WireChat.Domain.Entities
 {
-    public enum ChatType
-    {
-        OneToOne,
-        Group
-    };
-
     public class Chat : AggregateRoot<ChatID>
     {
         private ChatType _chatType;
