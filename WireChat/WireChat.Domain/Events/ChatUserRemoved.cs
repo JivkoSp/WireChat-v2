dae3482e81845c297e4e84360fbd52ea56da0593
JivkoSp <atjspasov@gmail.com>
2023-09-13 14:47:28 +0300

    Added new custom domain event type: WireChat.Domain/Events/ChatUserRemoved.cs

---
+++ WireChat/WireChat.Domain/Events/ChatUserRemoved.cs
@@ -0,0 +1,7 @@
+﻿using WireChat.Domain.Entities;
+using WireChat.Domain.ValueObjects;
+
+namespace WireChat.Domain.Events
+{
+    public record ChatUserRemoved(Chat Chat, ChatUser UserChat) : IDomainEvent;
+}
