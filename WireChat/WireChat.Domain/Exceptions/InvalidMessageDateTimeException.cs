b6913166f5b22a0e88fb4ea5cde06fef1cd2cd4a
JivkoSp <atjspasov@gmail.com>
2023-09-13 15:49:18 +0300

    Added new custom domain layer exception type: WireChat.Domain/Exceptions/InvalidMessageDateTimeException.cs

---
+++ WireChat/WireChat.Domain/Exceptions/InvalidMessageDateTimeException.cs
@@ -0,0 +1,10 @@
+﻿
+namespace WireChat.Domain.Exceptions
+{
+    public sealed class InvalidMessageDateTimeException : DomainException
+    {
+        internal InvalidMessageDateTimeException() : base(message: "Invalid Message datetime!")
+        {
+        }
+    }
+}
