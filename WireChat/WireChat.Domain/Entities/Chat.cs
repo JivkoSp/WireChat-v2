a4928939bd9622fced47b95d67d1ca56ed70b7b9
JivkoSp <atjspasov@gmail.com>
2023-09-13 13:57:58 +0300

    Added AddChatUser and RemoveChatUser methods to WireChat.Domain/Entities/Chat.cs

--- WireChat/WireChat.Domain/Entities/Chat.cs
+++ WireChat/WireChat.Domain/Entities/Chat.cs
@@ -1,4 +1,5 @@
-﻿using WireChat.Domain.Exceptions;
+﻿using WireChat.Domain.Events;
+using WireChat.Domain.Exceptions;
 using WireChat.Domain.ValueObjects;
 
 namespace WireChat.Domain.Entities
@@ -12,7 +13,7 @@ namespace WireChat.Domain.Entities
     public class Chat : AggregateRoot<ChatID>
     {
         private ChatType _chatType;
-        private List<User> _users;
+        private HashSet<ChatUser> _users =new HashSet<ChatUser>();
 
         private Chat() {}
 
@@ -22,6 +23,32 @@ namespace WireChat.Domain.Entities
 
             Id = chatId;
             _chatType = chatType;
+        }
+
+        //Add ChatUser to OneToOne or Group chat
+        public void AddChatUser(ChatUser chatUser)
+        {
+            var alreadyExists = _users.Contains(chatUser);  
+
+            if (alreadyExists)
+            {
+                throw new ChatUserAlreadyExistsException(chatUser.UserID, chatUser.ChatID);
+            }
+
+            _users.Add(chatUser);
+
+            AddEvent(new ChatUserAdded(this, chatUser));
+        }
+
+        //Remove ChatUser from Group chat.
+        public void RemoveChatUser(ChatUser chatUser)
+        {
+            var chatUserToRemove = _users.SingleOrDefault(chatUser);
+
+            if (chatUserToRemove is null)
+            {
+                throw new ChatUserNotFoundException(chatUser.UserID, chatUser.ChatID);
+            }
         }
     }
 }
