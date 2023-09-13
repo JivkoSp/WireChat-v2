f9bab5b8187a17ad61efbfaae9eba197bf0615ec
JivkoSp <atjspasov@gmail.com>
2023-09-13 10:28:31 +0300

    Added new domain value object: WireChat.Domain/ValueObjects/UserContactRequest.cs

---
+++ WireChat/WireChat.Domain/ValueObjects/UserContactRequest.cs
@@ -0,0 +1,33 @@
+﻿using WireChat.Domain.Exceptions;
+
+namespace WireChat.Domain.ValueObjects
+{
+    public record UserContactRequest
+    {
+        public UserID SenderUserId { get; }
+        public UserID ReceiverUserId { get; set; }
+        internal string Message { get; }
+
+        public UserContactRequest(UserID senderUserId, UserID receiverUserId, string message)
+        {
+            if (senderUserId == null)
+            {
+                throw new NullContactRequestSenderUserIdException();
+            }
+
+            if(receiverUserId == null)
+            {
+                throw new NullContactRequestReceiverUserIdException();
+            }
+
+            if(string.IsNullOrWhiteSpace(message))
+            {
+                throw new EmptyContactRequestMessageException();
+            }
+
+            SenderUserId = senderUserId;
+            ReceiverUserId = receiverUserId;
+            Message = message;
+        }
+    }
+}
