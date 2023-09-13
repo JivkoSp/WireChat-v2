1aae9f299fb9e6443e9c08e2d0ebbbac86988a2d
JivkoSp <atjspasov@gmail.com>
2023-09-13 15:40:14 +0300

    Added new custom domain layer exception type: WireChat.Domain/Exceptions/ChatMessageAlreadyExistsException.cs

---
+++ WireChat/WireChat.Domain/Exceptions/ChatMessageAlreadyExistsException.cs
@@ -0,0 +1,11 @@
+﻿
+namespace WireChat.Domain.Exceptions
+{
+    public sealed class ChatMessageAlreadyExistsException : DomainException
+    {
+        internal ChatMessageAlreadyExistsException(Guid chatMessageId) 
+            : base(message: $"Chat message with ID #{chatMessageId} already exists!")
+        {
+        }
+    }
+}
