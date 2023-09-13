b8f17e6b1bc6a74efaff2cd29f40db03cdba089e
JivkoSp <atjspasov@gmail.com>
2023-09-13 17:38:23 +0300

    Added new domain value object: WireChat.Domain/ValueObjects/MessageDateTime.cs

---
+++ WireChat/WireChat.Domain/ValueObjects/MessageDateTime.cs
@@ -0,0 +1,27 @@
+﻿using WireChat.Domain.Exceptions;
+
+namespace WireChat.Domain.ValueObjects
+{
+    public record MessageDateTime
+    {
+        internal DateTimeOffset DateTime { get; }
+
+        public MessageDateTime(DateTimeOffset dateTime)
+        {
+            if (dateTime == default || dateTime > DateTimeOffset.Now)
+            {
+                throw new InvalidMessageDateTimeException();
+            }
+
+            DateTime = dateTime;
+        }
+
+        //Conversion from ValueObject to DateTimeOffset.
+        public static implicit operator DateTimeOffset(MessageDateTime messageDateTime)
+            => messageDateTime.DateTime;
+
+        //Conversion from DateTimeOffset to ValueObject.
+        public static implicit operator MessageDateTime(DateTimeOffset messageDateTime)
+            => new MessageDateTime(messageDateTime);
+    }
+}
