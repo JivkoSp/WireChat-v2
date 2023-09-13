038d5f73ef8b69c352a92e14f3da6101c8ecbab1
JivkoSp <atjspasov@gmail.com>
2023-09-13 10:54:10 +0300

    Updated the WireChat.Domain/Entities/User.cs

--- WireChat/WireChat.Domain/Entities/User.cs
+++ WireChat/WireChat.Domain/Entities/User.cs
@@ -1,7 +1,39 @@
-﻿
+﻿using System.Collections.ObjectModel;
+using WireChat.Domain.Exceptions;
+using WireChat.Domain.ValueObjects;
+
 namespace WireChat.Domain.Entities
 {
-    public class User
+    public class User : AggregateRoot<UserID>
     {
+        private UserFirstName _firstName;
+        private UserLastName _lastName;
+        private UserName _userName;
+        private UserEmail _email;
+        private List<User> _contacts;
+        private List<UserContactRequest> _contactRequests;
+        
+        public IReadOnlyCollection<User> Contacts
+        {
+            get { return new ReadOnlyCollection<User>(_contacts); }
+        }
+
+        public IReadOnlyCollection<UserContactRequest> ContactRequests
+        {
+            get { return new ReadOnlyCollection<UserContactRequest>(_contactRequests); }
+        }
+
+        private User() {}
+
+        public User(UserID userId, UserFirstName userFirstName, UserLastName userLastName, UserName userName, UserEmail userEmail)
+        {
+            ValidateConstructorParameters<NullUserParametersException>([userId, userFirstName, userLastName, userName, userEmail]);
+
+            Id = userId;
+            _firstName = userFirstName;
+            _lastName = userLastName;
+            _userName = userName;
+            _email = userEmail;
+        }
     }
 }
