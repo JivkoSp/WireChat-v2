d7c46ca0139d575a29a6e119b0a9ffe481f0f735
JivkoSp <atjspasov@gmail.com>
2023-09-13 11:50:07 +0300

    Added new custom domain layar exception type: WireChat.Domain/Exceptions/ChatUserAlreadyExistsException.cs

---
+++ WireChat/WireChat.Domain/Exceptions/ChatUserAlreadyExistsException.cs
@@ -0,0 +1,11 @@
+﻿
+namespace WireChat.Domain.Exceptions
+{
+    public sealed class ChatUserAlreadyExistsException : DomainException
+    {
+        internal ChatUserAlreadyExistsException(Guid userId, Guid chatId) 
+            : base(message: $"User #{userId} already exists in chat #{chatId}!")
+        {
+        }
+    }
+}
