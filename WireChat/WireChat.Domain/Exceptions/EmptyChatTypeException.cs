725d9678fe0b7246c2f80ed875201cd55dae602e
JivkoSp <atjspasov@gmail.com>
2023-09-13 14:12:44 +0300

    Added new custom domain layer exception type: WireChat.Domain/Exceptions/EmptyChatTypeException.cs

---
+++ WireChat/WireChat.Domain/Exceptions/EmptyChatTypeException.cs
@@ -0,0 +1,10 @@
+﻿
+namespace WireChat.Domain.Exceptions
+{
+    public sealed class EmptyChatTypeException : DomainException
+    {
+        internal EmptyChatTypeException() : base(message: "The ChatType cannot be empty!")
+        {
+        }
+    }
+}
