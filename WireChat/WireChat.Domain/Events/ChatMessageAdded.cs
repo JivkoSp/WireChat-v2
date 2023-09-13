654c4df357e92a7f241c3217bd8489da74aae446
JivkoSp <atjspasov@gmail.com>
2023-09-13 17:40:49 +0300

    Added new custom domain event type: WireChat.Domain/Events/ChatMessageAdded.cs

---
+++ WireChat/WireChat.Domain/Events/ChatMessageAdded.cs
@@ -0,0 +1,7 @@
+﻿using WireChat.Domain.Entities;
+using WireChat.Domain.ValueObjects;
+
+namespace WireChat.Domain.Events
+{
+    public record ChatMessageAdded(Chat Chat, ChatMessage ChatMessage) : IDomainEvent;
+}
