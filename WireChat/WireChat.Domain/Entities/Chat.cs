920b18edfe286b7a7fb4ba2e61eb2aafd27a88f5
JivkoSp <atjspasov@gmail.com>
2023-09-13 11:46:17 +0300

    Added new domain entity type: WireChat.Domain/Entities/Chat.cs

---
+++ WireChat/WireChat.Domain/Entities/Chat.cs
@@ -0,0 +1,27 @@
+﻿using WireChat.Domain.Exceptions;
+using WireChat.Domain.ValueObjects;
+
+namespace WireChat.Domain.Entities
+{
+    public enum ChatType
+    {
+        OneToOne,
+        Group
+    };
+
+    public class Chat : AggregateRoot<ChatID>
+    {
+        private ChatType _chatType;
+        private List<User> _users;
+
+        private Chat() {}
+
+        public Chat(ChatID chatId, ChatType chatType)
+        {
+            ValidateConstructorParameters<NullChatParametersException>([chatId, chatType]);
+
+            Id = chatId;
+            _chatType = chatType;
+        }
+    }
+}
