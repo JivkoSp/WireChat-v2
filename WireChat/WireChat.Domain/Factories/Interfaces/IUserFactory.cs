6d6f2df2e0e8e192e66c089930b8fa4ebd1e2a96
JivkoSp <atjspasov@gmail.com>
2023-09-14 10:06:39 +0300

    Added WireChat/WireChat.Domain/Factories/Interfaces/IUserFactory.cs

---
+++ WireChat/WireChat.Domain/Factories/Interfaces/IUserFactory.cs
@@ -0,0 +1,10 @@
+﻿using WireChat.Domain.Entities;
+using WireChat.Domain.ValueObjects;
+
+namespace WireChat.Domain.Factories.Interfaces
+{
+    public interface IUserFactory
+    {
+        User Create(UserID userId, UserFirstName userFirstName, UserLastName userLastName, UserName userName, UserEmail userEmail);
+    }
+}
