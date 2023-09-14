b6be164f0e395e541644a721a5c00ee9afa8308a
JivkoSp <atjspasov@gmail.com>
2023-09-14 10:26:51 +0300

    Added Chat factory: WireChat/WireChat.Domain/Factories/ChatFactory.cs

---
+++ WireChat/WireChat.Domain/Factories/ChatFactory.cs
@@ -0,0 +1,12 @@
+﻿using WireChat.Domain.Entities;
+using WireChat.Domain.Factories.Interfaces;
+using WireChat.Domain.ValueObjects;
+
+namespace WireChat.Domain.Factories
+{
+    public sealed class ChatFactory : IChatFactory
+    {
+        public Chat Create(ChatID chatId, ChatType chatType)
+            => new Chat(chatId, chatType);
+    }
+}
