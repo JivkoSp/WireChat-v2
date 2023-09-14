90f2a3da35c15296d457cf9ada14d2a7bcfccd3a
JivkoSp <atjspasov@gmail.com>
2023-09-14 18:43:47 +0300

    Added new custom domain layer exception type: WireChat/WireChat.Domain/Exceptions/UserContactRequestAlreadyExistsException.cs

---
+++ WireChat/WireChat.Domain/Exceptions/UserContactRequestAlreadyExistsException.cs
@@ -0,0 +1,11 @@
+﻿
+namespace WireChat.Domain.Exceptions
+{
+    public sealed class UserContactRequestAlreadyExistsException : DomainException
+    {
+        internal UserContactRequestAlreadyExistsException(Guid senderUserId) 
+            : base(message: $"Contact request from User with ID #{senderUserId} already exists!")
+        {
+        }
+    }
+}
