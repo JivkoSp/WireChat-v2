454b7c63699bf16427b7be7731744e78dd7504ab
JivkoSp <atjspasov@gmail.com>
2023-09-14 12:28:58 +0300

    Renamed WireChat/WireChat.Application/Commands/AddedChatMessageCommand.cs to WireChat/WireChat.Application/Commands/AddChatMessageCommand.cs

--- WireChat/WireChat.Application/Commands/AddChatMessageCommand.cs
+++ WireChat/WireChat.Application/Commands/AddChatMessageCommand.cs
@@ -1,6 +1,6 @@
 ﻿
 namespace WireChat.Application.Commands
 {
-    public record AddedChatMessageCommand(Guid ChatId, Guid ChatMessageId, Guid UserId, 
+    public record AddChatMessageCommand(Guid ChatId, Guid ChatMessageId, Guid UserId, 
         string Message, DateTimeOffset MessageDateTime) : ICommand;
 }
