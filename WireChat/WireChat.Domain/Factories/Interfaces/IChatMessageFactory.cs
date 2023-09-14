d425ccb2358554597ddbc77b2406c4f9936c9545
JivkoSp <atjspasov@gmail.com>
2023-09-14 09:47:04 +0300

    Added WireChat/WireChat.Domain/Factories/Interfaces/IChatMessageFactory.cs

---
+++ WireChat/WireChat.Domain/Factories/Interfaces/IChatMessageFactory.cs
@@ -0,0 +1,10 @@
+﻿using WireChat.Domain.Entities;
+using WireChat.Domain.ValueObjects;
+
+namespace WireChat.Domain.Factories.Interfaces
+{
+    public interface IChatMessageFactory
+    {
+        ChatMessage Create(ChatMessageID chatMessageId, UserID userId, Message message, MessageDateTime messageDateTime);
+    }
+}
