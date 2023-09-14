627762359b0b90a93fe431298aa713c8034d12bf
JivkoSp <atjspasov@gmail.com>
2023-09-14 11:21:48 +0300

    Added ChatMessage repository interface: WireChat/WireChat.Domain/Repositories/IChatMessageRepository.cs.

---
+++ WireChat/WireChat.Domain/Repositories/IChatMessageRepository.cs
@@ -0,0 +1,12 @@
+﻿using WireChat.Domain.Entities;
+using WireChat.Domain.ValueObjects;
+
+namespace WireChat.Domain.Repositories
+{
+    public interface IChatMessageRepository
+    {
+        Task<ChatMessage> GetChatMessageByIdAsync(ChatMessageID chatMessageId);
+        Task AddChatMessageAsync(ChatMessage chatMessage);
+        Task DeleteChatMessageAsync(ChatMessage chatMessage);
+    }
+}
