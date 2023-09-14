7f53c7eecdcd1d88f0765e061ee632b29e92d795
JivkoSp <atjspasov@gmail.com>
2023-09-14 15:24:28 +0300

    Updated WireChat/WireChat.Application/Commands/AddContactRequestCommand.cs

--- WireChat/WireChat.Application/Commands/AddContactRequestCommand.cs
+++ WireChat/WireChat.Application/Commands/AddContactRequestCommand.cs
@@ -1,5 +1,5 @@
 ﻿
 namespace WireChat.Application.Commands
 {
-    public record class AddContactRequestCommand(Guid SenderUserId, Guid ReceiverUserId, string Message) : ICommand;
+    public record AddContactRequestCommand(Guid SenderUserId, Guid ReceiverUserId, string Message) : ICommand;
 }
