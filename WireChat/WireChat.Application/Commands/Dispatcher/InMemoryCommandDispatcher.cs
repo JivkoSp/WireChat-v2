6f023df1744ef996743c42481716af89d1ef42c1
JivkoSp <atjspasov@gmail.com>
2023-09-15 12:46:19 +0300

    Added implementation for the ICommandDispatcher: WireChat/WireChat.Application/Commands/Dispatcher/InMemoryCommandDispatcher.cs

---
+++ WireChat/WireChat.Application/Commands/Dispatcher/InMemoryCommandDispatcher.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using WireChat.Application.Commands.Handlers;
+
+namespace WireChat.Application.Commands.Dispatcher
+{
+    internal sealed class InMemoryCommandDispatcher : ICommandDispatcher
+    {
+        private readonly IServiceProvider _serviceProvider;
+
+        public InMemoryCommandDispatcher(IServiceProvider serviceProvider)
+        {
+            _serviceProvider = serviceProvider;
+        }
+
+        public async Task DispatchAsync<TCommand>(TCommand command) where TCommand : class, ICommand
+        {
+            using var scope = _serviceProvider.CreateScope();
+
+            var handler = scope.ServiceProvider.GetRequiredService<ICommandHandler<TCommand>>();
+
+            await handler.HandleAsync(command);
+        }
+    }
+}
