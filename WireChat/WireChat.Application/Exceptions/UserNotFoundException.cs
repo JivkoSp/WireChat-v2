7dd4df437265387ae277d915230cb9893c88b2b8
JivkoSp <atjspasov@gmail.com>
2023-09-15 14:27:42 +0300

    Added new custom application layer exception type: WireChat/WireChat.Application/Exceptions/UserNotFoundException.cs

---
+++ WireChat/WireChat.Application/Exceptions/UserNotFoundException.cs
@@ -0,0 +1,11 @@
+﻿
+namespace WireChat.Application.Exceptions
+{
+    public sealed class UserNotFoundException : ApplicationException
+    {
+        internal UserNotFoundException(Guid userId) 
+            : base(message: $"User with ID {userId} was not found!")
+        {
+        }
+    }
+}
