2749174518c4dff70ea4f1856ba6d4544a5a51b7
JivkoSp <atjspasov@gmail.com>
2023-09-15 14:18:22 +0300

    Added new command type: WireChat/WireChat.Application/Commands/Handlers/AddChatUserHandler.cs

---
+++ WireChat/WireChat.Application/Commands/Handlers/AddChatUserHandler.cs
@@ -0,0 +1,32 @@
+﻿using WireChat.Application.Exceptions;
+using WireChat.Domain.Repositories;
+using WireChat.Domain.ValueObjects;
+
+namespace WireChat.Application.Commands.Handlers
+{
+    internal sealed class AddChatUserHandler : ICommandHandler<AddChatUserCommand>
+    {
+        private readonly IChatRepository _chatRepository;
+
+        public AddChatUserHandler(IChatRepository chatRepository)
+        {
+            _chatRepository = chatRepository;
+        }
+
+        public async Task HandleAsync(AddChatUserCommand command)
+        {
+            var chat = await _chatRepository.GetChatByIdAsync(command.ChatId);
+
+            if (chat is null)
+            {
+                throw new ChatNotFoundException(command.ChatId);
+            }
+
+            var chatUser = new ChatUser(command.UserId, command.ChatId);
+
+            chat.AddChatUser(chatUser);
+
+            await _chatRepository.UpdateChatAsync(chat);
+        }
+    }
+}
