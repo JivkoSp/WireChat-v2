9f47b2860c6df259d079914a66ff59919fb884ce
JivkoSp <atjspasov@gmail.com>
2023-09-15 10:29:14 +0300

    Added new custom domain layer exception type: WireChat/WireChat.Domain/Exceptions/UserContactRequestNotFoundException.cs

---
+++ WireChat/WireChat.Domain/Exceptions/UserContactRequestNotFoundException.cs
@@ -0,0 +1,11 @@
+﻿
+namespace WireChat.Domain.Exceptions
+{
+    public sealed class UserContactRequestNotFoundException : DomainException
+    {
+        internal UserContactRequestNotFoundException(Guid senderUserId) 
+            : base(message: $"There is no contact request from User with ID #{senderUserId}!")
+        {
+        }
+    }
+}
