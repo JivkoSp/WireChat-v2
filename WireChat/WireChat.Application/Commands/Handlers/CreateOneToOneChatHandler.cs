2f0f457770dc15c208525abc4d66c1214a2d5db8
JivkoSp <atjspasov@gmail.com>
2023-09-16 09:07:36 +0300

    Added new command handler: WireChat/WireChat.Application/Commands/Handlers/CreateOneToOneChatHandler.cs

---
+++ WireChat/WireChat.Application/Commands/Handlers/CreateOneToOneChatHandler.cs
@@ -0,0 +1,64 @@
+using WireChat.Application.Exceptions;
+using WireChat.Application.Services.ReadServices;
+using WireChat.Domain.Factories.Interfaces;
+using WireChat.Domain.Repositories;
+using WireChat.Domain.ValueObjects;
+
+namespace WireChat.Application.Commands.Handlers
+{
+    internal sealed class CreateOneToOneChatHandler : ICommandHandler<CreateOneToOneChatCommand>
+    {
+        private readonly IChatRepository _chatRepository;
+        private readonly IChatFactory _chatFactory;
+        private readonly IUserReadService _userReadService;
+        private readonly IChatReadService _chatReadService;
+
+        public CreateOneToOneChatHandler(IChatRepository chatRepository, IChatFactory chatFactory, 
+            IUserReadService userReadService, IChatReadService chatReadService)
+        {
+            _chatRepository = chatRepository;
+            _chatFactory = chatFactory;
+            _userReadService = userReadService;
+            _chatReadService = chatReadService;
+        }
+
+        public async Task HandleAsync(CreateOneToOneChatCommand command)
+        {
+            var senderExists = await _userReadService.ExistsByIdAsync(command.SenderUserId);
+
+            if (senderExists is false)
+            {
+                throw new UserNotFoundException(command.SenderUserId);
+            }
+
+            var receiverExists = await _userReadService.ExistsByIdAsync(command.ReceiverUserId);
+
+            if (receiverExists is false)
+            {
+                throw new UserNotFoundException(command.ReceiverUserId);
+            }
+            
+            var chatId = Guid.NewGuid();
+
+            // There may be edge cases where the newly created chat has an ID that already exists in the database.
+            var chatExists = await _chatReadService.ExistsByIdAsync(chatId);
+
+            if (chatExists)
+            {
+                throw new ChatAlreadyExistsException(chatId);
+            }
+
+            var chat = _chatFactory.Create(chatId, "OneToOne");
+
+            var sender = new ChatUser(command.SenderUserId, chat.Id);
+
+            var receiver = new ChatUser(command.ReceiverUserId, chat.Id);
+
+            chat.AddChatUser(sender);
+
+            chat.AddChatUser(receiver);
+
+            await _chatRepository.AddChatAsync(chat);
+        }
+    }
+}
