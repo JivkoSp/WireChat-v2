7e7ab1954c3e8e080885e74da9f40e04606a367f
JivkoSp <atjspasov@gmail.com>
2023-09-17 17:40:03 +0300

    Added new IServiceCollection extension method in WireChat/WireChat.Application/Extensions/ServiceCollectionExtensions.cs. The method is: AddQueriesWithDispatcher.

--- WireChat/WireChat.Application/Extensions/ServiceCollectionExtensions.cs
+++ WireChat/WireChat.Application/Extensions/ServiceCollectionExtensions.cs
@@ -2,6 +2,8 @@ using System.Reflection;
 using Microsoft.Extensions.DependencyInjection;
 using WireChat.Application.Commands.Dispatcher;
 using WireChat.Application.Commands.Handlers;
+using WireChat.Application.Queries.Dispatcher;
+using WireChat.Application.Queries.Handlers;
 using WireChat.Domain.Factories;
 using WireChat.Domain.Factories.Interfaces;
 
@@ -39,6 +41,22 @@ namespace WireChat.Application.Extensions
             services.AddCommandsWithDispatcher();
 
             services.AddFactories();
+
+            return services;
+        }
+
+        public static IServiceCollection AddQueriesWithDispatcher(this IServiceCollection services)
+        {
+            var assembly = Assembly.GetCallingAssembly();
+
+            services.AddSingleton<IQueryDispatcher, InMemoryQueryDispatcher>();
+
+            //Adding automatic registration for all query handlers.
+            //Reason: There is possibility to forget to register query handler in the DI container.
+            //        Every time that new query handler is created it has to be registered in the DI container,
+            //        so in addition this eliminates duplicate work.
+            services.Scan(x => x.FromAssemblies(assembly).AddClasses(x =>
+                x.AssignableTo(typeof(IQueryHandler<,>))).AsImplementedInterfaces().WithScopedLifetime());
 
             return services;
         }
