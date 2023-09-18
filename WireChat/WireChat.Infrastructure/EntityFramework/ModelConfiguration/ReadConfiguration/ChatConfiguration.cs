1f73118be159effc20953f2768d33a5a8761761d
JivkoSp <atjspasov@gmail.com>
2023-09-18 12:16:23 +0300

    Added configuration for the read model: ChatReadModel in WireChat/WireChat.Infrastructure/EntityFramework/ModelConfiguration/ReadConfiguration/ChatConfiguration.cs.

---
+++ WireChat/WireChat.Infrastructure/EntityFramework/ModelConfiguration/ReadConfiguration/ChatConfiguration.cs
@@ -0,0 +1,32 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using WireChat.Infrastructure.EntityFramework.Encryption.EncryptionConverters;
+using WireChat.Infrastructure.EntityFramework.Encryption.EncryptionProvider;
+using WireChat.Infrastructure.EntityFramework.Models;
+
+namespace WireChat.Infrastructure.EntityFramework.ModelConfiguration
+{
+    internal sealed class ChatConfiguration : IEntityTypeConfiguration<ChatReadModel>
+    {
+        private readonly IEncryptionProvider _encryptionProvider;
+
+        public ChatConfiguration(IEncryptionProvider encryptionProvider)
+        {
+            _encryptionProvider = encryptionProvider;
+        }
+
+        public void Configure(EntityTypeBuilder<ChatReadModel> builder)
+        {
+            // Table name
+            builder.ToTable("Chat");
+
+            // Primary key
+            builder.HasKey(key => key.ChatId);
+
+            // Property config
+            builder.Property(p => p.ChatType)
+                .HasConversion(new EncryptedStringConverter(_encryptionProvider))
+                .IsRequired();
+        }
+    }
+}
