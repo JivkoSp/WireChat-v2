c626436604fd4fdefd6406773048741a48eb5c1b
JivkoSp <atjspasov@gmail.com>
2023-09-18 14:23:11 +0300

    Added configuration for the read model: UserReadModel in WireChat/WireChat.Infrastructure/EntityFramework/ModelConfiguration/ReadConfiguration/UserConfiguration.cs.

---
+++ WireChat/WireChat.Infrastructure/EntityFramework/ModelConfiguration/ReadConfiguration/UserConfiguration.cs
@@ -0,0 +1,47 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using WireChat.Infrastructure.EntityFramework.Encryption.EncryptionConverters;
+using WireChat.Infrastructure.EntityFramework.Encryption.EncryptionProvider;
+using WireChat.Infrastructure.EntityFramework.Models;
+
+namespace WireChat.Infrastructure.EntityFramework.ModelConfiguration
+{
+    internal sealed class UserConfiguration : IEntityTypeConfiguration<UserReadModel>
+    {
+        private readonly IEncryptionProvider _encryptionProvider;
+
+        public UserConfiguration(IEncryptionProvider encryptionProvider)
+        {
+            _encryptionProvider = encryptionProvider;
+        }
+
+        public void Configure(EntityTypeBuilder<UserReadModel> builder)
+        {
+            // Table name
+            builder.ToTable("User");
+
+            // Primary key
+            builder.HasKey(key => key.Id);
+
+            // Property config 
+            
+            builder.Property(p => p.UserFirstName)
+                .HasConversion(new EncryptedStringConverter(_encryptionProvider))
+                .IsRequired();
+            
+            builder.Property(p => p.UserLastName)
+                .HasConversion(new EncryptedStringConverter(_encryptionProvider))
+                .IsRequired();
+
+            builder.Property(p => p.Email)
+                .HasConversion(new EncryptedStringConverter(_encryptionProvider))
+                .IsRequired();
+
+            builder.Property(p => p.NormalizedEmail)
+                .HasConversion(new EncryptedStringConverter(_encryptionProvider));
+            
+            builder.Property(p => p.PhoneNumber)
+                .HasConversion(new EncryptedStringConverter(_encryptionProvider));
+        }
+    }
+}
