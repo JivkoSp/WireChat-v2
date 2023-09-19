3fb7cb2319182e3b070d2a4860a0563553f467d7
JivkoSp <atjspasov@gmail.com>
2023-09-19 11:47:15 +0300

    Added EntityFramework configuration for the ChatUser value object in WireChat.Infrastructure/EntityFramework/ModelConfiguration/WriteConfiguration/ChatUserConfiguration.cs.

---
+++ WireChat/WireChat.Infrastructure/EntityFramework/ModelConfiguration/WriteConfiguration/ChatUserConfiguration.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using WireChat.Domain.ValueObjects;
+
+namespace WireChat.Infrastructure.EntityFramework.ModelConfiguration.WriteConfiguration
+{
+    internal sealed class ChatUserConfiguration : IEntityTypeConfiguration<ChatUser>
+    {
+        public void Configure(EntityTypeBuilder<ChatUser> builder)
+        {
+            // Table name
+            builder.ToTable("ChatUser");
+
+            // Composite primary key
+            builder.HasKey(key => new { key.UserID, key.ChatID });
+
+            // Property config - Start
+
+            builder.Property(p => p.UserID)
+                .HasConversion(id => id.Value.ToString(), id => new UserID(Guid.Parse(id)))
+                .HasColumnName("UserId")
+                .IsRequired();
+
+            builder.Property(p => p.ChatID)
+                .HasConversion(id => id.Value.ToString(), id => new ChatID(Guid.Parse(id)))
+                .HasColumnName("ChatId")
+                .IsRequired();
+
+            // Property config - End
+        }
+    }
+}
