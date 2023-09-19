929bbd601df34ef28f31ff1234c9bb2ad17e24a3
JivkoSp <atjspasov@gmail.com>
2023-09-19 20:49:12 +0300

    Added EntityFramework Db context for the write side: WireChat/WireChat.Infrastructure/EntityFramework/Contexts/WriteDbContext.cs

---
+++ WireChat/WireChat.Infrastructure/EntityFramework/Contexts/WriteDbContext.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using WireChat.Infrastructure.EntityFramework.Encryption.EncryptionProvider;
+using WireChat.Infrastructure.EntityFramework.ModelConfiguration.WriteConfiguration;
+
+namespace WireChat.Infrastructure.EntityFramework.Contexts
+{
+    internal sealed class WriteDbContext : DbContext
+    {
+        private readonly ILoggerFactory _loggerFactory;
+        private readonly IEncryptionProvider _encryptionProvider;
+
+        public WriteDbContext(DbContextOptions<WriteDbContext> options, ILoggerFactory loggerFactory,
+            IEncryptionProvider encryptionProvider) : base(options)
+        {
+            _loggerFactory = loggerFactory;
+            _encryptionProvider = encryptionProvider;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseLoggerFactory(_loggerFactory); // Enable logging
+                optionsBuilder.EnableSensitiveDataLogging(); // Include parameter values in logs
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasDefaultSchema("wirechat");
+
+            base.OnModelCreating(modelBuilder);
+
+            //Applying configurations for the entity models
+            modelBuilder.ApplyConfiguration(new ChatConfiguration(_encryptionProvider));
+            modelBuilder.ApplyConfiguration(new ChatMessageConfiguration(_encryptionProvider));
+            modelBuilder.ApplyConfiguration(new ChatUserConfiguration());
+            modelBuilder.ApplyConfiguration(new UserConfiguration(_encryptionProvider));
+            modelBuilder.ApplyConfiguration(new UserContactRequestConfiguration(_encryptionProvider));
+        }
+    }
+}
