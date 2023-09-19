eb975964b2c83133dc8fe20accda250af2a08ee7
JivkoSp <atjspasov@gmail.com>
2023-09-19 18:36:12 +0300

    Added EntityFramework Db context for the read side: WireChat/WireChat.Infrastructure/EntityFramework/Contexts/ReadDbContext.cs

---
+++ WireChat/WireChat.Infrastructure/EntityFramework/Contexts/ReadDbContext.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using WireChat.Infrastructure.EntityFramework.Encryption.EncryptionProvider;
+using WireChat.Infrastructure.EntityFramework.ModelConfiguration.ReadConfiguration;
+using WireChat.Infrastructure.EntityFramework.Models;
+
+namespace WireChat.Infrastructure.EntityFramework.Contexts
+{
+    internal sealed class ReadDbContext : IdentityDbContext<UserReadModel>
+    {
+        private readonly IEncryptionProvider _encryptionProvider;
+
+        public ReadDbContext(DbContextOptions<ReadDbContext> options, IEncryptionProvider encryptionProvider) : base(options)
+        {
+            _encryptionProvider = encryptionProvider;
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasDefaultSchema("wirechat");
+
+            base.OnModelCreating(modelBuilder);
+
+            //Applying configurations for the entity models
+            modelBuilder.ApplyConfiguration(new ChatConfiguration(_encryptionProvider));
+            modelBuilder.ApplyConfiguration(new ChatMessageConfiguration(_encryptionProvider));
+            modelBuilder.ApplyConfiguration(new ChatUserConfiguration());
+            modelBuilder.ApplyConfiguration(new UserConfiguration(_encryptionProvider));
+            modelBuilder.ApplyConfiguration(new UserContactRequestConfiguration(_encryptionProvider));
+        }
+    }
+}
