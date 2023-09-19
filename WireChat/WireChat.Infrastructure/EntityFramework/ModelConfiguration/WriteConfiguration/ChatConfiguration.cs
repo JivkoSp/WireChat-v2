f61a73449d4bf0bec4cf0bb601d024f50986e86e
JivkoSp <atjspasov@gmail.com>
2023-09-19 10:20:11 +0300

    Added EntityFramework configuration for the Chat domain entity in WireChat.Infrastructure/EntityFramework/ModelConfiguration/WriteConfiguration/ChatConfiguration.cs

---
+++ WireChat/WireChat.Infrastructure/EntityFramework/ModelConfiguration/WriteConfiguration/ChatConfiguration.cs
@@ -0,0 +1,45 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using WireChat.Domain.Entities;
+using WireChat.Domain.ValueObjects;
+using WireChat.Infrastructure.EntityFramework.Encryption.EncryptionConverters;
+using WireChat.Infrastructure.EntityFramework.Encryption.EncryptionProvider;
+
+namespace WireChat.Infrastructure.EntityFramework.ModelConfiguration.WriteConfiguration
+{
+    internal sealed class ChatConfiguration : IEntityTypeConfiguration<Chat>
+    {
+        private readonly IEncryptionProvider _encryptionProvider;
+
+        public ChatConfiguration(IEncryptionProvider encryptionProvider)
+        {
+            _encryptionProvider = encryptionProvider;
+        }
+
+        public void Configure(EntityTypeBuilder<Chat> builder)
+        {
+            // Table name
+            builder.ToTable("Chat");
+
+            // Primary key
+            builder.HasKey(key => key.Id);
+
+            // Property config
+            builder.Property(p => p.Id)
+                .HasConversion(id => id.Value, id => new ChatID(id))
+                .HasColumnName("ChatId")
+                .IsRequired();
+
+            builder.Property(typeof(ChatType), "_chatType")
+                .HasConversion(new EncryptedChatTypeConverter(_encryptionProvider))
+                .HasColumnName("ChatType")
+                .IsRequired();
+
+            // Relationships
+            builder.HasMany(p => p.Users);
+
+            builder.HasMany(p => p.Messages);
+        }
+    }
+}
