caa4537b758552a0c33e7659a9416ee8683bcf59
JivkoSp <atjspasov@gmail.com>
2023-09-19 16:18:22 +0300

    Added EntityFramework configuration for the UserContactRequest value object in WireChat/WireChat.Infrastructure/EntityFramework/ModelConfiguration/WriteConfiguration/UserContactRequestConfiguration.cs.

---
+++ WireChat/WireChat.Infrastructure/EntityFramework/ModelConfiguration/WriteConfiguration/UserContactRequestConfiguration.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using WireChat.Domain.ValueObjects;
+using WireChat.Infrastructure.EntityFramework.Encryption.EncryptionConverters;
+using WireChat.Infrastructure.EntityFramework.Encryption.EncryptionProvider;
+
+namespace WireChat.Infrastructure.EntityFramework.ModelConfiguration.WriteConfiguration
+{
+    internal sealed class UserContactRequestConfiguration : IEntityTypeConfiguration<UserContactRequest>
+    {
+        private readonly IEncryptionProvider _encryptionProvider;
+
+        public UserContactRequestConfiguration(IEncryptionProvider encryptionProvider)
+        {
+            _encryptionProvider = encryptionProvider;
+        }
+
+        public void Configure(EntityTypeBuilder<UserContactRequest> builder)
+        {
+            // Table name
+            builder.ToTable("UserContactRequest");
+
+            // Composite primary key
+            builder.HasKey(key => new { key.SenderUserId, key.ReceiverUserId });
+
+            // Property config
+            builder.Property(typeof(string), "Message")
+                .HasConversion(new EncryptedStringConverter(_encryptionProvider));
+        }
+    }
+}
