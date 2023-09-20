d209b8c5e069c7ff5cedcb2fee0313fe4184f6ba
JivkoSp <atjspasov@gmail.com>
2023-09-20 15:42:32 +0300

    Added write repository: WireChat/WireChat.Infrastructure/EntityFramework/Repositories/PostgresChatRepository.cs

---
+++ WireChat/WireChat.Infrastructure/EntityFramework/Repositories/PostgresChatRepository.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WireChat.Domain.Entities;
+using WireChat.Domain.Repositories;
+using WireChat.Domain.ValueObjects;
+using WireChat.Infrastructure.EntityFramework.Contexts;
+
+namespace WireChat.Infrastructure.EntityFramework.Repositories
+{
+    internal sealed class PostgresChatRepository : IChatRepository
+    {
+        private readonly WriteDbContext _writeDbContext;
+
+        public PostgresChatRepository(WriteDbContext writeDbContext)
+        {
+            _writeDbContext = writeDbContext;
+        }
+
+        public Task<Chat> GetChatByIdAsync(ChatID chatId)
+            => _writeDbContext.Chats
+                .Include(x => x.Users)
+                .Include(x => x.Messages)
+                .SingleOrDefaultAsync(x => x.Id == chatId);
+
+        public async Task AddChatAsync(Chat chat)
+        {
+            await _writeDbContext.Chats.AddAsync(chat);
+
+            await _writeDbContext.SaveChangesAsync();
+        }
+
+        public async Task UpdateChatAsync(Chat chat)
+        {
+            _writeDbContext.Chats.Update(chat);
+
+            await _writeDbContext.SaveChangesAsync();
+        }
+
+        public async Task DeleteChatAsync(Chat chat)
+        {
+            _writeDbContext.Chats.Remove(chat);
+
+            await _writeDbContext.SaveChangesAsync();
+        }
+    }
+}
