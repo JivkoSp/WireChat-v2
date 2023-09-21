8284645e18bd052c75c4fc39c0c103ecb3389f1d
JivkoSp <atjspasov@gmail.com>
2023-09-21 18:39:12 +0300

    Changed the type of the UserId property in WireChat/WireChat.Infrastructure/EntityFramework/Models/ChatMessageReadModel.cs from Guid to string.

--- WireChat/WireChat.Infrastructure/EntityFramework/Models/ChatMessageReadModel.cs
+++ WireChat/WireChat.Infrastructure/EntityFramework/Models/ChatMessageReadModel.cs
@@ -9,7 +9,7 @@ namespace WireChat.Infrastructure.EntityFramework.Models
 
         // Relationships
         public Guid ChatId { get; set; } // Foreign key to the Chat table
-        public Guid UserId { get; set; } // Foreign key to the User table
+        public string UserId { get; set; } // Foreign key to the User table
         public virtual ChatReadModel Chat { get; set; }
         public virtual UserReadModel User { get; set; }
     }
