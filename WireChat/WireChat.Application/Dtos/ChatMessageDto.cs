940b8a0fa0b65a2025ee7aaa7d70c6b012451d7d
JivkoSp <atjspasov@gmail.com>
2023-09-21 16:37:19 +0300

    Changed the type of the UserId property in WireChat/WireChat.Application/Dtos/ChatMessageDto.cs from Guid to string.

--- WireChat/WireChat.Application/Dtos/ChatMessageDto.cs
+++ WireChat/WireChat.Application/Dtos/ChatMessageDto.cs
@@ -3,7 +3,7 @@ namespace WireChat.Application.Dtos
     public class ChatMessageDto
     {
         public Guid ChatMessageId { get; set; }
-        public Guid UserId { get; set; }
+        public string UserId { get; set; }
         public string Message { get; set; }
         public DateTimeOffset MessageDateTime { get; set; }
     }
