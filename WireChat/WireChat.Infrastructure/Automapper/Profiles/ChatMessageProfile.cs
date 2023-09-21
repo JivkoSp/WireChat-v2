7ed889189003fd3164e0fdb9aae02d52c635c41d
JivkoSp <atjspasov@gmail.com>
2023-09-21 15:10:43 +0300

    Added automapper profile type: WireChat/WireChat.Infrastructure/Automapper/Profiles/ChatMessageProfile.cs

---
+++ WireChat/WireChat.Infrastructure/Automapper/Profiles/ChatMessageProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using WireChat.Application.Dtos;
+using WireChat.Infrastructure.EntityFramework.Models;
+
+namespace WireChat.Infrastructure.Automapper.Profiles
+{
+    internal sealed class ChatMessageProfile : Profile
+    {
+        public ChatMessageProfile()
+        {
+            CreateMap<ChatMessageReadModel, ChatMessageDto>();
+        }
+    }
+}
