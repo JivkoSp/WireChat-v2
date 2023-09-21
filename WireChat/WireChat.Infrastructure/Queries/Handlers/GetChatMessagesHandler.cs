7b863c3a25881356716e054fe2bb6cde7e5b958b
JivkoSp <atjspasov@gmail.com>
2023-09-21 16:18:23 +0300

    Added new query handler: WireChat/WireChat.Infrastructure/Queries/Handlers/GetChatMessagesHandler.cs

---
+++ WireChat/WireChat.Infrastructure/Queries/Handlers/GetChatMessagesHandler.cs
@@ -0,0 +1,32 @@
+﻿using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+using WireChat.Application.Dtos;
+using WireChat.Application.Queries;
+using WireChat.Application.Queries.Handlers;
+using WireChat.Infrastructure.EntityFramework.Contexts;
+
+namespace WireChat.Infrastructure.Queries.Handlers
+{
+    internal sealed class GetChatMessagesHandler : IQueryHandler<GetChatMessagesQuery, List<ChatMessageDto>>
+    {
+        private readonly ReadDbContext _readDbContext;
+        private readonly IMapper _mapper;
+
+        public GetChatMessagesHandler(ReadDbContext readDbContext, IMapper mapper)
+        {
+            _readDbContext = readDbContext;
+            _mapper = mapper;
+        }
+
+        public async Task<List<ChatMessageDto>> HandleAsync(GetChatMessagesQuery query)
+        {
+            var chatMessageReadModels = await _readDbContext.ChatMessageReadModels
+                .Where(x => x.ChatMessageId == query.ChatId)
+                .Include(x => x.User)
+                .AsNoTracking()
+                .ToListAsync();
+
+            return _mapper.Map<List<ChatMessageDto>>(chatMessageReadModels);
+        }
+    }
+}
