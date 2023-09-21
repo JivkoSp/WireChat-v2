b82b97b7c74d19c4032abf8c8895bc76b3f71e1a
JivkoSp <atjspasov@gmail.com>
2023-09-21 11:23:08 +0300

    Added ChatId parameter of type Guid to the WireChat/WireChat.Application/Queries/GetChatMessagesQuery.cs

--- WireChat/WireChat.Application/Queries/GetChatMessagesQuery.cs
+++ WireChat/WireChat.Application/Queries/GetChatMessagesQuery.cs
@@ -2,5 +2,5 @@ using WireChat.Application.Dtos;
 
 namespace WireChat.Application.Queries
 {
-    public record GetChatMessagesQuery : IQuery<List<ChatMessageDto>>;
+    public record GetChatMessagesQuery(Guid ChatId) : IQuery<List<ChatMessageDto>>;
 }
