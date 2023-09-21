0a78db68b5d1f157b81ff66b8ebe9e0b4f464d2a
JivkoSp <atjspasov@gmail.com>
2023-09-21 14:21:49 +0300

    Added UserId parameter of type Guid to the WireChat/WireChat.Application/Queries/GetOneToOneChatsQuery.cs

--- WireChat/WireChat.Application/Queries/GetOneToOneChatsQuery.cs
+++ WireChat/WireChat.Application/Queries/GetOneToOneChatsQuery.cs
@@ -2,5 +2,5 @@ using WireChat.Application.Dtos;
 
 namespace WireChat.Application.Queries
 {
-    public record GetOneToOneChatsQuery : IQuery<List<ChatDto>>;
+    public record GetOneToOneChatsQuery(Guid UserId) : IQuery<List<ChatDto>>;
 }
