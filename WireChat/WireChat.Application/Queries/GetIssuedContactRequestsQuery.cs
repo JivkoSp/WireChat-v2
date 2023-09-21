5786ca79c17a20cdd01842189607630cb792813e
JivkoSp <atjspasov@gmail.com>
2023-09-21 21:37:18 +0300

    Changed the UserId argument type of the WireChat/WireChat.Application/Queries/GetIssuedContactRequestsQuery.cs from Guid to string.

--- WireChat/WireChat.Application/Queries/GetIssuedContactRequestsQuery.cs
+++ WireChat/WireChat.Application/Queries/GetIssuedContactRequestsQuery.cs
@@ -2,5 +2,5 @@ using WireChat.Application.Dtos;
 
 namespace WireChat.Application.Queries
 {
-    public record GetIssuedContactRequestsQuery(Guid UserId) : IQuery<List<UserContactRequestDto>>;
+    public record GetIssuedContactRequestsQuery(string UserId) : IQuery<List<UserContactRequestDto>>;
 }
