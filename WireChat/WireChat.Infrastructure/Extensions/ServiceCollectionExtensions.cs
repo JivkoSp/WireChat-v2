c6b4fda6368fd45b69bc39d23dd7913327b7b4b0
JivkoSp <atjspasov@gmail.com>
2023-09-22 16:02:54 +0300

    Added two classes: WireChat/WireChat.Infrastructure/Extensions/ConfigurationExtensions.cs and WireChat/WireChat.Infrastructure/Extensions/ServiceCollectionExtensions.cs that contain IServiceCollection and IConfiguration extension methods.

---
+++ WireChat/WireChat.Infrastructure/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using WireChat.Application.Services.ReadServices;
+using WireChat.Domain.Repositories;
+using WireChat.Application.Extensions;
+using WireChat.Infrastructure.EntityFramework.Contexts;
+using WireChat.Infrastructure.EntityFramework.Encryption.EncryptionProvider;
+using WireChat.Infrastructure.EntityFramework.Initialization;
+using WireChat.Infrastructure.EntityFramework.Options;
+using WireChat.Infrastructure.EntityFramework.Repositories;
+using WireChat.Infrastructure.EntityFramework.Services.ReadServices;
+using WireChat.Infrastructure.Automapper.Profiles;
+
+namespace WireChat.Infrastructure.Extensions
+{
+    internal static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+        {
+            var postgresOptions = configuration.GetOptions<PostgresOptions>("Postgres");
+
+            services.AddSingleton<IEncryptionProvider>(new EncryptionProvider("A1B2C3D4E5F60789"));
+
+            services.AddDbContext<ReadDbContext>(options => options.UseNpgsql(postgresOptions.ConnectionString));
+
+            services.AddDbContext<WriteDbContext>(options => options.UseNpgsql(postgresOptions.ConnectionString));
+
+            services.AddHostedService<DbInitializer>();
+
+            services.AddScoped<IChatMessageRepository, PostgresChatMessageRepository>();
+
+            services.AddScoped<IChatRepository, PostgresChatRepository>();
+
+            services.AddScoped<IUserRepository, PostgresUserRepository>();
+
+            services.AddScoped<IChatReadService, PostgresChatReadService>();
+
+            services.AddScoped<IUserReadService, PostgresUserReadService>();
+
+            services.AddQueriesWithDispatcher();
+
+            services.AddAutoMapper(configAction => {
+                configAction.AddProfile<ChatMessageProfile>();
+                configAction.AddProfile<ChatProfile>();
+                configAction.AddProfile<ChatUserProfile>();
+            });
+
+            return services;
+        }
+    }
+}
