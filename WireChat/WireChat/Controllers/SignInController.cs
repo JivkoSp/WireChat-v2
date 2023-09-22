ce49e79e593f5e84e810441bad2ac44ab55b65ef
JivkoSp <atjspasov@gmail.com>
2023-09-22 21:15:19 +0300

    Modified the WireChat/Controllers/SignInController.cs. The modification are: 1 - Added UserName, Password and RememberMe properties; 2 - Added SignIn method.

--- WireChat/WireChat/Controllers/SignInController.cs
+++ WireChat/WireChat/Controllers/SignInController.cs
@@ -1,12 +1,53 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+using WireChat.Infrastructure.EntityFramework.Models;
 
 namespace WireChat.Controllers
 {
     public class SignInController : Controller
     {
+        private readonly SignInManager<UserReadModel> _signInManager;
+
+        [BindProperty]
+        [Required(ErrorMessage = "Incorrect username or password.")]
+        public string UserName { get; set; }
+
+        [BindProperty]
+        [Required(ErrorMessage = "Incorrect username or password.")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [BindProperty]
+        public bool RememberMe { get; set; }
+
+
+        public SignInController(SignInManager<UserReadModel> signInManager)
+        {
+            _signInManager = signInManager;
+        }
+
         public IActionResult Index()
         {
             return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> SignIn()
+        {
+            if (ModelState.IsValid)
+            {
+                var result = await _signInManager.PasswordSignInAsync(UserName, Password, RememberMe, false);
+
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index", "Chat");
+                }
+
+                // TODO: Add logic for two factor authentication 
+            }
+
+            return View("Index");
         }
     }
 }
