765a7513409e1650e842a3f73c3ee02eec1e13d9
JivkoSp <atjspasov@gmail.com>
2023-09-22 14:23:21 +0300

    Added new query handler: WireChat/WireChat.Infrastructure/Queries/Handlers/GetReceivedContactRequestsHandler.cs.

---
+++ WireChat/WireChat.Infrastructure/Queries/Handlers/GetReceivedContactRequestsHandler.cs
@@ -0,0 +1,32 @@
+﻿using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+using WireChat.Application.Dtos;
+using WireChat.Application.Queries;
+using WireChat.Application.Queries.Handlers;
+using WireChat.Infrastructure.EntityFramework.Contexts;
+
+namespace WireChat.Infrastructure.Queries.Handlers
+{
+    internal sealed class GetReceivedContactRequestsHandler 
+        : IQueryHandler<GetReceivedContactRequestsQuery, List<UserContactRequestDto>>
+    {
+        private readonly ReadDbContext _readDbContext;
+        private readonly IMapper _mapper;
+
+        public GetReceivedContactRequestsHandler(ReadDbContext readDbContext, IMapper mapper)
+        {
+            _readDbContext = readDbContext;
+            _mapper = mapper;
+        }
+
+        public async Task<List<UserContactRequestDto>> HandleAsync(GetReceivedContactRequestsQuery query)
+        {
+            var userContactRequestReadModels = await _readDbContext.UserContactRequestReadModels
+                .Where(x => x.ReceiverUserId == query.UserId)
+                .AsNoTracking()
+                .ToListAsync();
+
+            return _mapper.Map<List<UserContactRequestDto>>(userContactRequestReadModels);
+        }
+    }
+}
