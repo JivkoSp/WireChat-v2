1a400bbc2bbb0341e5552ad8b04566445777addd
JivkoSp <atjspasov@gmail.com>
2023-09-22 10:43:01 +0300

    Changed the types of the properties SenderUserId and ReceiverUserId, defined in WireChat/WireChat.Application/Dtos/UserContactRequestDto.cs from Guid to string.

--- WireChat/WireChat.Application/Dtos/UserContactRequestDto.cs
+++ WireChat/WireChat.Application/Dtos/UserContactRequestDto.cs
@@ -2,8 +2,8 @@ namespace WireChat.Application.Dtos
 {
     public class UserContactRequestDto
     {
-        public Guid SenderUserId { get; set; }
-        public Guid ReceiverUserId { get; set; }
+        public string SenderUserId { get; set; }
+        public string ReceiverUserId { get; set; }
         public string Message { get; set; }
     }
 }
