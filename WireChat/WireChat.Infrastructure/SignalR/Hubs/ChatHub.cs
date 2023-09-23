c0751457e514b2fddd0a0d6952618b1150d33c3c
JivkoSp <atjspasov@gmail.com>
2023-09-23 15:53:14 +0300

    Added SignalR Hub: WireChat.Infrastructure/SignalR/Hubs/ChatHub.cs

---
+++ WireChat/WireChat.Infrastructure/SignalR/Hubs/ChatHub.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using WireChat.Infrastructure.Dtos.SignalR;
+
+namespace WireChat.Infrastructure.SignalR.Hubs
+{
+    public sealed class ChatHub : Hub
+    {
+        public async Task JoinGroup(string group)
+        {
+            await Groups.AddToGroupAsync(Context.ConnectionId, group);
+        }
+
+        public async Task SendMessage(SignalRChatMessageDto chatMessage)
+        {
+            await Clients.Group(chatMessage.ChatId.ToString()).SendAsync("SendMessage", chatMessage);
+        }
+
+        public async Task RemoveMessage(SignalRChatMessageDto chatMessage)
+        {
+            await Clients.Group(chatMessage.ChatId.ToString()).SendAsync("RemoveMessage", chatMessage);
+        }
+    }
+}
