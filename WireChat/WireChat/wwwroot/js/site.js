3908b213c13bf35cedb19386640dccb070ce6aeb
JivkoSp <atjspasov@gmail.com>
2023-09-23 15:10:27 +0300

    Added getCurrentDateTimeOffset function in WireChat/wwwroot/js/site.js

--- WireChat/WireChat/wwwroot/js/site.js
+++ WireChat/WireChat/wwwroot/js/site.js
@@ -1,4 +1,14 @@
-﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿// Get the current DateTimeOffset string
+function getCurrentDateTimeOffset() {
+    const now = new Date();
+    const offset = -now.getTimezoneOffset();
+    const offsetHours = Math.floor(Math.abs(offset) / 60);
+    const offsetMinutes = Math.abs(offset) % 60;
+    const offsetSign = offset >= 0 ? '+' : '-';
+    const pad = (num) => num.toString().padStart(2, '0');
 
-// Write your JavaScript code.
+    const formattedOffset = `${offsetSign}${pad(offsetHours)}:${pad(offsetMinutes)}`;
+    const dateTimeOffset = `${now.toISOString().split('.')[0]}${formattedOffset}`;
+
+    return dateTimeOffset;
+}
