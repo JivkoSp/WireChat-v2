c618610a87c847869cf1f13eef7e452ed71649d8
JivkoSp <atjspasov@gmail.com>
2023-09-23 19:02:10 +0300

    Added SignalR specific dtos in  WireChat.Infrastructure/Dtos/SignalR/

---
+++ WireChat/WireChat.Infrastructure/Dtos/SignalR/SignalRChatMessageDto.cs
@@ -0,0 +1,12 @@
+﻿
+namespace WireChat.Infrastructure.Dtos.SignalR
+{
+    public class SignalRChatMessageDto
+    {
+        public Guid ChatId { get; set; }
+        public string UserName { get; set; }
+        public string Message { get; set; }
+        public DateTimeOffset MessageDateTime { get; set; }
+        public bool IsFromCurrentUser { get; set; } = false;
+    }
+}
---
+++ WireChat/WireChat.Infrastructure/Dtos/SignalR/SignalRContactRequestDto.cs
@@ -0,0 +1,10 @@
+﻿
+namespace WireChat.Infrastructure.Dtos.SignalR
+{
+    public class SignalRContactRequestDto
+    {
+        public string ContactName { get; set; }
+        public string ContactMessage { get; set; }
+        public DateTimeOffset DateTime { get; set; }
+    }
+}
