e65921badb0277f1829ca4cca2ccca5bb8b6bba7
JivkoSp <atjspasov@gmail.com>
2023-09-24 21:43:11 +0300

    Added view components: WireChat/Components/GroupsSideMenuViewComponent.cs and WireChat/Components/SideMenuViewComponent.cs.

---
+++ WireChat/WireChat/Components/GroupsSideMenuViewComponent.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using WireChat.Application.Queries;
+using WireChat.Application.Queries.Dispatcher;
+using WireChat.Infrastructure.EntityFramework.Models;
+
+namespace WireChat.Components
+{
+    [Authorize]
+    public class GroupsSideMenuViewComponent : ViewComponent
+    {
+        private readonly IQueryDispatcher _queryDispatcher;
+        private readonly UserManager<UserReadModel> _userManager;
+
+        public GroupsSideMenuViewComponent(IQueryDispatcher queryDispatcher, UserManager<UserReadModel> userManager)
+        {
+            _queryDispatcher = queryDispatcher;
+            _userManager = userManager;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var user = await _userManager.FindByNameAsync(User.Identity.Name);
+
+            var getGroupsQuery = new GetGroupsQuery(user.Id);
+
+            var groups = await _queryDispatcher.DispatchAsync(getGroupsQuery);
+
+            return View(groups);
+        }
+    }
+}
---
+++ WireChat/WireChat/Components/SideMenuViewComponent.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using WireChat.Application.Queries;
+using WireChat.Application.Queries.Dispatcher;
+using WireChat.Infrastructure.EntityFramework.Models;
+
+namespace WireChat.Components
+{
+    [Authorize]
+    public class SideMenuViewComponent : ViewComponent
+    {
+        private readonly IQueryDispatcher _queryDispatcher;
+        private readonly UserManager<UserReadModel> _userManager;
+
+        public SideMenuViewComponent(IQueryDispatcher queryDispatcher, UserManager<UserReadModel> userManager)
+        {
+            _queryDispatcher = queryDispatcher;
+            _userManager = userManager;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var user = await _userManager.FindByNameAsync(User.Identity.Name);
+
+            var getContactsQuery = new GetContactsQuery(user.Id);
+
+            var contacts = await _queryDispatcher.DispatchAsync(getContactsQuery);
+
+            return View(contacts);
+        }
+    }
+}
