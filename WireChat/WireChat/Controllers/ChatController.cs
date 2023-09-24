da684c194d34378ccc2a7f0494a085739bd850d0
JivkoSp <atjspasov@gmail.com>
2023-09-24 17:21:43 +0300

    Added Index and PostMessage methods in WireChat/Controllers/ChatController.cs

--- WireChat/WireChat/Controllers/ChatController.cs
+++ WireChat/WireChat/Controllers/ChatController.cs
@@ -1,12 +1,48 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+using WireChat.Application.Commands;
+using WireChat.Application.Commands.Dispatcher;
+using WireChat.Application.Dtos;
+using WireChat.Application.Queries;
+using WireChat.Application.Queries.Dispatcher;
+using WireChat.ViewModels;
 
 namespace WireChat.Controllers
 {
     public class ChatController : Controller
     {
-        public IActionResult Index()
+        private readonly ICommandDispatcher _commandDispatcher;
+        private readonly IQueryDispatcher _queryDispatcher;
+
+        public ChatController(ICommandDispatcher commandDispatcher, IQueryDispatcher queryDispatcher)
         {
-            return View();
+            _commandDispatcher = commandDispatcher;
+            _queryDispatcher = queryDispatcher;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Index(Guid chatId)
+        {
+            var getChatQuery = new GetChatQuery(chatId);
+
+            var chatDto = await _queryDispatcher.DispatchAsync(getChatQuery);
+
+            var chatViewModel = new ChatViewModel
+            {
+                CurrentUserId = User.FindFirstValue(ClaimTypes.NameIdentifier),
+                ChatDto = chatDto,
+            };
+
+            return View(chatViewModel);
+        }
+
+        [HttpPost]
+        public async Task PostMessage(Guid chatId, string message, DateTimeOffset dateTime)
+        {
+            var addChatMessageCommand = new AddChatMessageCommand(chatId, Guid.NewGuid(), 
+                Guid.Parse(User.FindFirstValue(ClaimTypes.NameIdentifier)), message, dateTime);
+
+            await _commandDispatcher.DispatchAsync(addChatMessageCommand);
         }
     }
 }
