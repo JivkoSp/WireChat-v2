441505204e26be7e4259d49c09033344d9eaec28
JivkoSp <atjspasov@gmail.com>
2023-09-24 11:32:49 +0300

    Added new SignalR dto: WireChat.Infrastructure/Dtos/SignalR/SignalRRemoveContactRequestDto.cs

---
+++ WireChat/WireChat.Infrastructure/Dtos/SignalR/SignalRRemoveContactRequestDto.cs
@@ -0,0 +1,9 @@
+﻿
+namespace WireChat.Infrastructure.Dtos.SignalR
+{
+    public class SignalRRemoveContactRequestDto
+    {
+        public string SenderUserName { get; set; }
+        public string ReceiverUserName { get; set; }
+    }
+}
