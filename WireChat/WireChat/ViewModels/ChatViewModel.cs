66dae1752669847171341d323a1149e752ece273
JivkoSp <atjspasov@gmail.com>
2023-09-25 14:08:24 +0300

    Added view models:  WireChat/ViewModels/ChatViewModel.cs and WireChat/ViewModels/ContactRequestViewModel.cs

---
+++ WireChat/WireChat/ViewModels/ChatViewModel.cs
@@ -0,0 +1,10 @@
+﻿using WireChat.Application.Dtos;
+
+namespace WireChat.ViewModels
+{
+    public class ChatViewModel
+    {
+        public string CurrentUserId { get; set; }
+        public ChatDto ChatDto { get; set; }
+    }
+}
---
+++ WireChat/WireChat/ViewModels/ContactRequestViewModel.cs
@@ -0,0 +1,10 @@
+﻿using WireChat.Application.Dtos;
+
+namespace WireChat.ViewModels
+{
+    public class ContactRequestViewModel
+    {
+        public List<UserContactRequestDto> IssuedContactRequests { get; set; } = new List<UserContactRequestDto>();
+        public List<UserContactRequestDto> ReceivedContactRequests { get; set; } = new List<UserContactRequestDto>();
+    }
+}
