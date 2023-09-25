e6ef9a36c6ca287da72dee51b48f257488e15da3
JivkoSp <atjspasov@gmail.com>
2023-09-25 12:38:23 +0300

    Added new controller: WireChat/Controllers/GroupController.cs

---
+++ WireChat/WireChat/Controllers/GroupController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+using WireChat.Application.Commands.Dispatcher;
+using WireChat.Application.Commands;
+
+namespace WireChat.Controllers
+{
+    public class GroupController : Controller
+    {
+        private readonly ICommandDispatcher _commandDispatcher;
+
+        public GroupController(ICommandDispatcher commandDispatcher)
+        {
+            _commandDispatcher = commandDispatcher;
+        }
+
+        [HttpPost]
+        public async Task AddCroup(string groupName)
+        {
+            var userId = Guid.Parse(User.FindFirstValue(ClaimTypes.NameIdentifier));
+
+            var createGroupChatCommand = new CreateGroupChatCommand(userId, groupName);
+
+            await _commandDispatcher.DispatchAsync(createGroupChatCommand);
+        }
+    }
+}
