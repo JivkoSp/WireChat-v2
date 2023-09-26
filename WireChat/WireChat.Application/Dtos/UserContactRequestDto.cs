3da3f5f463aa1fa8bcc59b6e917bc92b7ebe6474
JivkoSp <atjspasov@gmail.com>
2023-09-26 15:48:11 +0300

    Modified the WireChat.Application/Dtos/UserContactRequestDto.cs. The modifications are: Replaced the SenderUserId and ReceiverUserId with SenderUserName and ReceiverUserName.

--- WireChat/WireChat.Application/Dtos/UserContactRequestDto.cs
+++ WireChat/WireChat.Application/Dtos/UserContactRequestDto.cs
@@ -2,8 +2,8 @@ namespace WireChat.Application.Dtos
 {
     public class UserContactRequestDto
     {
-        public string SenderUserId { get; set; }
-        public string ReceiverUserId { get; set; }
+        public string SenderUserName { get; set; }
+        public string ReceiverUserName { get; set; }
         public string Message { get; set; }
     }
 }
