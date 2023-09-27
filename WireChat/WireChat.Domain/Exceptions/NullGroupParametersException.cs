acc1fdae906afe4ea1511cdbafd0f5e72ec0e88c
JivkoSp <atjspasov@gmail.com>
2023-09-27 11:36:03 +0300

    Added new custom domain layer exception type: WireChat.Domain/Exceptions/NullGroupParametersException.cs

---
+++ WireChat/WireChat.Domain/Exceptions/NullGroupParametersException.cs
@@ -0,0 +1,10 @@
+﻿
+namespace WireChat.Domain.Exceptions
+{
+    public sealed class NullGroupParametersException : DomainException
+    {
+        internal NullGroupParametersException() : base(message: "Group cannot be initialized with one or more null parameters!")
+        {
+        }
+    }
+}
