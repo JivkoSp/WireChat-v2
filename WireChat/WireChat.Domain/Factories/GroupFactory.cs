b409798e3f61746909ad2f4d1e42a66fbfd542ac
JivkoSp <atjspasov@gmail.com>
2023-09-27 14:10:29 +0300

    Added new factory: WireChat.Domain/Factories/GroupFactory.cs.

---
+++ WireChat/WireChat.Domain/Factories/GroupFactory.cs
@@ -0,0 +1,12 @@
+﻿using WireChat.Domain.Entities;
+using WireChat.Domain.Factories.Interfaces;
+using WireChat.Domain.ValueObjects;
+
+namespace WireChat.Domain.Factories
+{
+    public class GroupFactory : IGroupFactory
+    {
+        public Group Create(GroupID groupId, GroupName groupName, Chat chat)
+            => new Group(groupId, groupName, chat);
+    }
+}
