797151b129f520df7e710231710b3e2c45c8436c
JivkoSp <atjspasov@gmail.com>
2023-09-27 19:38:23 +0300

    Added new domain value object: WireChat.Domain/ValueObjects/GroupName.cs

---
+++ WireChat/WireChat.Domain/ValueObjects/GroupName.cs
@@ -0,0 +1,27 @@
+﻿using WireChat.Domain.Exceptions;
+
+namespace WireChat.Domain.ValueObjects
+{
+    public record GroupName
+    {
+        internal string Value { get; }
+
+        public GroupName(string value)
+        {
+            if (string.IsNullOrWhiteSpace(value))
+            {
+                throw new EmptyGroupNameException();
+            }
+
+            Value = value;
+        }
+
+        //Conversion from ValueObject to String.
+        public static implicit operator string(GroupName groupName)
+            => groupName.Value;
+
+        //Conversion from String to ValueObject.
+        public static implicit operator GroupName(string groupName)
+            => new GroupName(groupName);
+    }
+}
