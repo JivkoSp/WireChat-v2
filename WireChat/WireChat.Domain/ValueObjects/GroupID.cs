856a71e9d131809ae057129acd5379acd67de5be
JivkoSp <atjspasov@gmail.com>
2023-09-27 19:14:57 +0300

    Added new domain value object: WireChat.Domain/ValueObjects/GroupID.cs

---
+++ WireChat/WireChat.Domain/ValueObjects/GroupID.cs
@@ -0,0 +1,27 @@
+﻿using WireChat.Domain.Exceptions;
+
+namespace WireChat.Domain.ValueObjects
+{
+    public record GroupID
+    {
+        public Guid Value { get; }
+
+        public GroupID(Guid value)
+        {
+            if (value == Guid.Empty)
+            {
+                throw new EmptyGuidIdException();
+            }
+
+            Value = value;
+        }
+
+        //Conversion from ValueObject to Guid.
+        public static implicit operator Guid(GroupID groupId)
+            => groupId.Value;
+
+        //Conversion from Guid to ValueObject.
+        public static implicit operator GroupID(Guid groupId)
+            => new GroupID(groupId);
+    }
+}
