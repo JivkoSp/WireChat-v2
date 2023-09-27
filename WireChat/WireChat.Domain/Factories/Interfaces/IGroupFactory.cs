e39f21c6bf771b14410d028ad3cb3b0b33c93593
JivkoSp <atjspasov@gmail.com>
2023-09-27 11:49:23 +0300

    Added new factory interface: WireChat.Domain/Factories/Interfaces/IGroupFactory.cs

---
+++ WireChat/WireChat.Domain/Factories/Interfaces/IGroupFactory.cs
@@ -0,0 +1,10 @@
+﻿using WireChat.Domain.Entities;
+using WireChat.Domain.ValueObjects;
+
+namespace WireChat.Domain.Factories.Interfaces
+{
+    public interface IGroupFactory
+    {
+        Group Create(GroupID groupId, GroupName groupName, Chat chat);
+    }
+}
