326a6e71ecf586c91838d98121f1c374951efe45
JivkoSp <atjspasov@gmail.com>
2023-09-27 15:24:18 +0300

    Added new repository interface: WireChat.Domain/Repositories/IGroupRepository.cs.

---
+++ WireChat/WireChat.Domain/Repositories/IGroupRepository.cs
@@ -0,0 +1,13 @@
+﻿using WireChat.Domain.Entities;
+using WireChat.Domain.ValueObjects;
+
+namespace WireChat.Domain.Repositories
+{
+    public interface IGroupRepository
+    {
+        Task<Group> GetGroupByIdAsync(GroupID groupId);
+        Task AddGroupAsync(Group group);
+        Task UpdateGroupAsync(Group group);
+        Task DeleteGroupAsync(Group group);
+    }
+}
