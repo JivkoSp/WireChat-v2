32ce3ce96349b4e60cf5f4873498587093a8bd68
JivkoSp <atjspasov@gmail.com>
2023-09-28 21:43:32 +0300

    Added new EntityFramework read model: WireChat.Infrastructure/EntityFramework/Models/GroupReadModel.cs

---
+++ WireChat/WireChat.Infrastructure/EntityFramework/Models/GroupReadModel.cs
@@ -0,0 +1,14 @@
+﻿
+namespace WireChat.Infrastructure.EntityFramework.Models
+{
+    public class GroupReadModel
+    {
+        // Primary key
+        public Guid GroupId { get; set; }
+        public int Version { get; set; }
+        public string GroupName { get; set; }
+
+        // Relationships
+        public virtual ChatReadModel Chat {  get; set; }
+    }
+}
