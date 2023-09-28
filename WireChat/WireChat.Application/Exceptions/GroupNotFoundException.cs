63359605aba0be11cf66633c9e1ceed7c5c48a7e
JivkoSp <atjspasov@gmail.com>
2023-09-28 11:37:48 +0300

    Added new custom application layer exception type: WireChat.Application/Exceptions/GroupNotFoundException.cs

---
+++ WireChat/WireChat.Application/Exceptions/GroupNotFoundException.cs
@@ -0,0 +1,11 @@
+﻿
+namespace WireChat.Application.Exceptions
+{
+    public sealed class GroupNotFoundException : ApplicationException
+    {
+        public GroupNotFoundException(Guid chatId) 
+            : base(message: $"Group with ID #{chatId} was NOT found!")
+        {
+        }
+    }
+}
