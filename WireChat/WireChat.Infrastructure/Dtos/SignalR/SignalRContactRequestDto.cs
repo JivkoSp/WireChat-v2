988230ef6771f6d738384dbe154d7c88e15bc4de
JivkoSp <atjspasov@gmail.com>
2023-09-29 17:48:10 +0300

    Updated the WireChat.Infrastructure/Dtos/SignalR/SignalRContactRequestDto.cs

--- WireChat/WireChat.Infrastructure/Dtos/SignalR/SignalRContactRequestDto.cs
+++ WireChat/WireChat.Infrastructure/Dtos/SignalR/SignalRContactRequestDto.cs
@@ -3,7 +3,8 @@ namespace WireChat.Infrastructure.Dtos.SignalR
 {
     public class SignalRContactRequestDto
     {
-        public string ContactName { get; set; }
+        public string SenderUserName { get; set; }
+        public string ReceiverUserName { get; set; }
         public string ContactMessage { get; set; }
         public DateTimeOffset DateTime { get; set; }
     }
