82e83a472e3b6198af3312c38de8844f38f49cc7
JivkoSp <atjspasov@gmail.com>
2023-09-29 10:14:18 +0300

    Added new EntityFramework write configuration: WireChat.Infrastructure/EntityFramework/ModelConfiguration/WriteConfiguration/GroupConfiguration.cs

---
+++ WireChat/WireChat.Infrastructure/EntityFramework/ModelConfiguration/WriteConfiguration/GroupConfiguration.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using WireChat.Domain.Entities;
+using WireChat.Domain.ValueObjects;
+using WireChat.Infrastructure.EntityFramework.Encryption.EncryptionConverters;
+using WireChat.Infrastructure.EntityFramework.Encryption.EncryptionProvider;
+
+namespace WireChat.Infrastructure.EntityFramework.ModelConfiguration.WriteConfiguration
+{
+    internal sealed class GroupConfiguration : IEntityTypeConfiguration<Group>
+    {
+        private readonly IEncryptionProvider _encryptionProvider;
+
+        public GroupConfiguration(IEncryptionProvider encryptionProvider)
+        {
+            _encryptionProvider = encryptionProvider;
+        }
+
+        public void Configure(EntityTypeBuilder<Group> builder)
+        {
+            // Table name
+            builder.ToTable("Group");
+
+            // Primary key
+            builder.HasKey(key => key.Id);
+
+            // Property config
+            builder.Property(p => p.Id)
+                .HasConversion(id => id.Value, id => new GroupID(id))
+                .HasColumnName("GroupId")
+                .IsRequired();
+
+            builder.Property(typeof(GroupName), "_groupName")
+                .HasConversion(new EncryptedGroupNameConverter(_encryptionProvider))
+                .HasColumnName("GroupName")
+                .IsRequired();
+
+            //builder.HasOne(p => p.Chat)
+            //   .WithOne()
+            //   .HasForeignKey<GroupReadModel>(p => p.GroupId)
+            //   .HasConstraintName("FK_Group_Chat")
+            //   .OnDelete(DeleteBehavior.SetNull);
+
+            //builder.Property(typeof(Chat), "_chat")
+            //    .HasColumnName("Chat")
+            //    .IsRequired();
+        }
+    }
+}
