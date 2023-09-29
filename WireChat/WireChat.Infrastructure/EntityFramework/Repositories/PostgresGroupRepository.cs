f9e8d32dec39ea96541bbdb8385f2f22df8b27a3
JivkoSp <atjspasov@gmail.com>
2023-09-29 11:29:04 +0300

    Added new repository: WireChat.Infrastructure/EntityFramework/Repositories/PostgresGroupRepository.cs

---
+++ WireChat/WireChat.Infrastructure/EntityFramework/Repositories/PostgresGroupRepository.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WireChat.Domain.Entities;
+using WireChat.Domain.Repositories;
+using WireChat.Domain.ValueObjects;
+using WireChat.Infrastructure.EntityFramework.Contexts;
+
+namespace WireChat.Infrastructure.EntityFramework.Repositories
+{
+    internal sealed class PostgresGroupRepository : IGroupRepository
+    {
+        private readonly WriteDbContext _writeDbContext;
+
+        public PostgresGroupRepository(WriteDbContext writeDbContext)
+        {
+            _writeDbContext = writeDbContext;
+        }
+
+        public Task<Group> GetGroupByIdAsync(GroupID groupId)
+            => _writeDbContext.Groups
+                .SingleOrDefaultAsync(x => x.Id == groupId);
+
+        public async Task AddGroupAsync(Group group)
+        {
+            await _writeDbContext.Groups.AddAsync(group);
+
+            await _writeDbContext.SaveChangesAsync();
+        }
+
+        public async Task UpdateGroupAsync(Group group)
+        {
+            _writeDbContext.Groups.Update(group);
+
+            await _writeDbContext.SaveChangesAsync();
+        }
+
+        public async Task DeleteGroupAsync(Group group)
+        {
+            _writeDbContext.Groups.Remove(group);
+
+            await _writeDbContext.SaveChangesAsync();
+        }
+    }
+}
