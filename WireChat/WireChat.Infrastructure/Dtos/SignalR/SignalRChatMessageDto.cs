5bb4919f6d6dbaa7becd4487dffd08dba2b22824
JivkoSp <atjspasov@gmail.com>
2023-09-29 17:19:38 +0300

    Updated the WireChat.Infrastructure/Dtos/SignalR/SignalRChatMessageDto.cs

--- WireChat/WireChat.Infrastructure/Dtos/SignalR/SignalRChatMessageDto.cs
+++ WireChat/WireChat.Infrastructure/Dtos/SignalR/SignalRChatMessageDto.cs
@@ -3,10 +3,9 @@ namespace WireChat.Infrastructure.Dtos.SignalR
 {
     public class SignalRChatMessageDto
     {
-        public Guid ChatId { get; set; }
+        public string ChatId { get; set; }
         public string UserName { get; set; }
         public string Message { get; set; }
         public DateTimeOffset MessageDateTime { get; set; }
-        public bool IsFromCurrentUser { get; set; } = false;
     }
 }
