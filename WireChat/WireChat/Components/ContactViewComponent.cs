8b5dcb7d4ef390f1df399fae57182abd0f6ffe80
JivkoSp <atjspasov@gmail.com>
2023-09-30 12:43:51 +0300

    Added new view component: WireChat/Components/ContactViewComponent.cs.

---
+++ WireChat/WireChat/Components/ContactViewComponent.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WireChat.Application.Queries;
+using WireChat.Application.Queries.Dispatcher;
+
+namespace WireChat.Components
+{
+    public class ContactViewComponent : ViewComponent
+    {
+        private readonly IQueryDispatcher _queryDispatcher;
+
+        public ContactViewComponent(IQueryDispatcher queryDispatcher)
+        {
+            _queryDispatcher = queryDispatcher;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync(string userId)
+        {
+            var getChatUserQuery = new GetChatUserQuery(userId);
+
+            var user = await _queryDispatcher.DispatchAsync(getChatUserQuery);
+
+            return View(user);
+        }
+    }
+}
