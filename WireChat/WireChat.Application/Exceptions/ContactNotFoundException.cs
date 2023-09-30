55ed19e23c57ddff820dc968634b8bebcf618108
JivkoSp <atjspasov@gmail.com>
2023-09-30 23:04:59 +0300

    Added new custom application layer exception type: WireChat.Application/Exceptions/ContactNotFoundException.cs

---
+++ WireChat/WireChat.Application/Exceptions/ContactNotFoundException.cs
@@ -0,0 +1,11 @@
+﻿
+namespace WireChat.Application.Exceptions
+{
+    public sealed class ContactNotFoundException : ApplicationException
+    {
+        internal ContactNotFoundException(Guid userId, Guid newChatUserId) 
+            : base(message: $"User with ID #{userId} does not have contact with ID {newChatUserId}!")
+        {
+        }
+    }
+}
