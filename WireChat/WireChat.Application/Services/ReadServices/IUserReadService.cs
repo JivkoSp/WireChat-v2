40d806d13f3f5f46f81c56b8df0e8ae89089aa2e
JivkoSp <atjspasov@gmail.com>
2023-10-01 17:23:10 +0300

    Added HasUserInContactsAsync method in WireChat.Application/Services/ReadServices/IUserReadService.cs

--- WireChat/WireChat.Application/Services/ReadServices/IUserReadService.cs
+++ WireChat/WireChat.Application/Services/ReadServices/IUserReadService.cs
@@ -4,5 +4,6 @@ namespace WireChat.Application.Services.ReadServices
     public interface IUserReadService
     {
         Task<bool> ExistsByIdAsync(Guid id);
+        Task<bool> HasUserInContactsAsync(Guid userId, Guid otherUserId);
     }
 }
