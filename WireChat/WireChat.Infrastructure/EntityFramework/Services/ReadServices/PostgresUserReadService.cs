7cd9ad88bf41dec479d8be12998a519419c5ab1b
JivkoSp <atjspasov@gmail.com>
2023-10-01 22:10:33 +0300

    Added implementation of the HasUserInContactsAsync method in WireChat.Infrastructure/EntityFramework/Services/ReadServices/PostgresUserReadService.cs

--- WireChat/WireChat.Infrastructure/EntityFramework/Services/ReadServices/PostgresUserReadService.cs
+++ WireChat/WireChat.Infrastructure/EntityFramework/Services/ReadServices/PostgresUserReadService.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using WireChat.Application.Queries;
+using WireChat.Application.Queries.Dispatcher;
 using WireChat.Application.Services.ReadServices;
 using WireChat.Infrastructure.EntityFramework.Contexts;
 
@@ -7,13 +9,24 @@ namespace WireChat.Infrastructure.EntityFramework.Services.ReadServices
     internal sealed class PostgresUserReadService : IUserReadService
     {
         private readonly ReadDbContext _readDbContext;
+        private readonly IQueryDispatcher _queryDispatcher;
 
-        public PostgresUserReadService(ReadDbContext readDbContext)
+        public PostgresUserReadService(ReadDbContext readDbContext, IQueryDispatcher queryDispatcher)
         {
             _readDbContext = readDbContext;
+            _queryDispatcher = queryDispatcher;
         }
 
         public Task<bool> ExistsByIdAsync(Guid id)
             => _readDbContext.UserReadModels.AnyAsync(x => x.Id == id.ToString());
+
+        public async Task<bool> HasUserInContactsAsync(Guid userId, Guid otherUserId)
+        {
+            var getContactsQuery = new GetContactsQuery(userId.ToString());
+
+            var chatUserReadModels = await _queryDispatcher.DispatchAsync(getContactsQuery);
+
+            return chatUserReadModels.Any(x => x.UserId == otherUserId.ToString());
+        }
     }
 }
