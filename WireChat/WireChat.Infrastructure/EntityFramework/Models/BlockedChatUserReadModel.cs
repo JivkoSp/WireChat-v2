2a768b2656c4816532560239155e872703db319b
JivkoSp <atjspasov@gmail.com>
2023-10-02 21:24:22 +0300

    Added new EntityFramework model type: WireChat.Infrastructure/EntityFramework/Models/BlockedChatUserReadModel.cs

---
+++ WireChat/WireChat.Infrastructure/EntityFramework/Models/BlockedChatUserReadModel.cs
@@ -0,0 +1,14 @@
+﻿
+namespace WireChat.Infrastructure.EntityFramework.Models
+{
+    public class BlockedChatUserReadModel
+    {
+        // Composite Primary Key { UserId, ChatId }
+        public string UserId { get; set; }
+        public Guid ChatId { get; set; }
+
+        // Relationships
+        public virtual UserReadModel User { get; set; }
+        public virtual ChatReadModel Chat { get; set; }
+    }
+}
