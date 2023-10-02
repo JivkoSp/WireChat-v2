fc8773ba1c12a149783c86728c67c253ee8aa521
JivkoSp <atjspasov@gmail.com>
2023-10-02 20:45:01 +0300

    Added new automapper profile type: WireChat.Infrastructure/Automapper/Profiles/BlockedChatUserProfile.cs

---
+++ WireChat/WireChat.Infrastructure/Automapper/Profiles/BlockedChatUserProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using WireChat.Application.Dtos;
+using WireChat.Infrastructure.EntityFramework.Models;
+
+namespace WireChat.Infrastructure.Automapper.Profiles
+{
+    internal sealed class BlockedChatUserProfile : Profile
+    {
+        public BlockedChatUserProfile()
+        {
+            CreateMap<BlockedChatUserReadModel, BlockedChatUserDto>()
+                .ForMember(dest => dest.UserName, opt => opt.MapFrom(src => src.User.UserName));
+        }
+    }
+}
