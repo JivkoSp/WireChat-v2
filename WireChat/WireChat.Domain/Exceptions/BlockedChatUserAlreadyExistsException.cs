156285e37e264568232fb03d0d112e1b65f566b7
JivkoSp <atjspasov@gmail.com>
2023-10-02 10:39:11 +0300

    Added new custom domain layer exception type: WireChat.Domain/Exceptions/BlockedChatUserAlreadyExistsException.cs

---
+++ WireChat/WireChat.Domain/Exceptions/BlockedChatUserAlreadyExistsException.cs
@@ -0,0 +1,11 @@
+﻿
+namespace WireChat.Domain.Exceptions
+{
+    public sealed class BlockedChatUserAlreadyExistsException : DomainException
+    {
+        internal BlockedChatUserAlreadyExistsException(Guid userId, Guid chatId) 
+            : base(message: $"User with ID #{userId} is already blocked from chat with ID #{chatId}!")
+        {
+        }
+    }
+}
