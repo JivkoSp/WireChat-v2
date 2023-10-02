ee08112c75ee1dc41ac5055756e6fccc6cd139ea
JivkoSp <atjspasov@gmail.com>
2023-10-02 12:36:58 +0300

    Added new custom event type: WireChat.Domain/Events/BlockedChatUserRemoved.cs

---
+++ WireChat/WireChat.Domain/Events/BlockedChatUserRemoved.cs
@@ -0,0 +1,7 @@
+﻿using WireChat.Domain.Entities;
+using WireChat.Domain.ValueObjects;
+
+namespace WireChat.Domain.Events
+{
+    public record BlockedChatUserRemoved(Chat Chat, BlockedChatUser BlockedChatUser) : IDomainEvent;
+}
