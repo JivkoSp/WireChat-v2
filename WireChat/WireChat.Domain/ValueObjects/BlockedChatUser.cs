b34499a564ee7734fb38321c4dfc5158245aaba3
JivkoSp <atjspasov@gmail.com>
2023-10-02 09:28:19 +0300

    Added new domain value object: WireChat.Domain/ValueObjects/BlockedChatUser.cs

---
+++ WireChat/WireChat.Domain/ValueObjects/BlockedChatUser.cs
@@ -0,0 +1,28 @@
+﻿using WireChat.Domain.Exceptions;
+
+namespace WireChat.Domain.ValueObjects
+{
+    public class BlockedChatUser
+    {
+        public UserID UserID { get; }
+        public ChatID ChatID { get; }
+
+        private BlockedChatUser() {}
+
+        public BlockedChatUser(UserID userId, ChatID chatId)
+        {
+            if (userId == null)
+            {
+                throw new NullUserIdException();
+            }
+
+            if (chatId == null)
+            {
+                throw new NullChatIdException();
+            }
+
+            UserID = userId;
+            ChatID = chatId;
+        }
+    }
+}
