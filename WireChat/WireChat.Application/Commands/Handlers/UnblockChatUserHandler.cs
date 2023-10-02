2fb0b14ee044ab1dfe9753782208d68840656dcb
JivkoSp <atjspasov@gmail.com>
2023-10-02 17:57:01 +0300

    Added new command handler: WireChat.Application/Commands/Handlers/UnblockChatUserHandler.cs

---
+++ WireChat/WireChat.Application/Commands/Handlers/UnblockChatUserHandler.cs
@@ -0,0 +1,29 @@
+﻿using WireChat.Application.Exceptions;
+using WireChat.Domain.Repositories;
+
+namespace WireChat.Application.Commands.Handlers
+{
+    internal sealed class UnblockChatUserHandler : ICommandHandler<UnblockChatUserCommand>
+    {
+        private readonly IChatRepository _chatRepository;
+
+        public UnblockChatUserHandler(IChatRepository chatRepository)
+        {
+            _chatRepository = chatRepository;
+        }
+
+        public async Task HandleAsync(UnblockChatUserCommand command)
+        {
+            var chat = await _chatRepository.GetChatByIdAsync(command.ChatId);
+
+            if (chat is null)
+            {
+                throw new ChatNotFoundException(chat.Id);
+            }
+
+            chat.UnblockChatUser(command.UserId);
+
+            await _chatRepository.UpdateChatAsync(chat);
+        }
+    }
+}
