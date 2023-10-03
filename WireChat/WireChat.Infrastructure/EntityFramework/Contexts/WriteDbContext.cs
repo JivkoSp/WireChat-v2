2a4593bf7c909d62fe4e239c7173fd6e51779b37
JivkoSp <atjspasov@gmail.com>
2023-10-03 14:21:43 +0300

    Updated WireChat.Infrastructure/EntityFramework/Contexts/WriteDbContext.cs

--- WireChat/WireChat.Infrastructure/EntityFramework/Contexts/WriteDbContext.cs
+++ WireChat/WireChat.Infrastructure/EntityFramework/Contexts/WriteDbContext.cs
@@ -46,6 +46,7 @@ namespace WireChat.Infrastructure.EntityFramework.Contexts
             modelBuilder.ApplyConfiguration(new UserConfiguration(_encryptionProvider));
             modelBuilder.ApplyConfiguration(new UserContactRequestConfiguration(_encryptionProvider));
             modelBuilder.ApplyConfiguration(new GroupConfiguration(_encryptionProvider));
+            modelBuilder.ApplyConfiguration(new BlockedChatUserConfiguration());
         }
     }
 }
