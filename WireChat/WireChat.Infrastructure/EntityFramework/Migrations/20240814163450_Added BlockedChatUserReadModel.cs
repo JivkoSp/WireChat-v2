993972e6dab5e080a09ef1285d31540ee70acf78
JivkoSp <atjspasov@gmail.com>
2023-10-03 14:47:01 +0300

    Added new EntityFramework migrations in WireChat.Infrastructure/EntityFramework/Migrations/

---
+++ WireChat/WireChat.Infrastructure/EntityFramework/Migrations/20240814163450_Added BlockedChatUserReadModel.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WireChat.Infrastructure.EntityFramework.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedBlockedChatUserReadModel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "BlockedChatUser",
+                schema: "wirechat",
+                columns: table => new
+                {
+                    UserId = table.Column<string>(type: "text", nullable: false),
+                    ChatId = table.Column<Guid>(type: "uuid", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_BlockedChatUser", x => new { x.UserId, x.ChatId });
+                    table.ForeignKey(
+                        name: "FK_Chat_BlockedChatUsers",
+                        column: x => x.ChatId,
+                        principalSchema: "wirechat",
+                        principalTable: "Chat",
+                        principalColumn: "ChatId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_User_BlockedChatUsers",
+                        column: x => x.UserId,
+                        principalSchema: "wirechat",
+                        principalTable: "User",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BlockedChatUser_ChatId",
+                schema: "wirechat",
+                table: "BlockedChatUser",
+                column: "ChatId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "BlockedChatUser",
+                schema: "wirechat");
+        }
+    }
+}
