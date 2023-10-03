4d3f8ba093615d39d2d1278cfe7ceb1d1f70879c
JivkoSp <atjspasov@gmail.com>
2023-10-03 12:04:56 +0300

    Added new EntityFramework configuration: WireChat.Infrastructure/EntityFramework/ModelConfiguration/WriteConfiguration/BlockedChatUserConfiguration.cs

---
+++ WireChat/WireChat.Infrastructure/EntityFramework/ModelConfiguration/WriteConfiguration/BlockedChatUserConfiguration.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using WireChat.Domain.ValueObjects;
+
+namespace WireChat.Infrastructure.EntityFramework.ModelConfiguration.WriteConfiguration
+{
+    internal sealed class BlockedChatUserConfiguration : IEntityTypeConfiguration<BlockedChatUser>
+    {
+        public void Configure(EntityTypeBuilder<BlockedChatUser> builder)
+        {
+            // Table name
+            builder.ToTable("BlockedChatUser");
+
+            // Composite primary key
+            builder.HasKey(key => new { key.UserID, key.ChatID });
+
+            // Property config - Start
+
+            builder.Property(p => p.UserID)
+                .HasConversion(id => id.Value.ToString(), id => new UserID(Guid.Parse(id)))
+                .HasColumnName("UserId")
+                .IsRequired();
+
+            builder.Property(p => p.ChatID)
+                .HasConversion(id => id.Value, id => new ChatID(id))
+                .HasColumnName("ChatId")
+                .IsRequired();
+
+            // Property config - End
+        }
+    }
+}
