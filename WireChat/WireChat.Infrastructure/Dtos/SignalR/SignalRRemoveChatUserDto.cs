68edad7f5e8281352ff6942e3227c9fd7c9287ef
JivkoSp <atjspasov@gmail.com>
2023-10-04 09:49:15 +0300

    Added new custom infrastructure layer dto type: WireChat/WireChat.Infrastructure/Dtos/SignalR/SignalRRemoveChatUserDto.cs

---
+++ WireChat/WireChat.Infrastructure/Dtos/SignalR/SignalRRemoveChatUserDto.cs
@@ -0,0 +1,10 @@
+﻿
+namespace WireChat.Infrastructure.Dtos.SignalR
+{
+    public class SignalRRemoveChatUserDto
+    {
+        public string ChatId { get; set; }
+        public string UserId { get; set; }
+        public string UserName { get; set; }
+    }
+}
