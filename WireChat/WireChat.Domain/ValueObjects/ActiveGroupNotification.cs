d3638d59cabf12582844c66bee812f1ec212f98f
JivkoSp <atjspasov@gmail.com>
2023-10-04 22:02:48 +0300

    Added new domain value object: WireChat/WireChat.Domain/ValueObjects/ActiveGroupNotification.cs'

---
+++ WireChat/WireChat.Domain/ValueObjects/ActiveGroupNotification.cs
@@ -0,0 +1,19 @@
+﻿using WireChat.Domain.Exceptions;
+
+namespace WireChat.Domain.ValueObjects
+{
+    public record ActiveGroupNotification
+    {
+        public GroupID GroupId { get; }
+
+        public ActiveGroupNotification(GroupID groupId)
+        {
+            if (groupId == null)
+            {
+                throw new NullGroupIdException();
+            }
+
+            GroupId = groupId;
+        }
+    }
+}
