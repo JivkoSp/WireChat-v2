f33fb7b393ba463e9d35a8f2fe0148e02a7c4474
JivkoSp <atjspasov@gmail.com>
2023-10-05 18:11:29 +0300

    Added new domain value object: WireChat/WireChat.Domain/ValueObjects/NotificationHubID.cs

---
+++ WireChat/WireChat.Domain/ValueObjects/NotificationHubID.cs
@@ -0,0 +1,27 @@
+﻿using WireChat.Domain.Exceptions;
+
+namespace WireChat.Domain.ValueObjects
+{
+    public record NotificationHubID
+    {
+        public Guid Value { get; }
+
+        public NotificationHubID(Guid value)
+        {
+            if (value == Guid.Empty)
+            {
+                throw new EmptyNotificationIdException();
+            }
+
+            Value = value;
+        }
+
+        //Conversion from ValueObject to Guid.
+        public static implicit operator Guid(NotificationHubID notificationId)
+            => notificationId.Value;
+
+        //Conversion from Guid to ValueObject.
+        public static implicit operator NotificationHubID(Guid notificationId)
+            => new NotificationHubID(notificationId);
+    }
+}
