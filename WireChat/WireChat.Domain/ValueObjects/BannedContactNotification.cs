f097d53f162342f6add492b9255c4c274af68e64
JivkoSp <atjspasov@gmail.com>
2023-10-05 10:17:36 +0300

    Added new domain value object: WireChat/WireChat.Domain/ValueObjects/BannedContactNotification.cs

---
+++ WireChat/WireChat.Domain/ValueObjects/BannedContactNotification.cs
@@ -0,0 +1,33 @@
+﻿using WireChat.Domain.Exceptions;
+
+namespace WireChat.Domain.ValueObjects
+{
+    public record BannedContactNotification
+    {
+        public UserID UserId { get; }
+        public ChatID ChatId { get; set; }
+        internal DateTimeOffset DateTime { get; }
+
+        public BannedContactNotification(UserID userId, ChatID chatId, DateTimeOffset dateTime)
+        {
+            if (userId == null)
+            {
+                throw new NullUserIdException();
+            }
+
+            if (chatId == null)
+            {
+                throw new NullChatIdException();
+            }
+
+            if (dateTime == default || dateTime > DateTimeOffset.Now)
+            {
+                throw new InvalidBannedContactDateTimeException();
+            }
+
+            UserId = userId;
+            ChatId = chatId;
+            DateTime = dateTime;
+        }
+    }
+}
