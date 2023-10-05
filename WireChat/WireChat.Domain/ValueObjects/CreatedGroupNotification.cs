f66291112d54eba4c7a7e4da25a782b5a64af725
JivkoSp <atjspasov@gmail.com>
2023-10-05 12:16:28 +0300

    Added new domain value object: WireChat/WireChat.Domain/ValueObjects/CreatedGroupNotification.cs

---
+++ WireChat/WireChat.Domain/ValueObjects/CreatedGroupNotification.cs
@@ -0,0 +1,33 @@
+﻿using WireChat.Domain.Exceptions;
+
+namespace WireChat.Domain.ValueObjects
+{
+    public record CreatedGroupNotification
+    {
+        public UserID UserId { get; }
+        public GroupID GroupId { get; }
+        internal DateTimeOffset DateTime { get; }
+
+        public CreatedGroupNotification(UserID userId, GroupID groupId, DateTimeOffset dateTime)
+        {
+            if (userId == null)
+            {
+                throw new NullUserIdException();
+            }
+
+            if (groupId == null)
+            {
+                throw new NullGroupIdException();
+            }
+
+            if (dateTime == default || dateTime > DateTimeOffset.Now)
+            {
+                throw new InvalidCreatedGroupDateTimeException();
+            }
+
+            UserId = userId;
+            GroupId = groupId;
+            DateTime = dateTime;
+        }
+    }
+}
