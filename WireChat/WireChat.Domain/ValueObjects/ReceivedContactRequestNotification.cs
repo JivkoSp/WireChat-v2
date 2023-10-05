429fe3514c499e64ff2024c3a541a1dcb3c2eaa3
JivkoSp <atjspasov@gmail.com>
2023-10-05 19:41:14 +0300

    Added new domain value object: WireChat/WireChat.Domain/ValueObjects/ReceivedContactRequestNotification.cs

---
+++ WireChat/WireChat.Domain/ValueObjects/ReceivedContactRequestNotification.cs
@@ -0,0 +1,33 @@
+﻿using WireChat.Domain.Exceptions;
+
+namespace WireChat.Domain.ValueObjects
+{
+    public record ReceivedContactRequestNotification
+    {
+        public UserID SenderUserId { get; }
+        public UserID ReceiverUserId { get; }
+        internal DateTimeOffset DateTime { get; }
+
+        public ReceivedContactRequestNotification(UserID senderUserId, UserID receiverUserId, DateTimeOffset dateTime)
+        {
+            if (senderUserId == null)
+            {
+                throw new NullUserIdException();
+            }
+
+            if (receiverUserId == null)
+            {
+                throw new NullUserIdException();
+            }
+
+            if (dateTime == default || dateTime > DateTimeOffset.Now)
+            {
+                throw new InvalidReceivedContactRequestDateTimeException();
+            }
+
+            SenderUserId = senderUserId;
+            ReceiverUserId = receiverUserId;
+            DateTime = dateTime;
+        }
+    }
+}
