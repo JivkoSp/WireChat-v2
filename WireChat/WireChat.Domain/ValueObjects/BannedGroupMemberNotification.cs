a6f38ac271f816afd8e5948b5f1d2bc33284c81c
JivkoSp <atjspasov@gmail.com>
2023-10-05 10:53:12 +0300

    Added new domain value object: WireChat/WireChat.Domain/ValueObjects/BannedGroupMemberNotification.cs

---
+++ WireChat/WireChat.Domain/ValueObjects/BannedGroupMemberNotification.cs
@@ -0,0 +1,40 @@
+﻿using WireChat.Domain.Exceptions;
+
+namespace WireChat.Domain.ValueObjects
+{
+    public record BannedGroupMemberNotification
+    {
+        public UserID GroupAdminUserId { get; }
+        public UserID GroupMemberUserId { get; }
+        public GroupID GroupId { get; }
+        internal DateTimeOffset DateTime { get; }
+
+        public BannedGroupMemberNotification(UserID groupAdminUserId, UserID groupMemberUserId, GroupID groupId, DateTimeOffset dateTime)
+        {
+            if (groupAdminUserId == null)
+            {
+                throw new NullUserIdException();
+            }
+
+            if (groupMemberUserId == null)
+            {
+                throw new NullUserIdException();
+            }
+
+            if (groupId == null)
+            {
+                throw new NullGroupIdException();
+            }
+
+            if (dateTime == default || dateTime > DateTimeOffset.Now)
+            {
+                throw new InvalidBannedGroupMemberDateTimeException();
+            }
+
+            GroupAdminUserId = groupAdminUserId;
+            GroupMemberUserId = groupMemberUserId;
+            GroupId = groupId;
+            DateTime = dateTime;
+        }
+    }
+}
