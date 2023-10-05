b07cde0f31d233e675f2f5ca79dcafb784f698c5
JivkoSp <atjspasov@gmail.com>
2023-10-05 15:23:14 +0300

    Added new domain value object: WireChat/WireChat.Domain/ValueObjects/DeclinedContactRequestNotification.cs

---
+++ WireChat/WireChat.Domain/ValueObjects/DeclinedContactRequestNotification.cs
@@ -0,0 +1,33 @@
+﻿using WireChat.Domain.Exceptions;
+
+namespace WireChat.Domain.ValueObjects
+{
+    public record DeclinedContactRequestNotification
+    {
+        public UserID SenderUserId { get; }
+        public UserID ReceiverUserId { get; }
+        internal DateTimeOffset DateTime { get; }
+
+        public DeclinedContactRequestNotification(UserID senderUserId, UserID receiverUserId, DateTimeOffset dateTime)
+        {
+            if (senderUserId == null)
+            {
+                throw new NullUserIdException();
+            }
+
+            if (receiverUserId == null)
+            {
+                throw new NullUserIdException();
+            }
+
+            if (dateTime == default || dateTime > DateTimeOffset.Now)
+            {
+                throw new InvalidDeclinedContactRequestDateTimeException();
+            }
+
+            SenderUserId = senderUserId;
+            ReceiverUserId = receiverUserId;    
+            DateTime = dateTime;
+        }
+    }
+}
