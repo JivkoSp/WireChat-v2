3a38a7d6071beb742e53ad8bb57a933e8fee5743
JivkoSp <atjspasov@gmail.com>
2023-10-05 23:32:45 +0300

    Added new domain value object: WireChat/WireChat.Domain/ValueObjects/RemovedGroupMemberNotification.cs

---
+++ WireChat/WireChat.Domain/ValueObjects/RemovedGroupMemberNotification.cs
@@ -0,0 +1,40 @@
+﻿using WireChat.Domain.Exceptions;
+
+namespace WireChat.Domain.ValueObjects
+{
+    public record RemovedGroupMemberNotification
+    {
+        public UserID GroupAdminUserId { get; }
+        public UserID GroupMemberUserId { get; }
+        public GroupID GroupId { get; }
+        internal DateTimeOffset DateTime { get; }
+
+        public RemovedGroupMemberNotification(UserID groupAdminUserId, UserID groupMemberUserId, GroupID groupId, DateTimeOffset dateTime)
+        {
+            if (groupAdminUserId == null)
+            {
+                throw new NullUserIdException();
+            }
+
+            if (groupMemberUserId == null)
+            {
+                throw new NullUserIdException();
+            }
+
+            if (groupId == null)
+            {
+                throw new NullGroupIdException();
+            }
+
+            if (dateTime == default || dateTime > DateTimeOffset.Now)
+            {
+                throw new InvalidRemovedGroupMemberDateTimeException();
+            }
+
+            GroupAdminUserId = groupAdminUserId;
+            GroupMemberUserId = groupMemberUserId;
+            GroupId = groupId;
+            DateTime = dateTime;
+        }
+    }
+}
