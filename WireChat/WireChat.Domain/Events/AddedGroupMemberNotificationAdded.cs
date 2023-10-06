b401d69af696bf38fa73c9c64b9d47ed95c5afaa
JivkoSp <atjspasov@gmail.com>
2023-10-06 21:23:39 +0300

    Added new custom domain event type: WireChat/WireChat.Domain/Events/AddedGroupMemberNotificationAdded.cs

---
+++ WireChat/WireChat.Domain/Events/AddedGroupMemberNotificationAdded.cs
@@ -0,0 +1,8 @@
+﻿using WireChat.Domain.Entities;
+using WireChat.Domain.ValueObjects;
+
+namespace WireChat.Domain.Events
+{
+    public record AddedGroupMemberNotificationAdded(NotificationHub NotificationHub, 
+        AddedGroupMemberNotification AddedGroupMemberNotification) : IDomainEvent;
+}
