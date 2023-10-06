d476f9f342e1e9fbafa445d5e9577ef2687a0c99
JivkoSp <atjspasov@gmail.com>
2023-10-06 13:18:34 +0300

    Added new custom domain event type: WireChat/WireChat.Domain/Events/AcceptedContactRequestNotificationAdded.cs

---
+++ WireChat/WireChat.Domain/Events/AcceptedContactRequestNotificationAdded.cs
@@ -0,0 +1,8 @@
+﻿using WireChat.Domain.Entities;
+using WireChat.Domain.ValueObjects;
+
+namespace WireChat.Domain.Events
+{
+    public record AcceptedContactRequestNotificationAdded(NotificationHub NotificationHub, 
+        AcceptedContactRequestNotification AcceptedContactRequestNotification) : IDomainEvent;
+}
