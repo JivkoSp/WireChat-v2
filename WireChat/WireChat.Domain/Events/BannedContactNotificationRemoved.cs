7f4e4007ab0f20117b40f2f5898b7e93d1024c80
JivkoSp <atjspasov@gmail.com>
2023-10-07 12:49:11 +0300

    Added new custom domain event type: WireChat/WireChat.Domain/Events/BannedContactNotificationRemoved.cs

---
+++ WireChat/WireChat.Domain/Events/BannedContactNotificationRemoved.cs
@@ -0,0 +1,8 @@
+﻿using WireChat.Domain.Entities;
+using WireChat.Domain.ValueObjects;
+
+namespace WireChat.Domain.Events
+{
+    public record BannedContactNotificationRemoved(NotificationHub NotificationHub, 
+        BannedContactNotification BannedContactNotification) : IDomainEvent;
+}
