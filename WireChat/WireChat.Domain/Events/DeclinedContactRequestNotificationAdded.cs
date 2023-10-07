aca1063d9a1fba7e525c34f2fdb070f8fb067c58
JivkoSp <atjspasov@gmail.com>
2023-10-07 11:58:24 +0300

    Added new custom domain event type: WireChat/WireChat.Domain/Events/DeclinedContactRequestNotificationAdded.cs

---
+++ WireChat/WireChat.Domain/Events/DeclinedContactRequestNotificationAdded.cs
@@ -0,0 +1,8 @@
+﻿using WireChat.Domain.Entities;
+using WireChat.Domain.ValueObjects;
+
+namespace WireChat.Domain.Events
+{
+    public record DeclinedContactRequestNotificationAdded(NotificationHub NotificationHub, 
+        DeclinedContactRequestNotification DeclinedContactRequestNotification) : IDomainEvent;
+}
