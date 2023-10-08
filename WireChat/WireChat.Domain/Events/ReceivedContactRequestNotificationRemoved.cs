63f18d74aee8b1e48d9e84ff468f455ea50347ca
JivkoSp <atjspasov@gmail.com>
2023-10-08 18:11:28 +0300

    Added new custom domain event type: WireChat/WireChat.Domain/Events/ReceivedContactRequestNotificationRemoved.cs

---
+++ WireChat/WireChat.Domain/Events/ReceivedContactRequestNotificationRemoved.cs
@@ -0,0 +1,8 @@
+﻿using WireChat.Domain.Entities;
+using WireChat.Domain.ValueObjects;
+
+namespace WireChat.Domain.Events
+{
+    public record ReceivedContactRequestNotificationRemoved(NotificationHub NotificationHub,
+        ReceivedContactRequestNotification ReceivedContactRequestNotification) : IDomainEvent;
+}
