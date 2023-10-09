b82e96c19f68d247bfefbe46abdfb3e2f49988c4
JivkoSp <atjspasov@gmail.com>
2023-10-09 13:54:29 +0300

    Added new entity factory: WireChat/WireChat.Domain/Factories/NotificationHubFactory.cs

---
+++ WireChat/WireChat.Domain/Factories/NotificationHubFactory.cs
@@ -0,0 +1,12 @@
+﻿using WireChat.Domain.Entities;
+using WireChat.Domain.Factories.Interfaces;
+using WireChat.Domain.ValueObjects;
+
+namespace WireChat.Domain.Factories
+{
+    public class NotificationHubFactory : INotificationHubFactory
+    {
+        public NotificationHub Create(NotificationHubID notificationId)
+            => new NotificationHub(notificationId);
+    }
+}
