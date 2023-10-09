9bb9e746319e4832e15e24e19e802337fda6cbbb
JivkoSp <atjspasov@gmail.com>
2023-10-09 10:23:11 +0300

    Added new custom domain event type: WireChat/WireChat.Domain/Events/RemovedGroupMemberNotificationRemoved.cs

---
+++ WireChat/WireChat.Domain/Events/RemovedGroupMemberNotificationRemoved.cs
@@ -0,0 +1,8 @@
+﻿using WireChat.Domain.Entities;
+using WireChat.Domain.ValueObjects;
+
+namespace WireChat.Domain.Events
+{
+    public record RemovedGroupMemberNotificationRemoved(NotificationHub NotificationHub,
+        RemovedGroupMemberNotification RemovedGroupMemberNotification) : IDomainEvent;
+}
