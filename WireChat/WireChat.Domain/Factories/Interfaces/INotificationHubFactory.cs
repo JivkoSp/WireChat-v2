c8682034ed650e2f75c856444587e54d5637b672
JivkoSp <atjspasov@gmail.com>
2023-10-09 11:02:31 +0300

    Added new entity factory interface: WireChat/WireChat.Domain/Factories/Interfaces/INotificationHubFactory.cs

---
+++ WireChat/WireChat.Domain/Factories/Interfaces/INotificationHubFactory.cs
@@ -0,0 +1,10 @@
+﻿using WireChat.Domain.Entities;
+using WireChat.Domain.ValueObjects;
+
+namespace WireChat.Domain.Factories.Interfaces
+{
+    public interface INotificationHubFactory
+    {
+        NotificationHub Create(NotificationHubID notificationId);
+    }
+}
