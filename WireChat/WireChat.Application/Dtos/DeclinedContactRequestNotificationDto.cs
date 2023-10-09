422813203c322a7a97be4af9ced69c3911ccc794
JivkoSp <atjspasov@gmail.com>
2023-10-09 23:38:12 +0300

    Added new application layer dto type: WireChat/WireChat.Application/Dtos/DeclinedContactRequestNotificationDto.cs

---
+++ WireChat/WireChat.Application/Dtos/DeclinedContactRequestNotificationDto.cs
@@ -0,0 +1,12 @@
+﻿
+namespace WireChat.Application.Dtos
+{
+    public class DeclinedContactRequestNotificationDto
+    {
+        public string SenderUserId { get; set; }
+        public string ReceiverUserId { get; set; }
+        public DateTimeOffset DateTime { get; set; }
+        public string SenderUserName { get; set; }
+        public string ReceiverUserName { get; set; }
+    }
+}
