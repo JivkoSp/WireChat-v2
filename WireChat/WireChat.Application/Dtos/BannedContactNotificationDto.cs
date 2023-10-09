c328008e846d43d8bf8ad7f97e556cccd3f61356
JivkoSp <atjspasov@gmail.com>
2023-10-09 18:58:34 +0300

    Added new application layer dto type: WireChat/WireChat.Application/Dtos/BannedContactNotificationDto.cs

---
+++ WireChat/WireChat.Application/Dtos/BannedContactNotificationDto.cs
@@ -0,0 +1,11 @@
+﻿
+namespace WireChat.Application.Dtos
+{
+    public class BannedContactNotificationDto
+    {
+        public string UserId { get; set; }
+        public string UserName { get; set; }
+        public Guid ChatId { get; set; }
+        public DateTimeOffset DateTime { get; set; }
+    }
+}
