a37b98e01a74687d5ce545fd0c95209996cceea1
JivkoSp <atjspasov@gmail.com>
2023-10-09 16:04:10 +0300

    Added new application layer dto type: WireChat/WireChat.Application/Dtos/ActiveGroupNotificationDto.cs

---
+++ WireChat/WireChat.Application/Dtos/ActiveGroupNotificationDto.cs
@@ -0,0 +1,9 @@
+﻿
+namespace WireChat.Application.Dtos
+{
+    public class ActiveGroupNotificationDto
+    {
+        public Guid GroupId { get; set; }
+        public string GroupName { get; set; }
+    }
+}
