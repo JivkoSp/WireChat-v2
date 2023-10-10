8c9097c2b021356d4ea250ad4f7ee29e3031060f
JivkoSp <atjspasov@gmail.com>
2023-10-10 11:59:11 +0300

    Added new domain repository interface: WireChat/WireChat.Domain/Repositories/INotificationHubRepository.cs

---
+++ WireChat/WireChat.Domain/Repositories/INotificationHubRepository.cs
@@ -0,0 +1,12 @@
+﻿using WireChat.Domain.Entities;
+using WireChat.Domain.ValueObjects;
+
+namespace WireChat.Domain.Repositories
+{
+    public interface INotificationHubRepository
+    {
+        Task<NotificationHub> GetNotificationHubByIdAsync(NotificationHubID notificationHubId);
+        Task AddNotificationHubAsync(NotificationHub notificationHub);
+        Task UpdateNotificationHubAsync(NotificationHub notificationHub);
+    }
+}
