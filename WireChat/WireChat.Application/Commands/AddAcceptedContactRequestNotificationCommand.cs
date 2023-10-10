5c1bd594c62f9ba8f9dae08bbf1de37af3a6e909
JivkoSp <atjspasov@gmail.com>
2023-10-10 14:17:08 +0300

    Added new command type: WireChat/WireChat.Application/Commands/AddAcceptedContactRequestNotificationCommand.cs

---
+++ WireChat/WireChat.Application/Commands/AddAcceptedContactRequestNotificationCommand.cs
@@ -0,0 +1,6 @@
+﻿
+namespace WireChat.Application.Commands
+{
+    public record AddAcceptedContactRequestNotificationCommand(Guid NotificationHubId, Guid SenderUserId,
+        Guid ReceiverUserId, DateTimeOffset DateTime) : ICommand;
+}
