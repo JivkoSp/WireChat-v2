be45ab1748480af0bb8291f07164b6962b32dfc2
JivkoSp <atjspasov@gmail.com>
2023-10-10 18:45:10 +0300

    Added new command type: WireChat/WireChat.Application/Commands/AddBannedContactNotificationCommand.cs

---
+++ WireChat/WireChat.Application/Commands/AddBannedContactNotificationCommand.cs
@@ -0,0 +1,5 @@
+﻿
+namespace WireChat.Application.Commands
+{
+    public record AddBannedContactNotificationCommand(Guid NotificationHubId, Guid UserId, Guid ChatId, DateTimeOffset DateTime) : ICommand;
+}
