bf253f76e6885bb7dbf9886aa58fb49d431fb04c
JivkoSp <atjspasov@gmail.com>
2023-10-11 21:36:59 +0300

    Added new command handler: WireChat/WireChat.Application/Commands/Handlers/AddIssuedContactRequestNotificationHandler.cs

---
+++ WireChat/WireChat.Application/Commands/Handlers/AddIssuedContactRequestNotificationHandler.cs
@@ -0,0 +1,50 @@
+﻿using WireChat.Application.Exceptions;
+using WireChat.Application.Services.ReadServices;
+using WireChat.Domain.Repositories;
+using WireChat.Domain.ValueObjects;
+
+namespace WireChat.Application.Commands.Handlers
+{
+    internal sealed class AddIssuedContactRequestNotificationHandler : ICommandHandler<AddIssuedContactRequestNotificationCommand>
+    {
+        private readonly INotificationHubRepository _notificationHubRepository;
+        private readonly IUserReadService _userReadService;
+
+        public AddIssuedContactRequestNotificationHandler(INotificationHubRepository notificationHubRepository, IUserReadService userReadService)
+        {
+            _notificationHubRepository = notificationHubRepository;
+            _userReadService = userReadService;
+        }
+
+        public async Task HandleAsync(AddIssuedContactRequestNotificationCommand command)
+        {
+            var notificationHub = await _notificationHubRepository.GetNotificationHubByIdAsync(command.NotificationHubId);
+
+            if (notificationHub is null)
+            {
+                throw new NotificationHubNotFoundException(command.NotificationHubId);
+            }
+
+            var senderExists = await _userReadService.ExistsByIdAsync(command.SenderUserId);
+
+            if (senderExists is false)
+            {
+                throw new UserNotFoundException(command.SenderUserId);
+            }
+
+            var receiverExists = await _userReadService.ExistsByIdAsync(command.ReceiverUserId);
+
+            if (receiverExists is false)
+            {
+                throw new UserNotFoundException(command.ReceiverUserId);
+            }
+
+            var issuedContactRequestNotification = 
+                new IssuedContactRequestNotification(command.SenderUserId, command.ReceiverUserId, command.DateTime);
+
+            notificationHub.AddIssuedContactRequestNotification(issuedContactRequestNotification);
+
+            await _notificationHubRepository.UpdateNotificationHubAsync(notificationHub);
+        }
+    }
+}
