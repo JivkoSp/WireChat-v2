8be2768a5d5fd0538f6c06177ad2b2f26f40bc1d
JivkoSp <atjspasov@gmail.com>
2023-10-11 20:48:34 +0300

    Added new command handler: WireChat/WireChat.Application/Commands/Handlers/AddDeclinedContactRequestNotificationHandler.cs

---
+++ WireChat/WireChat.Application/Commands/Handlers/AddDeclinedContactRequestNotificationHandler.cs
@@ -0,0 +1,50 @@
+﻿using WireChat.Application.Exceptions;
+using WireChat.Application.Services.ReadServices;
+using WireChat.Domain.Repositories;
+using WireChat.Domain.ValueObjects;
+
+namespace WireChat.Application.Commands.Handlers
+{
+    internal sealed class AddDeclinedContactRequestNotificationHandler : ICommandHandler<AddDeclinedContactRequestNotificationCommand>
+    {
+        private readonly INotificationHubRepository _notificationHubRepository;
+        private readonly IUserReadService _userReadService;
+
+        public AddDeclinedContactRequestNotificationHandler(INotificationHubRepository notificationHubRepository, IUserReadService userReadService)
+        {
+            _notificationHubRepository = notificationHubRepository;
+            _userReadService = userReadService;
+        }
+
+        public async Task HandleAsync(AddDeclinedContactRequestNotificationCommand command)
+        {
+            var notificationHub = await _notificationHubRepository.GetNotificationHubByIdAsync(command.NotificationHubId);
+
+            if (notificationHub is null)
+            {
+                throw new NotificationHubNotFoundException(command.NotificationHubId);
+            }
+
+            var senderExists = await _userReadService.ExistsByIdAsync(command.SenderUserId);
+
+            if (senderExists is false)
+            {
+                throw new UserNotFoundException(command.SenderUserId);
+            }
+
+            var receiverExists = await _userReadService.ExistsByIdAsync(command.ReceiverUserId);
+
+            if (receiverExists is false)
+            {
+                throw new UserNotFoundException(command.ReceiverUserId);
+            }
+
+            var declinedContactRequestNotification = 
+                new DeclinedContactRequestNotification(command.SenderUserId, command.ReceiverUserId, command.DateTime);
+
+            notificationHub.AddDeclinedContactRequestNotification(declinedContactRequestNotification);
+
+            await _notificationHubRepository.UpdateNotificationHubAsync(notificationHub);
+        }
+    }
+}
