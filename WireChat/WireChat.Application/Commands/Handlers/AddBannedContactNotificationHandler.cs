2a13057a10f80ee458012b32fe0df8914fff8bb3
JivkoSp <atjspasov@gmail.com>
2023-10-11 17:10:28 +0300

    Added new command handler: WireChat/WireChat.Application/Commands/Handlers/AddBannedContactNotificationHandler.cs

---
+++ WireChat/WireChat.Application/Commands/Handlers/AddBannedContactNotificationHandler.cs
@@ -0,0 +1,42 @@
+﻿using WireChat.Application.Exceptions;
+using WireChat.Application.Services.ReadServices;
+using WireChat.Domain.Repositories;
+using WireChat.Domain.ValueObjects;
+
+namespace WireChat.Application.Commands.Handlers
+{
+    internal sealed class AddBannedContactNotificationHandler : ICommandHandler<AddBannedContactNotificationCommand>
+    {
+        private readonly INotificationHubRepository _notificationHubRepository;
+        private readonly IUserReadService _userReadService;
+
+        public AddBannedContactNotificationHandler(INotificationHubRepository notificationHubRepository, IUserReadService userReadService)
+        {
+            _notificationHubRepository = notificationHubRepository;
+            _userReadService = userReadService; 
+        }
+
+        public async Task HandleAsync(AddBannedContactNotificationCommand command)
+        {
+            var notificationHub = await _notificationHubRepository.GetNotificationHubByIdAsync(command.NotificationHubId);
+
+            if (notificationHub is null)
+            {
+                throw new NotificationHubNotFoundException(command.NotificationHubId);
+            }
+
+            var userExists = await _userReadService.ExistsByIdAsync(command.UserId);
+
+            if (userExists is false)
+            {
+                throw new UserNotFoundException(command.UserId);
+            }
+
+            var bannedContactNotification = new BannedContactNotification(command.UserId, command.ChatId, command.DateTime);
+
+            notificationHub.AddBannedContactNotification(bannedContactNotification);
+
+            await _notificationHubRepository.UpdateNotificationHubAsync(notificationHub);
+        }
+    }
+}
