b25782184a0339937dc15ef2972ab4501241c6b5
JivkoSp <atjspasov@gmail.com>
2023-10-11 08:46:52 +0300

    Added new command type: WireChat/WireChat.Application/Commands/AddDeclinedContactRequestNotificationCommand.cs

---
+++ WireChat/WireChat.Application/Commands/AddDeclinedContactRequestNotificationCommand.cs
@@ -0,0 +1,6 @@
+﻿
+namespace WireChat.Application.Commands
+{
+    public record AddDeclinedContactRequestNotificationCommand(Guid NotificationHubId, Guid SenderUserId,
+        Guid ReceiverUserId, DateTimeOffset DateTime) : ICommand;
+}
