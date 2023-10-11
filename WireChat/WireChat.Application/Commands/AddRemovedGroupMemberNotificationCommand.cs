7531a43c82031304784c9919e1fe98ba4345f61c
JivkoSp <atjspasov@gmail.com>
2023-10-11 13:41:22 +0300

    Added new command type: WireChat/WireChat.Application/Commands/AddRemovedGroupMemberNotificationCommand.cs

---
+++ WireChat/WireChat.Application/Commands/AddRemovedGroupMemberNotificationCommand.cs
@@ -0,0 +1,6 @@
+﻿
+namespace WireChat.Application.Commands
+{
+    public record AddRemovedGroupMemberNotificationCommand(Guid NotificationHubId, Guid GroupAdminUserId, 
+        Guid GroupMemberUserId, Guid GroupId, DateTimeOffset DateTime) : ICommand;
+}
