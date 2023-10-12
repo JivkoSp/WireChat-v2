723405f0d199c8c5a9073b5887f2c34fe460d229
JivkoSp <atjspasov@gmail.com>
2023-10-12 17:24:46 +0300

    Added new command handler: WireChat/WireChat.Application/Commands/Handlers/RemoveBannedContactNotificationHandler.cs

---
+++ WireChat/WireChat.Application/Commands/Handlers/RemoveBannedContactNotificationHandler.cs
@@ -0,0 +1,39 @@
+﻿using WireChat.Application.Exceptions;
+using WireChat.Application.Services.ReadServices;
+using WireChat.Domain.Repositories;
+
+namespace WireChat.Application.Commands.Handlers
+{
+    internal sealed class RemoveBannedContactNotificationHandler : ICommandHandler<RemoveBannedContactNotificationCommand>
+    {
+        private readonly INotificationHubRepository _notificationHubRepository;
+        private readonly IUserReadService _userReadService;
+
+        public RemoveBannedContactNotificationHandler(INotificationHubRepository notificationHubRepository, IUserReadService userReadService)
+        {
+            _notificationHubRepository = notificationHubRepository;
+            _userReadService = userReadService;
+        }
+
+        public async Task HandleAsync(RemoveBannedContactNotificationCommand command)
+        {
+            var notificationHub = await _notificationHubRepository.GetNotificationHubByIdAsync(command.NotificationHubId);
+
+            if (notificationHub is null)
+            {
+                throw new NotificationHubNotFoundException(command.NotificationHubId);
+            }
+
+            var userExists = await _userReadService.ExistsByIdAsync(command.UserId);
+
+            if (userExists is false)
+            {
+                throw new UserNotFoundException(command.UserId);
+            }
+
+            notificationHub.RemoveBannedContactNotification(command.UserId);
+
+            await _notificationHubRepository.UpdateNotificationHubAsync(notificationHub);
+        }
+    }
+}
