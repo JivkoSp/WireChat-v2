0efde744eefadbc8b1678bf0f0139c081aa61c40
JivkoSp <atjspasov@gmail.com>
2023-10-12 22:34:09 +0300

    Added new command handler: WireChat/WireChat.Application/Commands/Handlers/RemoveDeclinedContactRequestNotificationHandler.cs

---
+++ WireChat/WireChat.Application/Commands/Handlers/RemoveDeclinedContactRequestNotificationHandler.cs
@@ -0,0 +1,39 @@
+﻿using WireChat.Application.Exceptions;
+using WireChat.Application.Services.ReadServices;
+using WireChat.Domain.Repositories;
+
+namespace WireChat.Application.Commands.Handlers
+{
+    internal sealed class RemoveDeclinedContactRequestNotificationHandler : ICommandHandler<RemoveDeclinedContactRequestNotificationCommand>
+    {
+        private readonly INotificationHubRepository _notificationHubRepository;
+        private readonly IUserReadService _userReadService;
+
+        public RemoveDeclinedContactRequestNotificationHandler(INotificationHubRepository notificationHubRepository, IUserReadService userReadService)
+        {
+            _notificationHubRepository = notificationHubRepository;
+            _userReadService = userReadService;
+        }
+
+        public async Task HandleAsync(RemoveDeclinedContactRequestNotificationCommand command)
+        {
+            var notificationHub = await _notificationHubRepository.GetNotificationHubByIdAsync(command.NotificationHubId);
+
+            if (notificationHub is null)
+            {
+                throw new NotificationHubNotFoundException(command.NotificationHubId);
+            }
+
+            var senderExists = await _userReadService.ExistsByIdAsync(command.SenderUserId);
+
+            if (senderExists is false)
+            {
+                throw new UserNotFoundException(command.SenderUserId);
+            }
+
+            notificationHub.RemoveDeclinedContactRequestNotification(command.SenderUserId);
+
+            await _notificationHubRepository.UpdateNotificationHubAsync(notificationHub);
+        }
+    }
+}
