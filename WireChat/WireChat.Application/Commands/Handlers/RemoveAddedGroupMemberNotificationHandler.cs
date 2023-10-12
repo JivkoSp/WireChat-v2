d1679868047eb599fadf78ca19e7dd83788d5c02
JivkoSp <atjspasov@gmail.com>
2023-10-12 16:47:12 +0300

    Added new command handler: WireChat/WireChat.Application/Commands/Handlers/RemoveAddedGroupMemberNotificationHandler.cs

---
+++ WireChat/WireChat.Application/Commands/Handlers/RemoveAddedGroupMemberNotificationHandler.cs
@@ -0,0 +1,39 @@
+﻿using WireChat.Application.Exceptions;
+using WireChat.Application.Services.ReadServices;
+using WireChat.Domain.Repositories;
+
+namespace WireChat.Application.Commands.Handlers
+{
+    internal sealed class RemoveAddedGroupMemberNotificationHandler : ICommandHandler<RemoveAddedGroupMemberNotificationCommand>
+    {
+        private readonly INotificationHubRepository _notificationHubRepository;
+        private readonly IUserReadService _userReadService;
+
+        public RemoveAddedGroupMemberNotificationHandler(INotificationHubRepository notificationHubRepository, IUserReadService userReadService)
+        {
+            _notificationHubRepository = notificationHubRepository;
+            _userReadService = userReadService;
+        }
+
+        public async Task HandleAsync(RemoveAddedGroupMemberNotificationCommand command)
+        {
+            var notificationHub = await _notificationHubRepository.GetNotificationHubByIdAsync(command.NotificationHubId);
+
+            if (notificationHub is null)
+            {
+                throw new NotificationHubNotFoundException(command.NotificationHubId);
+            }
+
+            var groupMemberExists = await _userReadService.ExistsByIdAsync(command.GroupMemberUserId);
+
+            if (groupMemberExists is false)
+            {
+                throw new UserNotFoundException(command.GroupMemberUserId);
+            }
+
+            notificationHub.RemoveAddedGroupMemberNotification(command.GroupMemberUserId);
+
+            await _notificationHubRepository.UpdateNotificationHubAsync(notificationHub);
+        }
+    }
+}
