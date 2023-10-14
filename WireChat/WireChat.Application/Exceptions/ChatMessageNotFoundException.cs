13d0bc4aaf160b501a673ff433d5f809789d44de
JivkoSp <atjspasov@gmail.com>
2023-10-14 14:42:12 +0300

    Added new custom application layer exception type: WireChat/WireChat.Application/Exceptions/ChatMessageNotFoundException.cs

---
+++ WireChat/WireChat.Application/Exceptions/ChatMessageNotFoundException.cs
@@ -0,0 +1,11 @@
+﻿
+namespace WireChat.Application.Exceptions
+{
+    public sealed class ChatMessageNotFoundException : ApplicationException
+    {
+        internal ChatMessageNotFoundException(Guid chatMessageId) 
+            : base(message: $"Chat message with ID #{chatMessageId} was NOT found!")
+        {
+        }
+    }
+}
