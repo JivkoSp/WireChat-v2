39075d32a53fe061dc731ed0572f5fe0036bb937
JivkoSp <atjspasov@gmail.com>
2023-10-14 22:32:41 +0300

    Added new automapper profile type: WireChat/WireChat.Infrastructure/Automapper/Profiles/AcceptedContactRequestNotificationProfile.cs

---
+++ WireChat/WireChat.Infrastructure/Automapper/Profiles/AcceptedContactRequestNotificationProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using WireChat.Application.Dtos;
+using WireChat.Infrastructure.EntityFramework.Models;
+
+namespace WireChat.Infrastructure.Automapper.Profiles
+{
+    internal sealed class AcceptedContactRequestNotificationProfile : Profile
+    {
+        public AcceptedContactRequestNotificationProfile()
+        {
+            CreateMap<AcceptedContactRequestNotificationReadModel, AcceptedContactRequestNotificationDto>()
+                .ForMember(dest => dest.SenderUserName, src => src.MapFrom(dest => dest.Sender.UserName))
+                .ForMember(dest => dest.ReceiverUsername, src => src.MapFrom(dest => dest.Receiver.UserName));
+        }
+    }
+}
