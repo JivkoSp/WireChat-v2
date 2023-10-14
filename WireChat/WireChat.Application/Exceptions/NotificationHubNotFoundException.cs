d8ba46a76074607289c5bf8229d231ee3bf34ea2
JivkoSp <atjspasov@gmail.com>
2023-10-14 16:37:38 +0300

    Added new custom application layer exception type: WireChat/WireChat.Application/Exceptions/NotificationHubNotFoundException.cs

---
+++ WireChat/WireChat.Application/Exceptions/NotificationHubNotFoundException.cs
@@ -0,0 +1,11 @@
+﻿
+namespace WireChat.Application.Exceptions
+{
+    public sealed class NotificationHubNotFoundException : ApplicationException
+    {
+        internal NotificationHubNotFoundException(Guid notificationHubId) 
+            : base(message: $"NotificationHub with ID #{notificationHubId} was NOT found!")
+        {
+        }
+    }
+}
