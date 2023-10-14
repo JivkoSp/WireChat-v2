cbaa6e5e31bf16470745369d27d62825aff0ed6e
JivkoSp <atjspasov@gmail.com>
2023-10-14 15:24:02 +0300

    Added new custom application layer exception type: WireChat/WireChat.Application/Exceptions/NotificationHubAlreadyExistsException.cs

---
+++ WireChat/WireChat.Application/Exceptions/NotificationHubAlreadyExistsException.cs
@@ -0,0 +1,11 @@
+﻿
+namespace WireChat.Application.Exceptions
+{
+    public sealed class NotificationHubAlreadyExistsException : ApplicationException
+    {
+        internal NotificationHubAlreadyExistsException(Guid notificationHubId) 
+            : base(message: $"NotificationHub with ID #{notificationHubId} already exists!")
+        {
+        }
+    }
+}
