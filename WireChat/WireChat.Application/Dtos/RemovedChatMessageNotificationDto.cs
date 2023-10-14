003de8b0d479051b4381efc3f4ffd44d10809ca5
JivkoSp <atjspasov@gmail.com>
2023-10-14 14:15:43 +0300

    Added new application layer dto: WireChat/WireChat.Application/Dtos/RemovedChatMessageNotificationDto.cs

---
+++ WireChat/WireChat.Application/Dtos/RemovedChatMessageNotificationDto.cs
@@ -0,0 +1,12 @@
+﻿
+namespace WireChat.Application.Dtos
+{
+    public class RemovedChatMessageNotificationDto
+    {
+        public Guid ChatId { get; set; }
+        public Guid ChatMessageId { get; set; }
+        public string UserId { get; set; }
+        public DateTimeOffset DateTime { get; set; }
+        public string UserName { get; set; }
+    }
+}
