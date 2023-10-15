6db71577df2e9c95e8003482564a662237278d8a
JivkoSp <atjspasov@gmail.com>
2023-10-15 19:11:27 +0300

    Added new automapper profile type: WireChat/WireChat.Infrastructure/Automapper/Profiles/NotificationHubProfile.cs

---
+++ WireChat/WireChat.Infrastructure/Automapper/Profiles/NotificationHubProfile.cs
@@ -0,0 +1,36 @@
+﻿using AutoMapper;
+using WireChat.Application.Dtos;
+using WireChat.Infrastructure.EntityFramework.Models;
+
+namespace WireChat.Infrastructure.Automapper.Profiles
+{
+    internal sealed class NotificationHubProfile : Profile
+    {
+        public NotificationHubProfile()
+        {
+            CreateMap<NotificationHubReadModel, NotificationHubDto>()
+                .ForMember(dest => dest.IssuedContactRequestNotificationDtos, opt => 
+                    opt.MapFrom(src => src.IssuedContactRequestNotifications))
+                .ForMember(dest => dest.ReceivedContactRequestNotificationDtos, opt => 
+                    opt.MapFrom(src => src.ReceivedContactRequestNotifications))
+                .ForMember(dest => dest.AcceptedContactRequestNotificationDtos, opt => 
+                    opt.MapFrom(src => src.AcceptedContactRequestNotifications))
+                .ForMember(dest => dest.DeclinedContactRequestNotificationDtos, opt => 
+                    opt.MapFrom(src => src.DeclinedContactRequestNotifications))
+                .ForMember(dest => dest.ActiveGroupNotificationDtos, opt => 
+                    opt.MapFrom(src => src.ActiveGroupNotifications))
+                .ForMember(dest => dest.AddedGroupMemberNotificationDtos, opt => 
+                    opt.MapFrom(src => src.AddedGroupMemberNotifications))
+                .ForMember(dest => dest.RemovedGroupMemberNotificationDtos, opt => 
+                    opt.MapFrom(src => src.RemovedGroupMemberNotifications))
+                .ForMember(dest => dest.BannedContactNotificationDtos, opt =>
+                    opt.MapFrom(src => src.BannedContactNotifications))
+                .ForMember(dest => dest.BannedGroupMemberNotificationDtos, opt => 
+                    opt.MapFrom(src => src.BannedGroupMemberNotifications))
+                .ForMember(dest => dest.CreatedGroupNotificationDtos, opt => 
+                    opt.MapFrom(src => src.CreatedGroupNotifications))
+                .ForMember(dest => dest.RemovedChatMessageNotificationDtos, opt => 
+                    opt.MapFrom(src => src.RemovedChatMessageNotifications));
+        }
+    }
+}
