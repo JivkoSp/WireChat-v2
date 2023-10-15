13471afee76a52f4263afe8aba00ec782e86c04a
JivkoSp <atjspasov@gmail.com>
2023-10-15 12:32:18 +0300

    Added new automapper profile type: WireChat/WireChat.Infrastructure/Automapper/Profiles/BannedContactNotificationProfile.cs

---
+++ WireChat/WireChat.Infrastructure/Automapper/Profiles/BannedContactNotificationProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using WireChat.Application.Dtos;
+using WireChat.Infrastructure.EntityFramework.Models;
+
+namespace WireChat.Infrastructure.Automapper.Profiles
+{
+    internal sealed class BannedContactNotificationProfile : Profile
+    {
+        public BannedContactNotificationProfile()
+        {
+            CreateMap<BannedContactNotificationReadModel, BannedContactNotificationDto>()
+                .ForMember(dest => dest.UserName, src => src.MapFrom(opt => opt.User.UserName));
+        }
+    }
+}
