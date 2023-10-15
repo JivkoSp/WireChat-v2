4ab8d8ad123c4e6a89b178f7ddc9a48a1b4d6f51
JivkoSp <atjspasov@gmail.com>
2023-10-15 22:23:45 +0300

    Added new automapper profile type: WireChat/WireChat.Infrastructure/Automapper/Profiles/RemovedChatMessageNotificationProfile.cs

---
+++ WireChat/WireChat.Infrastructure/Automapper/Profiles/RemovedChatMessageNotificationProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using WireChat.Application.Dtos;
+using WireChat.Infrastructure.EntityFramework.Models;
+
+namespace WireChat.Infrastructure.Automapper.Profiles
+{
+    internal sealed class RemovedChatMessageNotificationProfile : Profile
+    {
+        public RemovedChatMessageNotificationProfile()
+        {
+            CreateMap<RemovedChatMessageNotificationReadModel, RemovedChatMessageNotificationDto>()
+                .ForMember(dest => dest.UserName, opt => opt.MapFrom(src => src.User.UserName));
+        }
+    }
+}
