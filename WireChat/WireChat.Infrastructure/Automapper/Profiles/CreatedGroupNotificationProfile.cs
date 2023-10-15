26abc5d1b997ea4b2cdaa14b5be4c8ae82678527
JivkoSp <atjspasov@gmail.com>
2023-10-15 15:53:21 +0300

    Added new automapper profile type: WireChat/WireChat.Infrastructure/Automapper/Profiles/CreatedGroupNotificationProfile.cs

---
+++ WireChat/WireChat.Infrastructure/Automapper/Profiles/CreatedGroupNotificationProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using WireChat.Application.Dtos;
+using WireChat.Infrastructure.EntityFramework.Models;
+
+namespace WireChat.Infrastructure.Automapper.Profiles
+{
+    internal sealed class CreatedGroupNotificationProfile : Profile
+    {
+        public CreatedGroupNotificationProfile()
+        {
+            CreateMap<CreatedGroupNotificationReadModel, CreatedGroupNotificationDto>()
+                .ForMember(dest => dest.UserName, opt => opt.MapFrom(src => src.User.UserName))
+                .ForMember(dest => dest.GroupName, opt => opt.MapFrom(src => src.Group.GroupName));
+        }
+    }
+}
