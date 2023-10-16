7221d31cedb422b319b935887cf381bcbc4d5d77
JivkoSp <atjspasov@gmail.com>
2023-10-16 21:58:14 +0300

    Added new Entityframework model type: WireChat/WireChat.Infrastructure/EntityFramework/Models/ReceivedContactRequestNotificationReadModel.cs

---
+++ WireChat/WireChat.Infrastructure/EntityFramework/Models/ReceivedContactRequestNotificationReadModel.cs
@@ -0,0 +1,15 @@
+﻿
+namespace WireChat.Infrastructure.EntityFramework.Models
+{
+    public class ReceivedContactRequestNotificationReadModel
+    {
+        // Composite Primary Key { SenderUserId, ReceiverUserId }
+        public string SenderUserId { get; set; }
+        public string ReceiverUserId { get; set; }
+        public DateTimeOffset DateTime { get; set; }
+
+        // Relationships
+        public virtual UserReadModel Sender { get; set; }
+        public virtual UserReadModel Receiver { get; set; }
+    }
+}
