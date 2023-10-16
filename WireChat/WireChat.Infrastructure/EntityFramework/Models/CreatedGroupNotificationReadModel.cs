8db42bdd2012ae3afa3ca2f5bb299630ce04623b
JivkoSp <atjspasov@gmail.com>
2023-10-16 15:42:19 +0300

    Added new Entityframework model type: WireChat/WireChat.Infrastructure/EntityFramework/Models/CreatedGroupNotificationReadModel.cs

---
+++ WireChat/WireChat.Infrastructure/EntityFramework/Models/CreatedGroupNotificationReadModel.cs
@@ -0,0 +1,15 @@
+﻿
+namespace WireChat.Infrastructure.EntityFramework.Models
+{
+    public class CreatedGroupNotificationReadModel
+    {
+        // Composite Primary Key { UserId, GroupId }
+        public string UserId { get; set; }
+        public Guid GroupId { get; set; }
+        public DateTimeOffset DateTime { get; set; }
+
+        // Relationships
+        public virtual UserReadModel User { get; set; }
+        public virtual GroupReadModel Group { get; set; }
+    }
+}
