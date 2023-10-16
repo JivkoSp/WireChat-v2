328dfb965edb21f58a81aeb342dc1311a31dbbb0
JivkoSp <atjspasov@gmail.com>
2023-10-16 09:03:38 +0300

    Added new automapper profile type: WireChat/WireChat.Infrastructure/Automapper/Profiles/RemovedGroupMemberNotificationProfile.cs

---
+++ WireChat/WireChat.Infrastructure/Automapper/Profiles/RemovedGroupMemberNotificationProfile.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using WireChat.Application.Dtos;
+using WireChat.Infrastructure.EntityFramework.Models;
+
+namespace WireChat.Infrastructure.Automapper.Profiles
+{
+    internal sealed class RemovedGroupMemberNotificationProfile : Profile
+    {
+        public RemovedGroupMemberNotificationProfile()
+        {
+            CreateMap<RemovedGroupMemberNotificationReadModel, RemovedGroupMemberNotificationDto>()
+                .ForMember(dest => dest.GroupName, src => src.MapFrom(src => src.Group.GroupName))
+                .ForMember(dest => dest.GroupAdminUserName, src => src.MapFrom(src => src.GroupAdmin.UserName))
+                .ForMember(dest => dest.GroupMemberUserName, src => src.MapFrom(src => src.GroupMember.UserName));
+        }
+    }
+}
