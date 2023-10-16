b061456d744ee50905e664604bf9b9ec54cfce25
JivkoSp <atjspasov@gmail.com>
2023-10-16 23:10:39 +0300

    Added new Entityframework model type: WireChat/WireChat.Infrastructure/EntityFramework/Models/RemovedChatMessageNotificationReadModel.cs

---
+++ WireChat/WireChat.Infrastructure/EntityFramework/Models/RemovedChatMessageNotificationReadModel.cs
@@ -0,0 +1,17 @@
+﻿
+namespace WireChat.Infrastructure.EntityFramework.Models
+{
+    public class RemovedChatMessageNotificationReadModel
+    {
+        // Composite Primary Key { ChatId, ChatMessageId }
+        public Guid ChatId { get; set; }
+        public Guid ChatMessageId { get; set; }
+        public string UserId { get; set; }
+        public DateTimeOffset DateTime { get; set; }
+
+        // Relationships
+        public virtual ChatReadModel Chat { get; set; }
+        public virtual ChatMessageReadModel ChatMessage { get; set; }
+        public virtual UserReadModel User { get; set; }
+    }
+}
