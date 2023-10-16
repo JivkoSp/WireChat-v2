e4a65fe1ed243fa528534ba1b9379e0a088c961d
JivkoSp <atjspasov@gmail.com>
2023-10-16 14:16:24 +0300

    Added new Entityframework model type: WireChat/WireChat.Infrastructure/EntityFramework/Models/BannedContactNotificationReadModel.cs

---
+++ WireChat/WireChat.Infrastructure/EntityFramework/Models/BannedContactNotificationReadModel.cs
@@ -0,0 +1,15 @@
+﻿
+namespace WireChat.Infrastructure.EntityFramework.Models
+{
+    public class BannedContactNotificationReadModel
+    {
+        // Composite Primary Key { UserId, ChatId }
+        public string UserId { get; set; }
+        public Guid ChatId { get; set; }
+        public DateTimeOffset DateTime { get; set; }
+
+        // Relationships
+        public virtual UserReadModel User { get; set; }
+        public virtual ChatReadModel Chat { get; set; }
+    }
+}
