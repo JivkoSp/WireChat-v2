7764b2245ba30ade86e467ce7f04582a748f0d71
JivkoSp <atjspasov@gmail.com>
2023-10-17 20:27:48 +0300

    Added new Entityframework configuration: WireChat/WireChat.Infrastructure/EntityFramework/ModelConfiguration/ReadConfiguration/AcceptedContactRequestNotificationConfiguration.cs

---
+++ WireChat/WireChat.Infrastructure/EntityFramework/ModelConfiguration/ReadConfiguration/AcceptedContactRequestNotificationConfiguration.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using WireChat.Infrastructure.EntityFramework.Encryption.EncryptionConverters;
+using WireChat.Infrastructure.EntityFramework.Encryption.EncryptionProvider;
+using WireChat.Infrastructure.EntityFramework.Models;
+
+namespace WireChat.Infrastructure.EntityFramework.ModelConfiguration.ReadConfiguration
+{
+    internal sealed class AcceptedContactRequestNotificationConfiguration 
+        : IEntityTypeConfiguration<AcceptedContactRequestNotificationReadModel>
+    {
+        private readonly IEncryptionProvider _encryptionProvider;
+
+        public AcceptedContactRequestNotificationConfiguration(IEncryptionProvider encryptionProvider)
+        {
+            _encryptionProvider = encryptionProvider;
+        }
+
+        public void Configure(EntityTypeBuilder<AcceptedContactRequestNotificationReadModel> builder)
+        {
+            // Table name
+            builder.ToTable("AcceptedContactRequestNotification");
+
+            // Composite primary key
+            builder.HasKey(key => new { key.SenderUserId, key.ReceiverUserId });
+
+            // Property config
+            builder.Property(p => p.DateTime)
+                .HasConversion(new EncryptedDateTimeOffsetConverter(_encryptionProvider))
+                .IsRequired();
+
+            // Relationships
+            builder.HasOne(p => p.Sender)
+                .WithMany(p => p.SenderAcceptedContactRequestNotifications)
+                .HasForeignKey(p => p.SenderUserId)
+                .HasConstraintName("FK_Sender_SenderAcceptedContactRequestNotifications")
+                .OnDelete(DeleteBehavior.Cascade);
+
+            builder.HasOne(p => p.Receiver)
+                .WithMany(p => p.ReceiverAcceptedContactRequestNotifications)
+                .HasForeignKey(p => p.ReceiverUserId)
+                .HasConstraintName("FK_Receiver_ReceiverAcceptedContactRequestNotifications")
+                .OnDelete(DeleteBehavior.Cascade);
+        }
+    }
+}
