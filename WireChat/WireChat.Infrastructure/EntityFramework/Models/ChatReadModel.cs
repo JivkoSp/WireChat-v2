635eb19960860e2118608ea39e592881cf577104
JivkoSp <atjspasov@gmail.com>
2023-10-17 11:36:42 +0300

    Added new properties in WireChat/WireChat.Infrastructure/EntityFramework/Models/ChatReadModel.cs. The properties are: BannedContactNotifications and RemovedChatMessageNotifications

--- WireChat/WireChat.Infrastructure/EntityFramework/Models/ChatReadModel.cs
+++ WireChat/WireChat.Infrastructure/EntityFramework/Models/ChatReadModel.cs
@@ -11,5 +11,7 @@ namespace WireChat.Infrastructure.EntityFramework.Models
         public virtual List<ChatUserReadModel> ChatUsers { get; set; }
         public virtual List<BlockedChatUserReadModel> BlockedChatUsers { get; set; } 
         public virtual List<ChatMessageReadModel> ChatMessages { get; set; }
+        public virtual List<BannedContactNotificationReadModel> BannedContactNotifications { get; set; }
+        public virtual List<RemovedChatMessageNotificationReadModel> RemovedChatMessageNotifications { get; set; }
     }
 }
