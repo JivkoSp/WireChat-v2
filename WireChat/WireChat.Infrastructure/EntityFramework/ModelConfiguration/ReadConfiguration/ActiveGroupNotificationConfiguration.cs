64cd41ca292f7d40806ed2f623bcdbf6c3c74ea6
JivkoSp <atjspasov@gmail.com>
2023-10-17 22:28:10 +0300

    Added new Entityframework configuration: WireChat/WireChat.Infrastructure/EntityFramework/ModelConfiguration/ReadConfiguration/ActiveGroupNotificationConfiguration.cs

---
+++ WireChat/WireChat.Infrastructure/EntityFramework/ModelConfiguration/ReadConfiguration/ActiveGroupNotificationConfiguration.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using WireChat.Infrastructure.EntityFramework.Models;
+
+namespace WireChat.Infrastructure.EntityFramework.ModelConfiguration.ReadConfiguration
+{
+    internal sealed class ActiveGroupNotificationConfiguration : IEntityTypeConfiguration<ActiveGroupNotificationReadModel>
+    {
+        public void Configure(EntityTypeBuilder<ActiveGroupNotificationReadModel> builder)
+        {
+            // Table name
+            builder.ToTable("ActiveGroupNotification");
+
+            // Primary key
+            builder.HasKey(key => key.ActiveGroupNotificationId);
+
+            // Relationships
+            builder.HasOne(p => p.Group)
+                .WithMany(p => p.ActiveGroupNotifications)
+                .HasForeignKey(p => p.GroupId)
+                .HasConstraintName("FK_Group_ActiveGroupNotifications")
+                .OnDelete(DeleteBehavior.Cascade);
+        }
+    }
+}
