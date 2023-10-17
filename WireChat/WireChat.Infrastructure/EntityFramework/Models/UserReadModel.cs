4da12146bd8edf3f28ceaf829c2051fdb4de1727
JivkoSp <atjspasov@gmail.com>
2023-10-17 18:39:11 +0300

    Added new properties in WireChat/WireChat.Infrastructure/EntityFramework/Models/UserReadModel.cs. The properties are: SenderIssuedContactRequestNotifications, ReceiverIssuedContactRequestNotifications, SenderReceivedContactRequestNotifications, ReceiverReceivedContactRequestNotifications, SenderAcceptedContactRequestNotifications, ReceiverAcceptedContactRequestNotifications, SenderDeclinedContactRequestNotifications, ReceiverDeclinedContactRequestNotifications, GroupAdminAddedGroupMemberNotifications, GroupMemberAddedGroupMemberNotifications, GroupAdminRemovedGroupMemberNotifications, GroupMemberRemovedGroupMemberNotifications, BannedContactNotifications, GroupAdminBannedGroupMemberNotifications, GroupMemberBannedGroupMemberNotifications, CreatedGroupNotifications and RemovedChatMessageNotifications.

--- WireChat/WireChat.Infrastructure/EntityFramework/Models/UserReadModel.cs
+++ WireChat/WireChat.Infrastructure/EntityFramework/Models/UserReadModel.cs
@@ -14,5 +14,22 @@ namespace WireChat.Infrastructure.EntityFramework.Models
         public virtual List<ChatUserReadModel> ChatUsers { get; set; }
         public virtual List<BlockedChatUserReadModel> BlockedChatUsers { get; set; }
         public virtual List<ChatMessageReadModel> ChatMessages { get; set; }
+        public virtual List<IssuedContactRequestNotificationReadModel> SenderIssuedContactRequestNotifications { get; set; }
+        public virtual List<IssuedContactRequestNotificationReadModel> ReceiverIssuedContactRequestNotifications { get; set; }
+        public virtual List<ReceivedContactRequestNotificationReadModel> SenderReceivedContactRequestNotifications { get; set; }
+        public virtual List<ReceivedContactRequestNotificationReadModel> ReceiverReceivedContactRequestNotifications { get; set; }
+        public virtual List<AcceptedContactRequestNotificationReadModel> SenderAcceptedContactRequestNotifications { get; set; }
+        public virtual List<AcceptedContactRequestNotificationReadModel> ReceiverAcceptedContactRequestNotifications { get; set; }
+        public virtual List<DeclinedContactRequestNotificationReadModel> SenderDeclinedContactRequestNotifications { get; set; }
+        public virtual List<DeclinedContactRequestNotificationReadModel> ReceiverDeclinedContactRequestNotifications { get; set; }
+        public virtual List<AddedGroupMemberNotificationReadModel> GroupAdminAddedGroupMemberNotifications { get; set; }
+        public virtual List<AddedGroupMemberNotificationReadModel> GroupMemberAddedGroupMemberNotifications { get; set; }
+        public virtual List<RemovedGroupMemberNotificationReadModel> GroupAdminRemovedGroupMemberNotifications { get; set; }
+        public virtual List<RemovedGroupMemberNotificationReadModel> GroupMemberRemovedGroupMemberNotifications { get; set; }
+        public virtual List<BannedContactNotificationReadModel> BannedContactNotifications { get; set; }
+        public virtual List<BannedGroupMemberNotificationReadModel> GroupAdminBannedGroupMemberNotifications { get; set; }
+        public virtual List<BannedGroupMemberNotificationReadModel> GroupMemberBannedGroupMemberNotifications { get; set; }
+        public virtual List<CreatedGroupNotificationReadModel> CreatedGroupNotifications { get; set; }
+        public virtual List<RemovedChatMessageNotificationReadModel> RemovedChatMessageNotifications { get; set; }
     }
 }
