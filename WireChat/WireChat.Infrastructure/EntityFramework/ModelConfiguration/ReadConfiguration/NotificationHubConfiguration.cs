d5dbe2bfed3665646ecc6272ee6acba091b50c55
JivkoSp <atjspasov@gmail.com>
2023-10-18 19:14:46 +0300

    Added Entityframework configurations: WireChat/WireChat.Infrastructure/EntityFramework/ModelConfiguration/ReadConfiguration/NotificationHubConfiguration.cs and WireChat/WireChat.Infrastructure/EntityFramework/ModelConfiguration/ReadConfiguration/ReceivedContactRequestNotificationConfiguration.cs

---
+++ WireChat/WireChat.Infrastructure/EntityFramework/ModelConfiguration/ReadConfiguration/NotificationHubConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using WireChat.Infrastructure.EntityFramework.Models;
+
+namespace WireChat.Infrastructure.EntityFramework.ModelConfiguration.ReadConfiguration
+{
+    internal sealed class NotificationHubConfiguration : IEntityTypeConfiguration<NotificationHubReadModel>
+    {
+        public void Configure(EntityTypeBuilder<NotificationHubReadModel> builder)
+        {
+            // Table name
+            builder.ToTable("NotificationHub");
+
+            // Primary key
+            builder.HasKey(key => key.NotificationHubId);
+        }
+    }
+}
---
+++ WireChat/WireChat.Infrastructure/EntityFramework/ModelConfiguration/ReadConfiguration/ReceivedContactRequestNotificationConfiguration.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using WireChat.Infrastructure.EntityFramework.Encryption.EncryptionConverters;
+using WireChat.Infrastructure.EntityFramework.Encryption.EncryptionProvider;
+using WireChat.Infrastructure.EntityFramework.Models;
+
+namespace WireChat.Infrastructure.EntityFramework.ModelConfiguration.ReadConfiguration
+{
+    internal sealed class ReceivedContactRequestNotificationConfiguration
+        : IEntityTypeConfiguration<ReceivedContactRequestNotificationReadModel>
+    {
+        private readonly IEncryptionProvider _encryptionProvider;
+
+        public ReceivedContactRequestNotificationConfiguration(IEncryptionProvider encryptionProvider)
+        {
+            _encryptionProvider = encryptionProvider;
+        }
+
+        public void Configure(EntityTypeBuilder<ReceivedContactRequestNotificationReadModel> builder)
+        {
+            // Table name
+            builder.ToTable("ReceivedContactRequestNotification");
+
+            // Composite primary key
+            builder.HasKey(key => new { key.SenderUserId, key.ReceiverUserId });
+
+            // Property config
+            builder.Property(p => p.DateTime)
+                .HasConversion(new EncryptedDateTimeOffsetConverter(_encryptionProvider))
+                .IsRequired();
+
+            // Relationships
+            builder.HasOne(p => p.Sender)
+                .WithMany(p => p.SenderReceivedContactRequestNotifications)
+                .HasForeignKey(p => p.SenderUserId)
+                .HasConstraintName("FK_Sender_SenderReceivedContactRequestNotifications")
+                .OnDelete(DeleteBehavior.Cascade);
+
+            builder.HasOne(p => p.Receiver)
+                .WithMany(p => p.ReceiverReceivedContactRequestNotifications)
+                .HasForeignKey(p => p.ReceiverUserId)
+                .HasConstraintName("FK_Receiver_ReceiverReceivedContactRequestNotifications")
+                .OnDelete(DeleteBehavior.Cascade);
+        }
+    }
+}
