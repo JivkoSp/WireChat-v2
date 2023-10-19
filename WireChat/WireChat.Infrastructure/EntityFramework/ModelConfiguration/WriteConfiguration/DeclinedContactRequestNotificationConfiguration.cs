daeaf28a954c8e947e3d1d5d5e6affbb774c7b5c
JivkoSp <atjspasov@gmail.com>
2023-10-19 23:16:31 +0300

    Added new Entityframework configuration: WireChat/WireChat.Infrastructure/EntityFramework/ModelConfiguration/WriteConfiguration/DeclinedContactRequestNotificationConfiguration.cs

---
+++ WireChat/WireChat.Infrastructure/EntityFramework/ModelConfiguration/WriteConfiguration/DeclinedContactRequestNotificationConfiguration.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using WireChat.Domain.ValueObjects;
+using WireChat.Infrastructure.EntityFramework.Encryption.EncryptionConverters;
+using WireChat.Infrastructure.EntityFramework.Encryption.EncryptionProvider;
+
+namespace WireChat.Infrastructure.EntityFramework.ModelConfiguration.WriteConfiguration
+{
+    internal sealed class DeclinedContactRequestNotificationConfiguration
+        : IEntityTypeConfiguration<DeclinedContactRequestNotification>
+    {
+        private readonly IEncryptionProvider _encryptionProvider;
+
+        public DeclinedContactRequestNotificationConfiguration(IEncryptionProvider encryptionProvider)
+        {
+            _encryptionProvider = encryptionProvider;
+        }
+
+        public void Configure(EntityTypeBuilder<DeclinedContactRequestNotification> builder)
+        {
+            // Table name
+            builder.ToTable("DeclinedContactRequestNotification");
+
+            // Composite primary key
+            builder.HasKey(key => new { key.SenderUserId, key.ReceiverUserId });
+
+            // Property config
+            builder.Property(p => p.SenderUserId)
+                .HasConversion(id => id.Value.ToString(), id => new UserID(Guid.Parse(id)))
+                .IsRequired();
+
+            builder.Property(p => p.ReceiverUserId)
+                .HasConversion(id => id.Value.ToString(), id => new UserID(Guid.Parse(id)))
+                .IsRequired();
+
+            builder.Property(typeof(DateTimeOffset), "DateTime")
+               .HasConversion(new EncryptedDateTimeOffsetConverter(_encryptionProvider))
+               .IsRequired();
+        }
+    }
+}
