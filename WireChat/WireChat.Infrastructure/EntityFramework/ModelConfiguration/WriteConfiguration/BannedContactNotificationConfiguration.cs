d6ea280b5f3436f0dea71d77c3c84f373ad59f4d
JivkoSp <atjspasov@gmail.com>
2023-10-19 17:51:14 +0300

    Added new Entityframework configuration: WireChat/WireChat.Infrastructure/EntityFramework/ModelConfiguration/WriteConfiguration/BannedContactNotificationConfiguration.cs

---
+++ WireChat/WireChat.Infrastructure/EntityFramework/ModelConfiguration/WriteConfiguration/BannedContactNotificationConfiguration.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using WireChat.Domain.ValueObjects;
+using WireChat.Infrastructure.EntityFramework.Encryption.EncryptionConverters;
+using WireChat.Infrastructure.EntityFramework.Encryption.EncryptionProvider;
+
+namespace WireChat.Infrastructure.EntityFramework.ModelConfiguration.WriteConfiguration
+{
+    internal sealed class BannedContactNotificationConfiguration : IEntityTypeConfiguration<BannedContactNotification>
+    {
+        private readonly IEncryptionProvider _encryptionProvider;
+
+        public BannedContactNotificationConfiguration(IEncryptionProvider encryptionProvider)
+        {
+            _encryptionProvider = encryptionProvider;
+        }
+
+        public void Configure(EntityTypeBuilder<BannedContactNotification> builder)
+        {
+            // Table name
+            builder.ToTable("BannedContactNotification");
+
+            // Composite primary key
+            builder.HasKey(key => new { key.UserId, key.ChatId });
+
+            // Property config
+            builder.Property(p => p.UserId)
+                .HasConversion(id => id.Value.ToString(), id => new UserID(Guid.Parse(id)))
+                .IsRequired();
+
+            builder.Property(p => p.ChatId)
+                .HasConversion(id => id.Value, id => new ChatID(id))
+                .IsRequired();
+
+            builder.Property(typeof(DateTimeOffset), "DateTime")
+               .HasConversion(new EncryptedDateTimeOffsetConverter(_encryptionProvider))
+               .IsRequired();
+        }
+    }
+}
