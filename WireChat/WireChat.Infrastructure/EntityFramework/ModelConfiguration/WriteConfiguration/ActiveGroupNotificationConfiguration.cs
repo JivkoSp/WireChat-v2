ba816203a25e77f07df781b211ef90e94c088e34
JivkoSp <atjspasov@gmail.com>
2023-10-19 12:32:11 +0300

    Added new Entitiframework configuration: WireChat/WireChat.Infrastructure/EntityFramework/ModelConfiguration/WriteConfiguration/ActiveGroupNotificationConfiguration.cs

---
+++ WireChat/WireChat.Infrastructure/EntityFramework/ModelConfiguration/WriteConfiguration/ActiveGroupNotificationConfiguration.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using WireChat.Domain.ValueObjects;
+
+namespace WireChat.Infrastructure.EntityFramework.ModelConfiguration.WriteConfiguration
+{
+    internal sealed class ActiveGroupNotificationConfiguration : IEntityTypeConfiguration<ActiveGroupNotification>
+    {
+        public void Configure(EntityTypeBuilder<ActiveGroupNotification> builder)
+        {
+            // Table name
+            builder.ToTable("ActiveGroupNotification");
+
+            //Shadow property for ActiveGroupNotificationId.
+            builder.Property<Guid>("ActiveGroupNotificationId");
+
+            //Primary key
+            builder.HasKey("ActiveGroupNotificationId");
+
+            // Property config
+            builder.Property(p => p.GroupId)
+                .HasConversion(id => id.Value, id => new GroupID(id))
+                .IsRequired();
+        }
+    }
+}
