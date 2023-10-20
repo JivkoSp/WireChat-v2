f9ac8395b4395ab3db311c53ef6cd0d669db2d99
JivkoSp <atjspasov@gmail.com>
2023-10-20 10:48:43 +0300

    Added new Entityframework configuration: WireChat/WireChat.Infrastructure/EntityFramework/ModelConfiguration/WriteConfiguration/IssuedContactRequestNotificationConfiguration.cs

---
+++ WireChat/WireChat.Infrastructure/EntityFramework/ModelConfiguration/WriteConfiguration/IssuedContactRequestNotificationConfiguration.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using WireChat.Domain.ValueObjects;
+using WireChat.Infrastructure.EntityFramework.Encryption.EncryptionConverters;
+using WireChat.Infrastructure.EntityFramework.Encryption.EncryptionProvider;
+
+namespace WireChat.Infrastructure.EntityFramework.ModelConfiguration.WriteConfiguration
+{
+    internal sealed class IssuedContactRequestNotificationConfiguration : IEntityTypeConfiguration<IssuedContactRequestNotification>
+    {
+        private readonly IEncryptionProvider _encryptionProvider;
+
+        public IssuedContactRequestNotificationConfiguration(IEncryptionProvider encryptionProvider)
+        {
+            _encryptionProvider = encryptionProvider;
+        }
+
+        public void Configure(EntityTypeBuilder<IssuedContactRequestNotification> builder)
+        {
+            // Table name
+            builder.ToTable("IssuedContactRequestNotification");
+
+            // Composite primary key
+            builder.HasKey(key => new { key.SenderUserId, key.ReceiverUserId });
+
+            // Property config
+            builder.Property(p => p.SenderUserId)
+               .HasConversion(id => id.Value.ToString(), id => new UserID(Guid.Parse(id)))
+               .IsRequired();
+
+            builder.Property(p => p.ReceiverUserId)
+                .HasConversion(id => id.Value.ToString(), id => new UserID(Guid.Parse(id)))
+                .IsRequired();
+
+            builder.Property(typeof(DateTimeOffset), "DateTime")
+               .HasConversion(new EncryptedDateTimeOffsetConverter(_encryptionProvider))
+               .IsRequired();
+        }
+    }
+}
