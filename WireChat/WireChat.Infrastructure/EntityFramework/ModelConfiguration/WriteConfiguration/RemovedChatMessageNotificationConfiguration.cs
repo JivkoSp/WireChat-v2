c1f0ef783d5789dd92b8dc21d796c92f4eb1716c
JivkoSp <atjspasov@gmail.com>
2023-10-20 15:42:19 +0300

    Added new Entityframework configuration: WireChat/WireChat.Infrastructure/EntityFramework/ModelConfiguration/WriteConfiguration/RemovedChatMessageNotificationConfiguration.cs

---
+++ WireChat/WireChat.Infrastructure/EntityFramework/ModelConfiguration/WriteConfiguration/RemovedChatMessageNotificationConfiguration.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using WireChat.Domain.ValueObjects;
+using WireChat.Infrastructure.EntityFramework.Encryption.EncryptionConverters;
+using WireChat.Infrastructure.EntityFramework.Encryption.EncryptionProvider;
+
+namespace WireChat.Infrastructure.EntityFramework.ModelConfiguration.WriteConfiguration
+{
+    internal sealed class RemovedChatMessageNotificationConfiguration : IEntityTypeConfiguration<RemovedChatMessageNotification>
+    {
+        private readonly IEncryptionProvider _encryptionProvider;
+
+        public RemovedChatMessageNotificationConfiguration(IEncryptionProvider encryptionProvider)
+        {
+            _encryptionProvider = encryptionProvider;
+        }
+
+        public void Configure(EntityTypeBuilder<RemovedChatMessageNotification> builder)
+        {
+            // Table name
+            builder.ToTable("RemovedChatMessageNotification");
+
+            // Composite primary key
+            builder.HasKey(key => new { key.ChatId, key.ChatMessageId });
+
+            // Property config
+            builder.Property(p => p.ChatId)
+                .HasConversion(id => id.Value, id => new ChatID(id))
+                .IsRequired();
+
+            builder.Property(p => p.ChatMessageId)
+                .HasConversion(id => id.Value, id => new ChatMessageID(id))
+                .IsRequired();
+
+            builder.Property(p => p.UserId)
+                .HasConversion(id => id.Value.ToString(), id => new UserID(Guid.Parse(id)))
+                .IsRequired();
+
+            builder.Property(typeof(DateTimeOffset), "DateTime")
+               .HasConversion(new EncryptedDateTimeOffsetConverter(_encryptionProvider))
+               .IsRequired();
+        }
+    }
+}
