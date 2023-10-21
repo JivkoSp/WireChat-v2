a497e3640ee097fad47aa2947811c922fabbc718
JivkoSp <atjspasov@gmail.com>
2023-10-21 17:10:41 +0300

    Added ChatMessageExistsByIdAsync method in WireChat/WireChat.Application/Services/ReadServices/IChatReadService.cs

--- WireChat/WireChat.Application/Services/ReadServices/IChatReadService.cs
+++ WireChat/WireChat.Application/Services/ReadServices/IChatReadService.cs
@@ -4,5 +4,6 @@ namespace WireChat.Application.Services.ReadServices
     public interface IChatReadService
     {
         Task<bool> ExistsByIdAsync(Guid id);
+        Task<bool> ChatMessageExistsByIdAsync(Guid id);
     }
 }
