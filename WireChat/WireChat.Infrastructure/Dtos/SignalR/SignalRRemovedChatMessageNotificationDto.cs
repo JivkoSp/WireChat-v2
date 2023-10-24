1a92cbe789736e52ff7e5d1fc220536ee24d954d
JivkoSp <atjspasov@gmail.com>
2023-10-24 23:11:48 +0300

    Added new infrastructure layer dto type: WireChat/WireChat.Infrastructure/Dtos/SignalR/SignalRRemovedChatMessageNotificationDto.cs

---
+++ WireChat/WireChat.Infrastructure/Dtos/SignalR/SignalRRemovedChatMessageNotificationDto.cs
@@ -0,0 +1,10 @@
+﻿
+namespace WireChat.Infrastructure.Dtos.SignalR
+{
+    public class SignalRRemovedChatMessageNotificationDto
+    {
+        public string ChatId { get; set; }
+        public string UserName { get; set; } 
+        public DateTimeOffset DateTime { get; set; }
+    }
+}
