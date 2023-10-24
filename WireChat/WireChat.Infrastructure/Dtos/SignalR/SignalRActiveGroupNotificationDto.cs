673c6aab3d0dccad7c5e969046d31be0b42154d4
JivkoSp <atjspasov@gmail.com>
2023-10-24 12:56:12 +0300

    Added new infrastructure layer dto type: WireChat/WireChat.Infrastructure/Dtos/SignalR/SignalRActiveGroupNotificationDto.cs

---
+++ WireChat/WireChat.Infrastructure/Dtos/SignalR/SignalRActiveGroupNotificationDto.cs
@@ -0,0 +1,9 @@
+﻿
+namespace WireChat.Infrastructure.Dtos.SignalR
+{
+    public class SignalRActiveGroupNotificationDto
+    {
+        public string GroupId { get; set; }
+        public string GroupName { get; set; }
+    }
+}
