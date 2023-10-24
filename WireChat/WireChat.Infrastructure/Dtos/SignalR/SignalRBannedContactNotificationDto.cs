a4113d29d4badd9fdabc64b30df9d155fc0b0415
JivkoSp <atjspasov@gmail.com>
2023-10-24 14:48:56 +0300

    Added new infrastructure layer dto type: WireChat/WireChat.Infrastructure/Dtos/SignalR/SignalRBannedContactNotificationDto.cs

---
+++ WireChat/WireChat.Infrastructure/Dtos/SignalR/SignalRBannedContactNotificationDto.cs
@@ -0,0 +1,11 @@
+﻿
+namespace WireChat.Infrastructure.Dtos.SignalR
+{
+    public class SignalRBannedContactNotificationDto
+    {
+        public string ChatId { get; set; }
+        public string IssuerUserName { get; set; }
+        public string BannedUserName { get; set; }
+        public DateTimeOffset DateTime {  get; set; }
+    }
+}
