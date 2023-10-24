ab121dc544dfac78850527479090c17b41655a55
JivkoSp <atjspasov@gmail.com>
2023-10-24 12:19:54 +0300

    Added new infrastructure layer dto type: WireChat/WireChat.Infrastructure/Dtos/SignalR/SignalRAcceptedContactRequestNotificationDto.cs

---
+++ WireChat/WireChat.Infrastructure/Dtos/SignalR/SignalRAcceptedContactRequestNotificationDto.cs
@@ -0,0 +1,10 @@
+﻿
+namespace WireChat.Infrastructure.Dtos.SignalR
+{
+    public class SignalRAcceptedContactRequestNotificationDto
+    {
+        public string SenderUserName { get; set; }
+        public string ReceiverUserName { get; set; } 
+        public DateTimeOffset DateTime { get; set; }
+    }
+}
