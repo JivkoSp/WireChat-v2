7d3ed9ee10364baeb3285fa086287da7d950eaea
JivkoSp <atjspasov@gmail.com>
2023-10-24 17:21:59 +0300

    Added new infrastructure layer dto type: WireChat/WireChat.Infrastructure/Dtos/SignalR/SignalRCreatedGroupNotificationDto.cs

---
+++ WireChat/WireChat.Infrastructure/Dtos/SignalR/SignalRCreatedGroupNotificationDto.cs
@@ -0,0 +1,10 @@
+﻿
+namespace WireChat.Infrastructure.Dtos.SignalR
+{
+    public class SignalRCreatedGroupNotificationDto
+    {
+        public string UserName { get; set; } 
+        public string GroupName { get; set; } 
+        public DateTimeOffset DateTime { get; set; }
+    }
+}
