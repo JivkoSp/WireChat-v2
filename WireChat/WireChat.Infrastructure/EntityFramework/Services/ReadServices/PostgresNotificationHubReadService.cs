c4c3340c1d1e53e8bcc56c60b56d043ad512d51a
JivkoSp <atjspasov@gmail.com>
2023-10-25 10:46:15 +0300

    Added new read service: WireChat/WireChat.Infrastructure/EntityFramework/Services/ReadServices/PostgresNotificationHubReadService.cs

---
+++ WireChat/WireChat.Infrastructure/EntityFramework/Services/ReadServices/PostgresNotificationHubReadService.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WireChat.Application.Services.ReadServices;
+using WireChat.Infrastructure.EntityFramework.Contexts;
+
+namespace WireChat.Infrastructure.EntityFramework.Services.ReadServices
+{
+    internal sealed class PostgresNotificationHubReadService : INotificationHubReadService
+    {
+        private readonly ReadDbContext _readDbContext;
+
+        public PostgresNotificationHubReadService(ReadDbContext readDbContext)
+        {
+            _readDbContext = readDbContext;
+        }
+
+        public Task<bool> ExistsByIdAsync(Guid id)
+            => _readDbContext.NotificationHubReadModels.AnyAsync(x => x.NotificationHubId == id);
+    }
+}
