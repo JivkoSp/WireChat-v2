da9a4ac0e7a643f16b853acd38c1d4658cae3e6e
JivkoSp <atjspasov@gmail.com>
2023-10-25 13:58:20 +0300

    Added new SignalR Hub: WireChat/WireChat.Infrastructure/SignalR/Hubs/GroupNotificationHub.cs

---
+++ WireChat/WireChat.Infrastructure/SignalR/Hubs/GroupNotificationHub.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using WireChat.Infrastructure.Dtos.SignalR;
+
+namespace WireChat.Infrastructure.SignalR.Hubs
+{
+    public sealed class GroupNotificationHub : Hub
+    {
+        public async Task JoinGroup(string group)
+        {
+            await Groups.AddToGroupAsync(Context.ConnectionId, group);
+        }
+
+        public async Task ActiveGroupNotification(SignalRActiveGroupNotificationDto activeGroupNotification)
+        {
+            await Clients.Group(activeGroupNotification.GroupId)
+                .SendAsync("ActiveGroupNotification", activeGroupNotification);
+        }
+
+        public async Task AddedGroupMemberNotification(SignalRAddedGroupMemberNotificationDto addedGroupMemberNotification)
+        {
+            await Clients.Group(addedGroupMemberNotification.GroupId)
+                .SendAsync("AddedGroupMemberNotification", addedGroupMemberNotification);
+        }
+
+        public async Task BannedGroupMemberNotification(SignalRBannedGroupMemberNotificationDto bannedGroupMemberNotification)
+        {
+            await Clients.Group(bannedGroupMemberNotification.GroupId)
+                .SendAsync("BannedGroupMemberNotification", bannedGroupMemberNotification);
+        }
+
+        public async Task RemovedGroupMemberNotification(SignalRRemovedGroupMemberNotificationDto removedGroupMemberNotification)
+        {
+            await Clients.Group(removedGroupMemberNotification.GroupId)
+                .SendAsync("RemovedGroupMemberNotification", removedGroupMemberNotification);
+        }
+    }
+}
