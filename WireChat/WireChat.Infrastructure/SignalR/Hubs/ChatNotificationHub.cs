16deb4d2aefeeb641caf8c3d74b80e00593e6024
JivkoSp <atjspasov@gmail.com>
2023-10-25 13:16:31 +0300

    Added new SignalR Hub: WireChat/WireChat.Infrastructure/SignalR/Hubs/ChatNotificationHub.cs

---
+++ WireChat/WireChat.Infrastructure/SignalR/Hubs/ChatNotificationHub.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using WireChat.Infrastructure.Dtos.SignalR;
+
+namespace WireChat.Infrastructure.SignalR.Hubs
+{
+    public sealed class ChatNotificationHub : Hub
+    {
+        public async Task JoinGroup(string group)
+        {
+            await Groups.AddToGroupAsync(Context.ConnectionId, group);
+        }
+
+        public async Task BannedContactNotification(SignalRBannedContactNotificationDto bannedContactNotification)
+        {
+            await Clients.Group(bannedContactNotification.ChatId)
+                .SendAsync("BannedContactNotification", bannedContactNotification);
+        }
+
+        public async Task RemovedChatMessageNotification(SignalRRemovedChatMessageNotificationDto removedChatMessageNotification)
+        {
+            await Clients.Group(removedChatMessageNotification.ChatId)
+                .SendAsync("RemovedChatMessageNotification", removedChatMessageNotification);
+        }
+    }
+}
