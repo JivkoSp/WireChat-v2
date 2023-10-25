122879ae389dcfbab9a9f4b0df225e23fad6a915
JivkoSp <atjspasov@gmail.com>
2023-10-25 16:56:21 +0300

    Added new view component: WireChat/WireChat/Components/NotificationsViewComponent.cs

---
+++ WireChat/WireChat/Components/NotificationsViewComponent.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using WireChat.Application.Queries;
+using WireChat.Application.Queries.Dispatcher;
+using WireChat.Infrastructure.EntityFramework.Models;
+
+namespace WireChat.Components
+{
+    public class NotificationsViewComponent : ViewComponent
+    {
+        private readonly IQueryDispatcher _queryDispatcher;
+        private readonly UserManager<UserReadModel> _userManager;
+
+        public NotificationsViewComponent(IQueryDispatcher queryDispatcher, UserManager<UserReadModel> userManager)
+        {
+            _queryDispatcher = queryDispatcher;
+            _userManager = userManager;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var user = await _userManager.FindByNameAsync(User.Identity.Name);
+
+            var getNotificationHubQuery = new GetNotificationHubQuery(Guid.Parse(user.Id));
+
+            var notificationHub = await _queryDispatcher.DispatchAsync(getNotificationHubQuery);
+
+            return View(notificationHub);
+        }
+    }
+}
