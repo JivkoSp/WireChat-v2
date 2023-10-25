56a6eb94904b4e8ee78d5e4d0b722df837237cd3
JivkoSp <atjspasov@gmail.com>
2023-10-25 19:11:33 +0300

    Updated WireChat/WireChat.Application/Dtos/AcceptedContactRequestNotificationDto.cs

--- WireChat/WireChat.Application/Dtos/AcceptedContactRequestNotificationDto.cs
+++ WireChat/WireChat.Application/Dtos/AcceptedContactRequestNotificationDto.cs
@@ -7,6 +7,6 @@ namespace WireChat.Application.Dtos
         public string ReceiverUserId { get; set; }
         public DateTimeOffset DateTime { get; set; }
         public string SenderUserName { get; set; }
-        public string ReceiverUsername { get; set; }
+        public string ReceiverUserName { get; set; }
     }
 }
