01fb93844ca7f124e2d13bcbbf68a328b855c6c2
JivkoSp <atjspasov@gmail.com>
2023-10-25 16:02:31 +0300

    Added new SignalR Hub: WireChat/WireChat.Infrastructure/SignalR/Hubs/UserNotificationHub.cs

---
+++ WireChat/WireChat.Infrastructure/SignalR/Hubs/UserNotificationHub.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using WireChat.Infrastructure.Dtos.SignalR;
+
+namespace WireChat.Infrastructure.SignalR.Hubs
+{
+    public sealed class UserNotificationHub : Hub
+    {
+        public async Task JoinGroup(string group)
+        {
+            await Groups.AddToGroupAsync(Context.ConnectionId, group);
+        }
+
+        public async Task AcceptedContactRequestNotification(SignalRAcceptedContactRequestNotificationDto 
+            acceptedContactRequestNotification)
+        {
+            await Clients.Group(acceptedContactRequestNotification.SenderUserName)
+                .SendAsync("AcceptedContactRequestNotification", acceptedContactRequestNotification);
+
+            await Clients.Group(acceptedContactRequestNotification.ReceiverUserName)
+                .SendAsync("AcceptedContactRequestNotification", acceptedContactRequestNotification);
+        }
+
+        public async Task CreatedGroupNotification(SignalRCreatedGroupNotificationDto createdGroupNotification)
+        {
+            await Clients.Group(createdGroupNotification.UserName)
+                .SendAsync("CreatedGroupNotification", createdGroupNotification);
+        }
+
+        public async Task DeclinedContactRequestNotification(SignalRDeclinedContactRequestNotificationDto declinedContactRequestNotification)
+        {
+            await Clients.Group(declinedContactRequestNotification.SenderUserName)
+                .SendAsync("DeclinedContactRequestNotification", declinedContactRequestNotification);
+
+            await Clients.Group(declinedContactRequestNotification.ReceiverUserName)
+                .SendAsync("DeclinedContactRequestNotification", declinedContactRequestNotification);
+        }
+
+        public async Task IssuedContactRequestNotification(SignalRIssuedContactRequestNotificationDto issuedContactRequestNotification)
+        {
+            await Clients.Group(issuedContactRequestNotification.SenderUserName)
+                .SendAsync("IssuedContactRequestNotification", issuedContactRequestNotification);
+
+            await Clients.Group(issuedContactRequestNotification.ReceiverUserName)
+                .SendAsync("ReceivedContactRequestNotification", issuedContactRequestNotification);
+        }
+    }
+}
