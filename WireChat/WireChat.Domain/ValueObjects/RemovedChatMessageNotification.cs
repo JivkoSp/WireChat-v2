3e463a802b3c2bdfcf5d8ed2450952d937be33f6
JivkoSp <atjspasov@gmail.com>
2023-10-27 20:51:14 +0300

    Added new property NotificationHubId of type NotificationHubID in WireChat/WireChat.Domain/ValueObjects/RemovedChatMessageNotification.cs

--- WireChat/WireChat.Domain/ValueObjects/RemovedChatMessageNotification.cs
+++ WireChat/WireChat.Domain/ValueObjects/RemovedChatMessageNotification.cs
@@ -7,9 +7,11 @@ namespace WireChat.Domain.ValueObjects
         public ChatID ChatId { get; }
         public ChatMessageID ChatMessageId { get; }
         public UserID UserId { get; }
+        public NotificationHubID NotificationHubId { get; set; }
         internal DateTimeOffset DateTime { get; }
 
-        public RemovedChatMessageNotification(ChatID chatId, ChatMessageID chatMessageId, UserID userId, DateTimeOffset dateTime)
+        public RemovedChatMessageNotification(ChatID chatId, ChatMessageID chatMessageId, UserID userId,
+            NotificationHubID notificationHubId, DateTimeOffset dateTime)
         {
             if (chatId == null)
             {
@@ -26,6 +28,11 @@ namespace WireChat.Domain.ValueObjects
                 throw new NullUserIdException();
             }
 
+            if (notificationHubId == null)
+            {
+                throw new NullNotificationHubIdException();
+            }
+
             if (dateTime == default || dateTime > DateTimeOffset.Now)
             {
                 throw new InvalidRemovedChatMessageDateTimeException();
@@ -34,6 +41,7 @@ namespace WireChat.Domain.ValueObjects
             ChatId = chatId;
             ChatMessageId = chatMessageId;
             UserId = userId;
+            NotificationHubId = notificationHubId;
             DateTime = dateTime;
         }
     }
