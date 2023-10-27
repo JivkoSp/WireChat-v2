7a4e40938c9f36ba340d01469c1ada46270bfcea
JivkoSp <atjspasov@gmail.com>
2023-10-27 14:02:41 +0300

    Added new property NotificationHubId of type NotificationHubID in WireChat/WireChat.Domain/ValueObjects/ActiveGroupNotification.cs

--- WireChat/WireChat.Domain/ValueObjects/ActiveGroupNotification.cs
+++ WireChat/WireChat.Domain/ValueObjects/ActiveGroupNotification.cs
@@ -5,15 +5,21 @@ namespace WireChat.Domain.ValueObjects
     public record ActiveGroupNotification
     {
         public GroupID GroupId { get; }
-
-        public ActiveGroupNotification(GroupID groupId)
+        public NotificationHubID NotificationHubId { get; set; }
+        public ActiveGroupNotification(GroupID groupId, NotificationHubID notificationHubId)
         {
             if (groupId == null)
             {
                 throw new NullGroupIdException();
             }
 
+            if (notificationHubId == null)
+            {
+                throw new NullNotificationHubIdException();
+            }
+
             GroupId = groupId;
+            NotificationHubId = notificationHubId;
         }
     }
 }
