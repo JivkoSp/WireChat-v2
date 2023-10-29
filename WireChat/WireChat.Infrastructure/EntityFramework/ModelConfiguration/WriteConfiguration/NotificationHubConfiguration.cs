cc63026ed8d12c773013ecc7d3bf3e19aff77859
JivkoSp <atjspasov@gmail.com>
2023-10-29 22:17:29 +0300

    Updated WireChat/WireChat.Infrastructure/EntityFramework/ModelConfiguration/WriteConfiguration/NotificationHubConfiguration.cs

--- WireChat/WireChat.Infrastructure/EntityFramework/ModelConfiguration/WriteConfiguration/NotificationHubConfiguration.cs
+++ WireChat/WireChat.Infrastructure/EntityFramework/ModelConfiguration/WriteConfiguration/NotificationHubConfiguration.cs
@@ -22,17 +22,71 @@ namespace WireChat.Infrastructure.EntityFramework.ModelConfiguration.WriteConfig
                 .IsRequired();
 
             // Relationships
-            builder.HasMany(p => p.IssuedContactRequestNotifications);
-            builder.HasMany(p => p.ReceivedContactRequestNotifications);
-            builder.HasMany(p => p.AcceptedContactRequestNotifications);
-            builder.HasMany(p => p.DeclinedContactRequestNotifications);
-            builder.HasMany(p => p.ActiveGroupNotifications);
-            builder.HasMany(p => p.AddedGroupMemberNotifications);
-            builder.HasMany(p => p.RemovedGroupMemberNotifications);
-            builder.HasMany(p => p.BannedContactNotifications);
-            builder.HasMany(p => p.BannedGroupMemberNotifications);
-            builder.HasMany(p => p.CreatedGroupNotifications);
-            builder.HasMany(p => p.RemovedChatMessageNotifications);
+            builder.HasMany(p => p.IssuedContactRequestNotifications)
+                .WithOne()
+                .HasForeignKey("NotificationHubId")
+                .HasConstraintName("FK_NotificationHub_IssuedContactRequestNotifications")
+                .OnDelete(DeleteBehavior.Cascade);
+
+            builder.HasMany(p => p.ReceivedContactRequestNotifications)
+                .WithOne()
+                .HasForeignKey("NotificationHubId")
+                .HasConstraintName("FK_NotificationHub_ReceivedContactRequestNotifications")
+                .OnDelete(DeleteBehavior.Cascade);
+
+            builder.HasMany(p => p.AcceptedContactRequestNotifications)
+                .WithOne()
+                .HasForeignKey("NotificationHubId")
+                .HasConstraintName("FK_NotificationHub_AcceptedContactRequestNotifications")
+                .OnDelete(DeleteBehavior.Cascade);
+
+            builder.HasMany(p => p.DeclinedContactRequestNotifications)
+                .WithOne()
+                .HasForeignKey("NotificationHubId")
+                .HasConstraintName("FK_NotificationHub_DeclinedContactRequestNotifications")
+                .OnDelete(DeleteBehavior.Cascade);
+
+            builder.HasMany(p => p.ActiveGroupNotifications)
+                .WithOne()
+                .HasForeignKey("NotificationHubId")
+                .HasConstraintName("FK_NotificationHub_ActiveGroupNotifications")
+                .OnDelete(DeleteBehavior.Cascade);
+
+            builder.HasMany(p => p.AddedGroupMemberNotifications)
+                .WithOne()
+                .HasForeignKey("NotificationHubId")
+                .HasConstraintName("FK_NotificationHub_AddedGroupMemberNotifications")
+                .OnDelete(DeleteBehavior.Cascade);
+
+            builder.HasMany(p => p.RemovedGroupMemberNotifications)
+                .WithOne()
+                .HasForeignKey("NotificationHubId")
+                .HasConstraintName("FK_NotificationHub_RemovedGroupMemberNotifications")
+                .OnDelete(DeleteBehavior.Cascade);
+
+            builder.HasMany(p => p.BannedContactNotifications)
+                .WithOne()
+                .HasForeignKey("NotificationHubId")
+                .HasConstraintName("FK_NotificationHub_BannedContactNotifications")
+                .OnDelete(DeleteBehavior.Cascade);
+
+            builder.HasMany(p => p.BannedGroupMemberNotifications)
+                .WithOne()
+                .HasForeignKey("NotificationHubId")
+                .HasConstraintName("FK_NotificationHub_BannedGroupMemberNotifications")
+                .OnDelete(DeleteBehavior.Cascade);
+
+            builder.HasMany(p => p.CreatedGroupNotifications)
+                .WithOne()
+                .HasForeignKey("NotificationHubId")
+                .HasConstraintName("FK_NotificationHub_CreatedGroupNotifications")
+                .OnDelete(DeleteBehavior.Cascade);
+
+            builder.HasMany(p => p.RemovedChatMessageNotifications)
+                .WithOne()
+                .HasForeignKey("NotificationHubId")
+                .HasConstraintName("FK_NotificationHub_RemovedChatMessageNotifications")
+                .OnDelete(DeleteBehavior.Cascade);
         }
     }
 }
