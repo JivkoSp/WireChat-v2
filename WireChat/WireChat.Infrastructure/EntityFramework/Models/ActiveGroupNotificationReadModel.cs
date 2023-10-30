4adb9d441ee4c54c429304757c42259008c9aade
JivkoSp <atjspasov@gmail.com>
2023-10-30 14:38:15 +0300

    Added NotificationHubId property of type Guid in WireChat/WireChat.Infrastructure/EntityFramework/Models/ActiveGroupNotificationReadModel.cs

--- WireChat/WireChat.Infrastructure/EntityFramework/Models/ActiveGroupNotificationReadModel.cs
+++ WireChat/WireChat.Infrastructure/EntityFramework/Models/ActiveGroupNotificationReadModel.cs
@@ -7,7 +7,9 @@ namespace WireChat.Infrastructure.EntityFramework.Models
         public Guid ActiveGroupNotificationId { get; set; }
 
         // Relationships
-        public Guid GroupId { get; set; }
+        public Guid NotificationHubId { get; set; } // Foreign key to the NotificationHub table
+        public Guid GroupId { get; set; } // Foreign key to the Group table
+        public virtual NotificationHubReadModel NotificationHub { get; set; }
         public virtual GroupReadModel Group { get; set; }
     }
 }
