44a4221304d1fa2a197b890bede9b020d51101fb
JivkoSp <atjspasov@gmail.com>
2023-11-02 16:29:37 +0300

    Added new application layer dto type: WireChat/WireChat.Application/Dtos/UserInfoDto.cs

---
+++ WireChat/WireChat.Application/Dtos/UserInfoDto.cs
@@ -0,0 +1,11 @@
+﻿
+namespace WireChat.Application.Dtos
+{
+    public class UserInfoDto
+    {
+        public string UserName { get; set; }
+        public string UserFirstName { get; set; }
+        public string UserLastName { get; set; }
+        public string UserEmail { get; set; }
+    }
+}
