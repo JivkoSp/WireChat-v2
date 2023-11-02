3710dca5dcf451f62badbcec98b9f20f518dee50
JivkoSp <atjspasov@gmail.com>
2023-11-02 22:54:32 +0300

    Added new query handler: WireChat/WireChat.Infrastructure/Queries/Handlers/GetUserInfoHandler.cs

---
+++ WireChat/WireChat.Infrastructure/Queries/Handlers/GetUserInfoHandler.cs
@@ -0,0 +1,30 @@
+﻿using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+using WireChat.Application.Dtos;
+using WireChat.Application.Queries;
+using WireChat.Application.Queries.Handlers;
+using WireChat.Infrastructure.EntityFramework.Contexts;
+
+namespace WireChat.Infrastructure.Queries.Handlers
+{
+    internal sealed class GetUserInfoHandler : IQueryHandler<GetUserInfoQuery, UserInfoDto>
+    {
+        private readonly ReadDbContext _readDbContext;
+        private readonly IMapper _mapper;
+
+        public GetUserInfoHandler(ReadDbContext readDbContext, IMapper mapper)
+        {
+            _readDbContext = readDbContext;
+            _mapper = mapper;
+        }
+
+        public async Task<UserInfoDto> HandleAsync(GetUserInfoQuery query)
+        {
+            var userReadModel = await _readDbContext.UserReadModels
+                .AsNoTracking()
+                .SingleOrDefaultAsync(x => x.Id == query.UserId);
+
+            return _mapper.Map<UserInfoDto>(userReadModel);
+        }
+    }
+}
