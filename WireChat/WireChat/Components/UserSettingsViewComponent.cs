583b6616ffd2c2891ce3d113065b5b84d661fb42
JivkoSp <atjspasov@gmail.com>
2023-11-03 12:11:39 +0300

    Added new view component: WireChat/WireChat/Components/UserSettingsViewComponent.cs

---
+++ WireChat/WireChat/Components/UserSettingsViewComponent.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using WireChat.Application.Queries;
+using WireChat.Application.Queries.Dispatcher;
+using WireChat.Infrastructure.EntityFramework.Models;
+
+namespace WireChat.Components
+{
+    public class UserSettingsViewComponent : ViewComponent
+    {
+        private readonly IQueryDispatcher _queryDispatcher;
+        private readonly UserManager<UserReadModel> _userManager;
+
+        public UserSettingsViewComponent(IQueryDispatcher queryDispatcher, UserManager<UserReadModel> userManager)
+        {
+            _queryDispatcher = queryDispatcher;
+            _userManager = userManager;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var user = await _userManager.FindByNameAsync(User.Identity.Name);
+
+            var getUserSettingsQuery = new GetUserSettingsQuery(user.Id);
+
+            var userDto = await _queryDispatcher.DispatchAsync(getUserSettingsQuery);
+
+            return View(userDto);
+        }
+    }
+}
