2783064cc57586bb2c52999188242823784be303
JivkoSp <atjspasov@gmail.com>
2023-11-04 17:16:54 +0300

    Added new application layer dto type: WireChat/WireChat.Application/Dtos/BlockedChatUserDto.cs

--- WireChat/WireChat.Application/Dtos/BlockedChatUserDto.cs
+++ WireChat/WireChat.Application/Dtos/BlockedChatUserDto.cs
@@ -6,5 +6,6 @@ namespace WireChat.Application.Dtos
         public string UserId { get; set; }
         public Guid ChatId { get; set; }
         public string UserName { get; set; }
+        public string UserPicture { get; set; }
     }
 }
