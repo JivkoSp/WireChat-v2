d2a26c7b9440693f19b5773f530c6545677aed93
JivkoSp <atjspasov@gmail.com>
2023-11-06 17:12:23 +0300

    Added WireChat/WireChat/Extensions/StringExtensions.cs

---
+++ WireChat/WireChat/Extensions/StringExtensions.cs
@@ -0,0 +1,19 @@
+﻿
+namespace WireChat.Extensions
+{
+    public static class StringExtensions
+    {
+        private static readonly HttpClient _httpClient = new HttpClient();
+
+        private const string RoboHashUrl = "https://robohash.org/";
+
+        public static string CreatePicture(this string value) 
+        {
+            var imageBytes = _httpClient.GetByteArrayAsync($"{RoboHashUrl}{value}.png?size=200x200").Result;
+
+            string base64String = Convert.ToBase64String(imageBytes);
+
+            return base64String;
+        }
+    }
+}
