9ef419ee879368073403d93496b87d049daf9a09
JivkoSp <atjspasov@gmail.com>
2023-11-06 11:23:14 +0300

    Added new infrastructure dto type: WireChat/WireChat.Infrastructure/Dtos/SignalR/SignalRBlockedChatUserDto.cs

---
+++ WireChat/WireChat.Infrastructure/Dtos/SignalR/SignalRBlockedChatUserDto.cs
@@ -0,0 +1,11 @@
+﻿
+namespace WireChat.Infrastructure.Dtos.SignalR
+{
+    public class SignalRBlockedChatUserDto
+    {
+        public string ChatId { get; set; }
+        public string UserId { get; set; }
+        public string UserName { get; set; }
+        public string UserPicture { get; set; }
+    }
+}
