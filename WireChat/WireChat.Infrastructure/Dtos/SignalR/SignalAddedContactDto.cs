e4bcea66ce6cd4bb7a41819cbe2a56e64a813d01
JivkoSp <atjspasov@gmail.com>
2023-11-06 09:54:48 +0300

    Added new infrastructure dto type: WireChat/WireChat.Infrastructure/Dtos/SignalR/SignalAddedContactDto.cs

---
+++ WireChat/WireChat.Infrastructure/Dtos/SignalR/SignalAddedContactDto.cs
@@ -0,0 +1,12 @@
+﻿
+namespace WireChat.Infrastructure.Dtos.SignalR
+{
+    public class SignalAddedContactDto
+    {
+        public string IssuerUserName { get; set; }
+        public string ReceiverUserName { get; set; }
+        public string IssuerUserPicture { get; set; }
+        public string ReceiverUserPicture { get; set; }
+        public Guid ChatId { get; set; }
+    }
+}
