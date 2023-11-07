9b092020cdba93e1fbaf956c6a8b5cbf73d0fce3
JivkoSp <atjspasov@gmail.com>
2023-11-07 16:46:14 +0300

    Added CurrentUserPicture property of type string in WireChat/WireChat/ViewModels/ChatViewModel.cs

--- WireChat/WireChat/ViewModels/ChatViewModel.cs
+++ WireChat/WireChat/ViewModels/ChatViewModel.cs
@@ -5,6 +5,7 @@ namespace WireChat.ViewModels
     public class ChatViewModel
     {
         public string CurrentUserId { get; set; }
+        public string CurrentUserPicture { get; set; }
         public ChatDto ChatDto { get; set; }
     }
 }
