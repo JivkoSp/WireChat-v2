9ee466a79c27a95697dd05e4737260bd5d4fb31c
JivkoSp <atjspasov@gmail.com>
2023-11-08 15:46:52 +0300

    Added ChatId property of type Guid in WireChat/WireChat.Application/Dtos/ChatMessageDto.cs

--- WireChat/WireChat.Application/Dtos/ChatMessageDto.cs
+++ WireChat/WireChat.Application/Dtos/ChatMessageDto.cs
@@ -3,6 +3,7 @@ namespace WireChat.Application.Dtos
     public class ChatMessageDto
     {
         public Guid ChatMessageId { get; set; }
+        public Guid ChatId { get; set; }
         public string UserId { get; set; }
         public string UserName { get; set; }
         public string UserPicture { get; set; }
